cafb8cce5dc656c3f64d8a12ba6a54a457b89b23
Jon Schewe <jpschewe@users.sourceforge.net>
2008-07-06 03:32:52 +0000

    Merged revisions 587 via svnmerge from https://fll-sw.svn.sourceforge.net/svnroot/fll-sw/branches/jpschewe/scoring
    ........
      r587 | jpschewe | 2008-07-05 22:31:19 -0500 (Sat, 05 Jul 2008) | 4 lines
      
      Added the ability to specify how many rows to add when adding rows to the judges assignment page.
      
      task 914619: assign judges should allow add N rows 
      https://sourceforge.net/tracker/?group_id=76410&func=detail&atid=705211&aid=914619
    ........

--- scoring/src/fll/web/admin/Judges.java
+++ scoring/src/fll/web/admin/Judges.java
@@ -23,6 +23,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.jsp.JspWriter;
 
+import org.apache.log4j.Logger;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -32,13 +33,15 @@ import fll.db.Queries;
 
 /**
  * Java code used in judges.jsp
- *
+ * 
  * @version $Revision$
  */
 public final class Judges {
-   
+
+  private static final Logger LOG = Logger.getLogger(Judges.class);
+
   private Judges() {
-     
+
   }
 
   /**
@@ -47,114 +50,133 @@ public final class Judges {
   public static void generatePage(final JspWriter out,
                                   final ServletContext application,
                                   final HttpServletRequest request,
-                                  final HttpServletResponse response)
-    throws SQLException, IOException, ParseException {
+                                  final HttpServletResponse response) throws SQLException, IOException, ParseException {
     final Document challengeDocument = (Document)application.getAttribute("challengeDocument");
     final Connection connection = (Connection)application.getAttribute("connection");
     final String tournament = Queries.getCurrentTournament(connection);
-    final String submitButton = request.getParameter("submit");
 
     final NodeList subjectiveCategories = challengeDocument.getDocumentElement().getElementsByTagName("subjectiveCategory");
 
     // count the number of rows present
-    int rowIndex= 0;
-    while(null != request.getParameter("cat" + (rowIndex+1))) {
+    int rowIndex = 0;
+    while(null != request.getParameter("cat" + (rowIndex + 1))) {
       ++rowIndex;
-      out.println("<!-- found a row " + rowIndex+ "-->");
+      out.println("<!-- found a row " + rowIndex + "-->");
     }
-    if("Add Row".equals(submitButton)) {
-      out.println("<!-- adding another row to " + rowIndex+ "-->");
-      ++rowIndex;
+    if(null != request.getParameter("add_rows")) {
+      if(LOG.isDebugEnabled()) {
+        LOG.debug("<!-- adding another row to " + rowIndex + "-->");
+      }
+      try {
+        final Integer numRows = Integer.valueOf(request.getParameter("num_rows"));
+        rowIndex += numRows;
+      } catch(final NumberFormatException nfe) {
+        if(LOG.isDebugEnabled()) {
+          LOG.debug("Got exception reading number of rows, defaulting to 1", nfe);
+        }
+        rowIndex += 1;
+      }
     }
-    out.println("<!-- final count of rows is " + rowIndex + "-->");
+
     final int numRows = rowIndex + 1;
     out.println("<form action='judges.jsp' method='POST' name='judges'>");
 
     String errorString = null;
-    if("Finished".equals(submitButton)) {
+    if(null != request.getParameter("finished")) {
       errorString = generateVerifyTable(out, subjectiveCategories, request);
-    } else if("Commit".equals(submitButton)) {
+      if(null == errorString) {
+        // everything is good
+        out.println("</form>");
+        return;
+      }
+    } else if(null != request.getParameter("commit")) {
       commitData(subjectiveCategories, request, response, connection, Queries.getCurrentTournament(connection));
+      return;
     }
 
-    if(null == submitButton || "Cancel".equals(submitButton) || "Add Row".equals(submitButton) || null != errorString) {
-      if(null != errorString) {
-        out.println("<p id='error'><font color='red'>" + errorString + "</font></p>");
-      }
+    if(null != errorString) {
+      out.println("<p id='error'><font color='red'>" + errorString + "</font></p>");
+    }
 
-      //get list of divisions and add "All" as a possible value 
-      final List<String> divisions = Queries.getDivisions(connection);
-      divisions.add(0, "All");
-      
-      out.println("<p>Judges ID's must be unique.  They can be just the name of the judge.  Keep in mind that this ID needs to be entered on the judging forms.  There must be at least 1 judge for each category.</p>");
-      
-      out.println("<table border='1'><tr><th>ID</th><th>Category</th><th>Division</th></tr>");
+    // get list of divisions and add "All" as a possible value
+    final List<String> divisions = Queries.getDivisions(connection);
+    divisions.add(0, "All");
 
-      int row = 0; //keep track of which row we're generating
-      
-      if(null == request.getParameter("cat0")) {
-        //this is the first time the page has been visited so we need to read
-        //the judges out of the DB
-        ResultSet rs = null;
-        Statement stmt = null;
-        try {
-          stmt = connection.createStatement();
-          rs = stmt.executeQuery("SELECT id, category, event_division FROM Judges WHERE Tournament = '" + tournament + "'");
-          for(row=0; rs.next(); row++) {
-            final String id = rs.getString(1);
-            final String category = rs.getString(2);
-            final String division = rs.getString(3);
-            generateRow(out, subjectiveCategories, divisions, row, id, category, division);
-          }
-        } finally {
-          Utilities.closeResultSet(rs);
-          Utilities.closeStatement(stmt);
-        }
-      } else {
-        //need to walk the parameters to see what we've been passed
-        String id = request.getParameter("id" + row);
-        String category = request.getParameter("cat" + row);
-        String division = request.getParameter("div" + row);
-        while(null != category) {
+    out
+        .println("<p>Judges ID's must be unique.  They can be just the name of the judge.  Keep in mind that this ID needs to be entered on the judging forms.  There must be at least 1 judge for each category.</p>");
+
+    out.println("<table border='1'><tr><th>ID</th><th>Category</th><th>Division</th></tr>");
+
+    int row = 0; // keep track of which row we're generating
+
+    if(null == request.getParameter("cat0")) {
+      // this is the first time the page has been visited so we need to read
+      // the judges out of the DB
+      ResultSet rs = null;
+      Statement stmt = null;
+      try {
+        stmt = connection.createStatement();
+        rs = stmt.executeQuery("SELECT id, category, event_division FROM Judges WHERE Tournament = '" + tournament + "'");
+        for(row = 0; rs.next(); row++) {
+          final String id = rs.getString(1);
+          final String category = rs.getString(2);
+          final String division = rs.getString(3);
           generateRow(out, subjectiveCategories, divisions, row, id, category, division);
-          
-          row++;
-          id = request.getParameter("id" + row);
-          category= request.getParameter("cat" + row);
-          division = request.getParameter("div" + row);
         }
+      } finally {
+        Utilities.closeResultSet(rs);
+        Utilities.closeStatement(stmt);
       }
+    } else {
+      // need to walk the parameters to see what we've been passed
+      String id = request.getParameter("id" + row);
+      String category = request.getParameter("cat" + row);
+      String division = request.getParameter("div" + row);
+      while(null != category) {
+        generateRow(out, subjectiveCategories, divisions, row, id, category, division);
 
-      //if there aren't enough judges in the database for each category,
-      //generate some more
-      final int tableRows = Math.max(Math.max(numRows, subjectiveCategories.getLength()),
-                                     row);
-      
-      for(; row < tableRows; row++) {
-        generateRow(out, subjectiveCategories, divisions, row, null, null, null);
+        row++;
+        id = request.getParameter("id" + row);
+        category = request.getParameter("cat" + row);
+        division = request.getParameter("div" + row);
       }
-
-      out.println("</table>");
-      out.println("<input type='submit' name='submit' value='Add Row'>");
-      out.println("<input type='submit' name='submit' value='Finished'>");
     }
-    
+
+    // if there aren't enough judges in the database for each category,
+    // generate some more
+    final int tableRows = Math.max(Math.max(numRows, subjectiveCategories.getLength()), row);
+
+    for(; row < tableRows; row++) {
+      generateRow(out, subjectiveCategories, divisions, row, null, null, null);
+    }
+
+    out.println("</table>");
+    out.println("<input type='text' name='num_rows' value='1' size='10'/>");
+    out.println("<input type='submit' name='add_rows' value='Add Rows'/><br/>");
+    out.println("<input type='submit' name='finished' value='Finished'/><br/>");
+
     out.println("</form>");
   }
-
 
   /**
    * Generate a row in the judges table defaulting the form elemenets to the
    * given information.
-   *
-   * @param out where to print
-   * @param subjectiveCategories the possible categroies
-   * @param divisions List of divisions as Strings, "All" is always the first
-   * element in the list
-   * @param row the row being generated, used for naming form elements
-   * @param id id of judge, can be null
-   * @param cat category of judge, can be null
-   * @param division division judge is scoring, can be null
+   * 
+   * @param out
+   *          where to print
+   * @param subjectiveCategories
+   *          the possible categroies
+   * @param divisions
+   *          List of divisions as Strings, "All" is always the first element in
+   *          the list
+   * @param row
+   *          the row being generated, used for naming form elements
+   * @param id
+   *          id of judge, can be null
+   * @param cat
+   *          category of judge, can be null
+   * @param division
+   *          division judge is scoring, can be null
    */
   private static void generateRow(final JspWriter out,
                                   final NodeList subjectiveCategories,
@@ -162,17 +184,16 @@ public final class Judges {
                                   final int row,
                                   final String id,
                                   final String cat,
-                                  final String division)
-    throws IOException {
+                                  final String division) throws IOException {
     out.println("<tr>");
     out.print("  <td><input type='text' name='id" + row + "'");
     if(null != id) {
       out.print(" value='" + id + "'");
     }
     out.println("></td>");
-    
+
     out.println("  <td><select name='cat" + row + "'>");
-    for(int choice=0; choice < subjectiveCategories.getLength(); choice++) {
+    for(int choice = 0; choice < subjectiveCategories.getLength(); choice++) {
       final Element category = (Element)subjectiveCategories.item(choice);
       final String categoryName = category.getAttribute("name");
       out.print("  <option value='" + categoryName + "'");
@@ -192,38 +213,37 @@ public final class Judges {
       out.println(">" + div + "</option>");
     }
     out.println("  </select></td>");
-    
+
     out.println("</tr>");
   }
 
   /**
-   * Validate the list of judges in request and if ok generate a final table
-   * for the user to check along with buttons for submit and cancel.  If
-   * something is wrong, return a useful message.
-   *
+   * Validate the list of judges in request and if ok generate a final table for
+   * the user to check along with buttons for submit and cancel. If something is
+   * wrong, return a useful message.
+   * 
    * @return null if everything is ok, otherwise the error message
    */
-  private static String generateVerifyTable(final JspWriter out,
-                                            final NodeList subjectiveCategories,
-                                            final HttpServletRequest request) throws IOException, ParseException {
-    //keep track of any errors
+  private static String generateVerifyTable(final JspWriter out, final NodeList subjectiveCategories, final HttpServletRequest request)
+      throws IOException, ParseException {
+    // keep track of any errors
     final StringBuffer error = new StringBuffer();
 
     // keep track of which categories have judges
     final Map<String, Set<String>> hash = new HashMap<String, Set<String>>();
-    
-    //populate a hash where key is category name and value is an empty
-    //Set.  Use set so there are no duplicates
-    for(int i=0; i<subjectiveCategories.getLength(); i++) {
+
+    // populate a hash where key is category name and value is an empty
+    // Set. Use set so there are no duplicates
+    for(int i = 0; i < subjectiveCategories.getLength(); i++) {
       final String categoryName = ((Element)subjectiveCategories.item(i)).getAttribute("name");
       hash.put(categoryName, new HashSet<String>());
     }
-    
-    //walk request and push judge id into the Set, if not null or empty,
-    //in the value for each category in the hash.
-    int row=0;
+
+    // walk request and push judge id into the Set, if not null or empty,
+    // in the value for each category in the hash.
+    int row = 0;
     String id = request.getParameter("id" + row);
-    String category= request.getParameter("cat" + row);
+    String category = request.getParameter("cat" + row);
     while(null != category) {
       if(null != id) {
         id = id.trim();
@@ -233,34 +253,32 @@ public final class Judges {
           set.add(id);
         }
       }
-      
+
       row++;
       id = request.getParameter("id" + row);
-      category= request.getParameter("cat" + row);
+      category = request.getParameter("cat" + row);
     }
 
-    
-    //now walk the keys of the hash and make sure that all values have a list
-    //of size > 0, otherwise append an error to error.
+    // now walk the keys of the hash and make sure that all values have a list
+    // of size > 0, otherwise append an error to error.
     for(String categoryName : hash.keySet()) {
       final Set<String> set = hash.get(categoryName);
       if(set.isEmpty()) {
         error.append("You must specify at least one judge for " + categoryName + "<br>");
       }
     }
-    
+
     if(error.length() > 0) {
       return error.toString();
     } else {
       out.println("<p>If everything looks ok, click Commit, otherwise click Cancel and you'll go back to the edit page.</p>");
-      //generate final table with submit button
+      // generate final table with submit button
       out.println("<table border='1'><tr><th>ID</th><th>Category</th><th>Division</th></tr>");
 
-
-      //walk request and put data in a table
-      row=0;
+      // walk request and put data in a table
+      row = 0;
       id = request.getParameter("id" + row);
-      category= request.getParameter("cat" + row);
+      category = request.getParameter("cat" + row);
       String division = request.getParameter("div" + row);
       while(null != category) {
         if(null != id && division != null) {
@@ -272,21 +290,20 @@ public final class Judges {
             out.println("  <td>" + category + " <input type='hidden' name='cat" + row + "' value='" + category + "'></td>");
             out.println("  <td>" + division + " <input type='hidden' name='div" + row + "' value='" + division + "'></td>");
             out.println("</tr>");
-          
+
           }
         }
-      
+
         row++;
         id = request.getParameter("id" + row);
-        category= request.getParameter("cat" + row);
-        division= request.getParameter("div" + row);
+        category = request.getParameter("cat" + row);
+        division = request.getParameter("div" + row);
       }
 
-      
       out.println("</table>");
-      out.println("<input type='submit' name='submit' value='Commit'>");
-      out.println("<input type='submit' name='submit' value='Cancel'>");
-      
+      out.println("<input type='submit' name='commit' value='Commit'>");
+      out.println("<input type='submit' name='cancel' value='Cancel'>");
+
       return null;
     }
   }
@@ -294,48 +311,48 @@ public final class Judges {
   /**
    * Commit the subjective data from request to the database and redirect
    * response back to index.jsp.
-   *
-   * @param tournament the current tournament
+   * 
+   * @param tournament
+   *          the current tournament
    */
   private static void commitData(final NodeList subjectiveCategories,
                                  final HttpServletRequest request,
                                  final HttpServletResponse response,
                                  final Connection connection,
-                                 final String tournament)
-    throws SQLException, IOException {
+                                 final String tournament) throws SQLException, IOException {
     Statement stmt = null;
     PreparedStatement prep = null;
     try {
       stmt = connection.createStatement();
 
-      //delete old data in judges
+      // delete old data in judges
       stmt.executeUpdate("DELETE FROM Judges where Tournament = '" + tournament + "'");
-      
-      //walk request parameters and insert data into database
+
+      // walk request parameters and insert data into database
       prep = connection.prepareStatement("INSERT INTO Judges (id, category, event_division, Tournament) VALUES(?, ?, ?, ?)");
       prep.setString(4, tournament);
       int row = 0;
       String id = request.getParameter("id" + row);
-      String category= request.getParameter("cat" + row);
-      String division= request.getParameter("div" + row);
+      String category = request.getParameter("cat" + row);
+      String division = request.getParameter("div" + row);
       while(null != category) {
         prep.setString(1, id);
         prep.setString(2, category);
         prep.setString(3, division);
         prep.executeUpdate();
-          
+
         row++;
         id = request.getParameter("id" + row);
         category = request.getParameter("cat" + row);
         division = request.getParameter("div" + row);
       }
-      
+
     } finally {
       Utilities.closeStatement(stmt);
       Utilities.closePreparedStatement(prep);
     }
-    
-    //finally redirect to index.jsp 
+
+    // finally redirect to index.jsp
     response.sendRedirect(response.encodeRedirectURL("index.jsp?message=Successfully+assigned+judges."));
   }
 
