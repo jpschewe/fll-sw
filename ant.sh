6714266a0e048526cc44ab3eb299b1535d34e10d
Jon Schewe <jpschewe@users.sourceforge.net>
2009-04-06 03:06:38 +0000

    Merged revisions 873,876-878 via svnmerge from https://fll-sw.svn.sourceforge.net/svnroot/fll-sw/branches/jpschewe
    ........
      r873 | jpschewe | 2009-04-05 14:17:01 -0500 (Sun, 05 Apr 2009) | 1 line
      
      Switched to using a vendor branch for tomcat.
    ........
      r876 | jpschewe | 2009-04-05 21:26:39 -0500 (Sun, 05 Apr 2009) | 3 lines
      
      Pulling tomcat 6.0.8 into development tree.
    ........
      r877 | jpschewe | 2009-04-05 21:36:41 -0500 (Sun, 05 Apr 2009) | 1 line
      
      Upgraded to tomcat 6.0.18.
    ........
      r878 | jpschewe | 2009-04-05 21:55:47 -0500 (Sun, 05 Apr 2009) | 1 line
      
      Fixed jsp compilation again. Really it works now...
    ........

--- ant.sh
+++ ant.sh
@@ -1,1 +1,3 @@
-ant --noconfig -lib lib -lib lib/ant -lib lib/test $*
+# commented out as it's providing some bad JSP libraries
+#ant --noconfig -lib lib -lib lib/ant -lib lib/test $*
+ant --noconfig $*
