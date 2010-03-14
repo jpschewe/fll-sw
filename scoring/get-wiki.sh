97d6d1fffc9b4c3c490f00e29ae2bd478cf14c6d
Jon Schewe <jpschewe@mtu.net>
2010-03-13 21:10:21 -0600

    Filenames of downloaded wiki are now legal on windows as well as Linux

--- scoring/get-wiki.sh
+++ scoring/get-wiki.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 mkdir -p build/tomcat/webapps/fll-sw/wiki
-wget -P web/wiki -nH --cut-dirs=3 -k -I /apps/trac/fll-sw/wiki --mirror http://sourceforge.net/apps/trac/fll-sw/wiki 
+
+
+wget \
+  -I /apps/trac/fll-sw/wiki \
+  --page-requisites \
+  --html-extension \
+  --restrict-file-names=windows \
+  -P web/wiki \
+  -nH \
+  --cut-dirs=3 \
+  -k \
+  --mirror \
+  http://sourceforge.net/apps/trac/fll-sw/wiki 
