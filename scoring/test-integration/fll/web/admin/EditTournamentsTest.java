dede30f36847d6669d22f975d46cb73ce8554298
Jon Schewe <jpschewe@mtu.net>
2010-03-17 21:28:37 -0500

    Factored out test utils

--- scoring/test-integration/fll/web/admin/EditTournamentsTest.java
+++ scoring/test-integration/fll/web/admin/EditTournamentsTest.java
@@ -17,6 +17,7 @@ import com.thoughtworks.selenium.SeleneseTestCase;
 
 import fll.db.GenerateDB;
 import fll.web.InitializeDatabaseTest;
+import fll.web.IntegrationTestUtils;
 
 /**
  * Test editing the tournaments list 
@@ -33,20 +34,23 @@ public class EditTournamentsTest extends SeleneseTestCase {
     selenium.open("http://localhost:9080/fll-sw/setup/");
     
     final InputStream challengeStream = InitializeDatabaseTest.class.getResourceAsStream("data/challenge-ft.xml");
-    InitializeDatabaseTest.initializeDatabase(selenium, challengeStream, true);
+    IntegrationTestUtils.initializeDatabase(selenium, challengeStream, true);
     
     selenium.click("Admin Index");
-    selenium.waitForPageToLoad("60000");
+    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
 
     selenium.click("Edit Tournaments");
-    selenium.waitForPageToLoad("60000");
+    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
     
     Assert.assertFalse("Should not have internal tournament listed", selenium.isTextPresent(GenerateDB.INTERNAL_TOURNAMENT_NAME));
     
     selenium.click("Add Row");
-    selenium.waitForPageToLoad("60000");
+    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
 
+    // type in tournament name 
 //    selenium.
+    selenium.click("Finished");
+    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
 
   }
 
