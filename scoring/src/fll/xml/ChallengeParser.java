3a68c8b7f0eb08e1f9bf4382ef8e2eaeda71e6f0
Jon Schewe <jpschewe@mtu.net>
2010-05-09 14:23:44 -0500

    Testing for missing goals works.
    Moved the descriptor structure checks into ChallengeParser.

--- scoring/src/fll/xml/ChallengeParser.java
+++ scoring/src/fll/xml/ChallengeParser.java
@@ -35,6 +35,7 @@ import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 
 import fll.Utilities;
+import fll.db.GenerateDB;
 import fll.util.FP;
 
 /**
@@ -286,4 +287,66 @@ public final class ChallengeParser {
       } // end if child node (performance or subjective)
     } // end foreach child node
   } // end validateDocument
+
+
+  /**
+   * If the new document
+   * differs from the current document in a way that the database structure will be
+   * modified.
+   * 
+   * @param curDoc the current document
+   * @param newDoc the document to check against
+   * @return null if everything checks out OK, otherwise the error message
+   */
+  public static String compareStructure(final Document curDoc,
+                                       final Document newDoc)  {
+    final Element curDocRoot = curDoc.getDocumentElement();
+    final Element newDocRoot = newDoc.getDocumentElement();
+    final Element curPerfElement = (Element) curDocRoot.getElementsByTagName("Performance").item(0);
+    final Element newPerfElement = (Element) newDocRoot.getElementsByTagName("Performance").item(0);
+
+    final Map<String, String> curPerGoals = gatherColumnDefinitions(curPerfElement);
+    final Map<String, String> newPerGoals = gatherColumnDefinitions(newPerfElement);
+    if (curPerGoals.size() != newPerGoals.size()) {
+      return "New document has "
+          + newPerGoals.size() + " performance goals, current document has " + curPerGoals.size() + " performance goals";
+    }
+    for(final Map.Entry<String, String> curEntry : curPerGoals.entrySet()) {
+      if(!newPerGoals.containsKey(curEntry.getKey())) {
+        return "New document is missing goal '" + curEntry.getKey() +"'";
+      }
+      final String curDef = curEntry.getValue();
+      final String newDef = newPerGoals.get(curEntry.getKey());
+      if(!curDef.equals(newDef)) {
+        return "Database definition for goal '" + curEntry.getKey() + "' is different in the new document from the current document"
+        + " '" + curDef + "' vs '" + newDef + "'";
+      }
+    }
+    
+//    final List<Element> curSubCats = XMLUtils.filterToElements(curRootElement.getElementsByTagName("subjectiveCategory"));
+//    final List<Element> newSubCats = XMLUtils.filterToElements(newRootElement.getElementsByTagName("subjectiveCategory"));
+//    if(curSubCats.size() != newSubCats.size()) {
+//      return "New document has "
+//                  + newSubCats.size() + " subjective categories, current document has " + curSubCats.size() + " subjective categories";
+//    }
+    
+    return null;
+  }
+
+  /**
+   * Get the column definitions for all goals in the specified element
+   */
+  private static Map<String, String> gatherColumnDefinitions(final Element element) {
+    final Map<String, String> goalDefs = new HashMap<String, String>();
+    
+    for (final Element goal : XMLUtils.filterToElements(element.getElementsByTagName("goal"))) {
+      final String columnDefinition = GenerateDB.generateGoalColumnDefinition(goal);
+      final String goalName = goal.getAttribute("name");
+      goalDefs.put(goalName, columnDefinition);
+    }
+
+    return goalDefs;
+  }
+
+
 }
