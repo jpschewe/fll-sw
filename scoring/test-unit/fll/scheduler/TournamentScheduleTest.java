9cbd96a14835967f8886256a93705c7af8335b5a
Jon Schewe <jpschewe@mtu.net>
2010-08-18 21:38:44 -0400

    Remove extra import

--- scoring/test-unit/fll/scheduler/TournamentScheduleTest.java
+++ scoring/test-unit/fll/scheduler/TournamentScheduleTest.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
