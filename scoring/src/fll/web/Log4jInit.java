162bb6e44345bde2f3cbe494b45c6ecad7276692
Jon Schewe <jpschewe@mtu.net>
2010-09-25 21:35:36 -0500

    Explicitly initialize log4j
    Buffer all log messages until LogUtils.initializeLogging has been
    called.

--- scoring/src/fll/web/Log4jInit.java
+++ scoring/src/fll/web/Log4jInit.java
@@ -8,40 +8,30 @@ package fll.web;
 import javax.servlet.http.HttpServlet;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
+
+import fll.util.LogUtils;
 
 /**
  * @author jpschewe
- * 
- *  @web.servlet
- *     load-on-startup="1"
- *     name="log4j-init"
- *     
- * @web.servlet-init-param
- *     name="log4j-init-file"
- *     value="WEB-INF/classes/log4j.xml"
+ * @web.servlet load-on-startup="1" name="log4j-init"
  */
 public class Log4jInit extends HttpServlet {
+
+  private static final Logger LOGGER = LogUtils.getLogger();
 
   @Override
   public void init() {
     final String prefix = getServletContext().getRealPath("/");
-    final String file = getInitParameter("log4j-init-file");
 
-    // if the log4j-init-file is not set, then no point in trying
-    if (file != null) {
+    // set some properties that are used in the log4j config file
+    System.setProperty("app.name", "web");
+    System.setProperty("logroot", prefix
+        + "/"); // need the trailing slash because of how the log4j.xml file
+    // is built
 
-      // set some properties that are used in the log4j config file
-      System.setProperty("app.name", "web");
-      System.setProperty("logroot", prefix
-          + "/"); // need the trailing slash because of how the log4j.xml file
-                  // is built
+    LogUtils.initializeLogging();
 
-      DOMConfigurator.configure(prefix
-          + file);
-
-      Logger.getRootLogger().info("Logging initialized");
-    }
+    LOGGER.info("Logging initialized");
   }
 
 }
