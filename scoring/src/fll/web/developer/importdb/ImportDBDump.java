6dd6fd266fc57996aef669b0507d24d92bae7cc5
Jon Schewe <jpschewe@mtu.net>
2010-12-30 17:03:59 -0600

    Check challenge descriptors on import
    This fixes ticket:10

--- scoring/src/fll/web/developer/importdb/ImportDBDump.java
+++ scoring/src/fll/web/developer/importdb/ImportDBDump.java
@@ -12,16 +12,21 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+import net.mtu.eggplant.util.sql.SQLFunctions;
+
 import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
 
 import fll.Utilities;
 import fll.db.ImportDB;
+import fll.db.Queries;
 import fll.util.LogUtils;
 import fll.web.BaseFLLServlet;
 import fll.web.SessionAttributes;
 import fll.web.UploadProcessor;
+import fll.xml.ChallengeParser;
 
 /**
  * Import a database dump into the existing database.
@@ -47,29 +52,42 @@ public class ImportDBDump extends BaseFLLServlet {
     final StringBuilder message = new StringBuilder();
 
     Utilities.loadDBDriver();
-
+    Connection destConnection = null;
     try {
       // must be first to ensure the form parameters are set
       UploadProcessor.processUpload(request);
 
       if (null != request.getAttribute("importdb")) {
 
-        final String databaseName = "dbimport" + String.valueOf(_importdbCount++); 
-        final String url = "jdbc:hsqldb:mem:" + databaseName;
+        final String databaseName = "dbimport"
+            + String.valueOf(_importdbCount++);
+        final String url = "jdbc:hsqldb:mem:"
+            + databaseName;
         // TODO should figure out how to clean up this database
         final DataSource importDataSource = Utilities.createDataSource(databaseName, url);
 
-        // let other pages know where the connection is 
+        // let other pages know where the connection is
         session.setAttribute("dbimport", importDataSource);
 
-        final Connection memConnection = importDataSource.getConnection();        
+        final Connection memConnection = importDataSource.getConnection();
 
         // import the database
         final FileItem dumpFileItem = (FileItem) request.getAttribute("dbdump");
         final ZipInputStream zipfile = new ZipInputStream(dumpFileItem.getInputStream());
         ImportDB.loadDatabaseDump(zipfile, memConnection);
 
-        session.setAttribute(SessionAttributes.REDIRECT_URL, "selectTournament.jsp");
+        final DataSource destDataSource = SessionAttributes.getDataSource(session);
+        destConnection = destDataSource.getConnection();
+        final String docMessage = checkChallengeDescriptors(memConnection, destConnection);
+        if (null == docMessage) {
+          session.setAttribute(SessionAttributes.REDIRECT_URL, "selectTournament.jsp");
+        } else {
+          message.append("<p class='error'>");
+          message.append("Import failed: Challenge descriptors are incompatible. ");
+          message.append(docMessage);
+          message.append("</p>");
+          session.setAttribute(SessionAttributes.REDIRECT_URL, "../index.jsp");
+        }
       } else {
         message.append("<p class='error'>Unknown form state, expected form fields not seen: "
             + request + "</p>");
@@ -80,10 +98,25 @@ public class ImportDBDump extends BaseFLLServlet {
     } catch (final SQLException sqle) {
       LOG.error(sqle, sqle);
       throw new RuntimeException("Error loading data into the database", sqle);
+    } finally {
+      SQLFunctions.close(destConnection);
     }
 
     session.setAttribute(SessionAttributes.MESSAGE, message.toString());
-    response.sendRedirect(response.encodeRedirectURL(SessionAttributes.getRedirectURL(session)));    
+    response.sendRedirect(response.encodeRedirectURL(SessionAttributes.getRedirectURL(session)));
+  }
+
+  /**
+   * @param memConnection
+   * @param destConnection
+   * @return message to the user if there are errors, null if everything is OK
+   */
+  private String checkChallengeDescriptors(final Connection sourceConnection,
+                                           final Connection destConnection) throws SQLException {
+    final Document sourceDoc = Queries.getChallengeDocument(sourceConnection);
+    final Document destDoc = Queries.getChallengeDocument(destConnection);
+    final String compareMessage = ChallengeParser.compareStructure(destDoc, sourceDoc);
+    return compareMessage;
   }
 
 }
