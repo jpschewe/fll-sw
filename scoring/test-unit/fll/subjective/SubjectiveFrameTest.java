e9b1a75e79645201b19be76b75920912abad808c
Jon Schewe <jpschewe@mtu.net>
2011-01-03 15:29:14 -0600

    Finished basic tests for subjective score entry

--- scoring/test-unit/fll/subjective/SubjectiveFrameTest.java
+++ scoring/test-unit/fll/subjective/SubjectiveFrameTest.java
@@ -6,6 +6,7 @@
 
 package fll.subjective;
 
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -20,10 +21,16 @@ import java.util.zip.ZipInputStream;
 
 import net.mtu.eggplant.xml.NodelistElementCollectionAdapter;
 
+import org.fest.swing.core.KeyPressInfo;
+import org.fest.swing.core.MouseButton;
+import org.fest.swing.core.matcher.JButtonMatcher;
+import org.fest.swing.data.TableCell;
 import org.fest.swing.edt.FailOnThreadViolationRepaintManager;
 import org.fest.swing.edt.GuiActionRunner;
 import org.fest.swing.edt.GuiQuery;
+import org.fest.swing.finder.JOptionPaneFinder;
 import org.fest.swing.fixture.FrameFixture;
+import org.fest.swing.fixture.JOptionPaneFixture;
 import org.fest.swing.fixture.JTabbedPaneFixture;
 import org.fest.swing.fixture.JTableFixture;
 import org.junit.After;
@@ -43,7 +50,7 @@ import fll.util.LogUtils;
 import fll.web.admin.DownloadSubjectiveData;
 
 /**
- * 
+ * Some basic tests for the subjective app.
  */
 public class SubjectiveFrameTest {
 
@@ -96,7 +103,6 @@ public class SubjectiveFrameTest {
 
       // create the subjective datafile
       subjectiveScores = File.createTempFile("testStartupState", ".fll");
-      subjectiveScores.deleteOnExit();
       final FileOutputStream fileStream = new FileOutputStream(subjectiveScores);
       DownloadSubjectiveData.writeSubjectiveScores(connection, document, fileStream);
       fileStream.close();
@@ -125,40 +131,46 @@ public class SubjectiveFrameTest {
   @After
   public void tearDown() {
     window.cleanUp();
-    subjectiveScores.delete();
+    if (!subjectiveScores.delete()) {
+      subjectiveScores.deleteOnExit();
+    }
     TestUtils.deleteDatabase(database);
   }
 
   @Test
   public void testStartupState() throws IOException {
     try {
-    window.show(); // shows the frame to test
+      window.show(); // shows the frame to test
 
-    final JTabbedPaneFixture tabbedPane = window.tabbedPane();
+      final JTabbedPaneFixture tabbedPane = window.tabbedPane();
 
-    final Map<String, Integer> expectedRowCounts = new HashMap<String, Integer>();
-    expectedRowCounts.put("Teamwork", 29);
-    expectedRowCounts.put("Design", 41);
-    expectedRowCounts.put("Programming", 29);
-    expectedRowCounts.put("Research Project Assessment", 58);
-    for (final Element categoryDescription : new NodelistElementCollectionAdapter(
-                                                                                  document
-                                                                                          .getDocumentElement()
-                                                                                          .getElementsByTagName(
-                                                                                                                "subjectiveCategory"))) {
-      final String title = categoryDescription.getAttribute("title");
+      final Map<String, Integer> expectedRowCounts = new HashMap<String, Integer>();
+      expectedRowCounts.put("Teamwork", 29);
+      expectedRowCounts.put("Design", 41);
+      expectedRowCounts.put("Programming", 29);
+      expectedRowCounts.put("Research Project Assessment", 58);
+      for (final Element categoryDescription : new NodelistElementCollectionAdapter(
+                                                                                    document
+                                                                                            .getDocumentElement()
+                                                                                            .getElementsByTagName(
+                                                                                                                  "subjectiveCategory"))) {
+        final String title = categoryDescription.getAttribute("title");
 
-      tabbedPane.selectTab(title);
-      final JTableFixture table = window.table();
-      if (expectedRowCounts.containsKey(title)) {
-        final int expected = expectedRowCounts.get(title);
-        Assert.assertEquals("Category "
-            + title, expected, table.rowCount());
-      } else {
-        Assert.fail("Unknow category '"
-            + title + "'");
+        tabbedPane.selectTab(title);
+        final JTableFixture table = window.table();
+        if (expectedRowCounts.containsKey(title)) {
+          final int expected = expectedRowCounts.get(title);
+          Assert.assertEquals("Category "
+              + title, expected, table.rowCount());
+        } else {
+          Assert.fail("Unknow category '"
+              + title + "'");
+        }
       }
-    }
+      
+      window.button(JButtonMatcher.withText("Quit")).click();
+      final JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().using(window.robot);
+      optionPane.button(JButtonMatcher.withText("Yes")).click();
     } catch (final AssertionError e) {
       TestUtils.saveScreenshot();
       throw e;
@@ -168,12 +180,130 @@ public class SubjectiveFrameTest {
     }
   }
 
-  // TODO create test for invalid values
+  @Test
+  public void testValidValue() throws IOException {
+    try {
+      window.show(); // shows the frame to test
 
-  // TODO make sure that the totals add up, take weights into account
+      final JTabbedPaneFixture tabbedPane = window.tabbedPane();
 
-  // TODO make sure that clicking on cell does overwrite
+      tabbedPane.selectTab("Design");
+      final JTableFixture table = window.table();
+      // find 306
+      final TableCell teamCell = table.cell("306");
+      final int colIdx = table.columnIndexFor("Structural");
+      final TableCell dataCell = TableCell.row(teamCell.row).column(colIdx);
+      table.enterValue(dataCell, "10");
+      Assert.assertEquals("10", table.valueAt(dataCell));
 
-  // TODO make sure that backspace on cell clears
+      window.button(JButtonMatcher.withText("Quit")).click();
+      final JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().using(window.robot);
+      optionPane.button(JButtonMatcher.withText("Yes")).click();
+    } catch (final AssertionError e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    } catch (final RuntimeException e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    }
+  }
 
+  @Test
+  public void testInvalidValue() throws IOException {
+    try {
+      window.show(); // shows the frame to test
+
+      final JTabbedPaneFixture tabbedPane = window.tabbedPane();
+
+      tabbedPane.selectTab("Design");
+      final JTableFixture table = window.table();
+      // find 306
+      final TableCell teamCell = table.cell("306");
+      final int colIdx = table.columnIndexFor("Structural");
+      final TableCell dataCell = TableCell.row(teamCell.row).column(colIdx);
+      final String prevValue = table.valueAt(dataCell);
+      table.enterValue(dataCell, "50");
+      Assert.assertEquals("Value should have reverted", prevValue, table.valueAt(dataCell));
+      
+      window.button(JButtonMatcher.withText("Quit")).click();
+      final JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().using(window.robot);
+      optionPane.button(JButtonMatcher.withText("Yes")).click();
+    } catch (final AssertionError e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    } catch (final RuntimeException e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    }
+  }
+
+  /**
+   * When one clicks on a cell, then the data should be overwritten.
+   */
+  @Test
+  public void testOverwriteOnSelect() throws IOException {
+    try {
+      window.show(); // shows the frame to test
+
+      final JTabbedPaneFixture tabbedPane = window.tabbedPane();
+
+      tabbedPane.selectTab("Design");
+      final JTableFixture table = window.table();
+      // find 306
+      final TableCell teamCell = table.cell("306");
+      final int colIdx = table.columnIndexFor("Structural");
+      final TableCell dataCell = TableCell.row(teamCell.row).column(colIdx);
+      table.click(dataCell, MouseButton.LEFT_BUTTON);
+      window.robot.enterText("1");
+      table.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_TAB));
+      Assert.assertEquals("Value should have been overwritten", "1", table.valueAt(dataCell));
+
+      window.button(JButtonMatcher.withText("Quit")).click();
+      final JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().using(window.robot);
+      optionPane.button(JButtonMatcher.withText("Yes")).click();
+    } catch (final AssertionError e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    } catch (final RuntimeException e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    }
+  }
+
+  /**
+   * When one clicks on a cell, then the data should be overwritten.
+   */
+  @Test
+  public void testBackspaceClears() throws IOException {
+    try {
+      window.show(); // shows the frame to test
+
+      final JTabbedPaneFixture tabbedPane = window.tabbedPane();
+
+      tabbedPane.selectTab("Design");
+      final JTableFixture table = window.table();
+      // find 306
+      final TableCell teamCell = table.cell("306");
+      for (int column = 0; column < 5; ++column) {
+        final TableCell dataCell = TableCell.row(teamCell.row).column(teamCell.column
+            + 4 + column);
+        table.click(dataCell, MouseButton.LEFT_BUTTON);
+        table.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_BACK_SPACE));
+        table.pressAndReleaseKey(KeyPressInfo.keyCode(KeyEvent.VK_TAB));
+        Assert.assertEquals("Value should have been overwritten row: "
+            + dataCell.row + " column: " + dataCell.column, "", table.valueAt(dataCell));
+      }
+
+      window.button(JButtonMatcher.withText("Quit")).click();
+      final JOptionPaneFixture optionPane = JOptionPaneFinder.findOptionPane().using(window.robot);
+      optionPane.button(JButtonMatcher.withText("Yes")).click();
+    } catch (final AssertionError e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    } catch (final RuntimeException e) {
+      TestUtils.saveScreenshot();
+      throw e;
+    }
+  }
+  
 }
