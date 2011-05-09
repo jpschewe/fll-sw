2e4200f4f8304ed9fba08bc5802a24f2f28d7537
Jon Schewe <jpschewe@mtu.net>
2011-05-08 21:26:40 -0500

    Use type-safe session attribute accessor

--- scoring/src/fll/web/DoLogin.java
+++ scoring/src/fll/web/DoLogin.java
@@ -37,7 +37,7 @@ public class DoLogin extends BaseFLLServlet {
                                 final ServletContext application,
                                 final HttpSession session) throws IOException, ServletException {
 
-    final DataSource datasource = (DataSource) session.getAttribute(SessionAttributes.DATASOURCE);
+    final DataSource datasource = SessionAttributes.getDataSource(session);
     try {
       final Connection connection = datasource.getConnection();
 
