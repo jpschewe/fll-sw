c032d21dda5f480c8f785ec48f0e60b4cedef40e
Jon Schewe <jpschewe@mtu.net>
2011-05-12 22:16:41 -0500

    Do better job of handling login cookies
    * remove multiple logins if they exist
    * remove cookies from browser

--- scoring/src/fll/web/DoLogout.java
+++ scoring/src/fll/web/DoLogout.java
@@ -9,6 +9,7 @@ package fll.web;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Collection;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -18,7 +19,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+import org.apache.log4j.Logger;
+
 import fll.db.Queries;
+import fll.util.LogUtils;
 
 /**
  * Log a user out of the application.
@@ -28,23 +32,42 @@ import fll.db.Queries;
  */
 public class DoLogout extends BaseFLLServlet {
 
+  private static final Logger LOGGER = LogUtils.getLogger();
+
   @Override
   protected void processRequest(final HttpServletRequest request,
                                 final HttpServletResponse response,
                                 final ServletContext application,
                                 final HttpSession session) throws IOException, ServletException {
-    final Cookie loginCookie = CookieUtils.findLoginCookie(request);
-    if (null != loginCookie) {
-      final DataSource datasource = SessionAttributes.getDataSource(session);
+    final Collection<Cookie> loginCookies = CookieUtils.findLoginCookie(request);
+    final DataSource datasource = SessionAttributes.getDataSource(session);
+    final String hostHeader = request.getHeader("host");
+    final int colonIndex = hostHeader.indexOf(":");
+    final String domain;
+    if (-1 != colonIndex) {
+      domain = hostHeader.substring(0, colonIndex);
+    } else {
+      domain = hostHeader;
+    }
+    LOGGER.debug("domain: "
+        + domain);
+
+    for (final Cookie loginCookie : loginCookies) {
+      Cookie delCookie = new Cookie(loginCookie.getName(), "");
+      delCookie.setMaxAge(0);
+      delCookie.setDomain(domain);
+      response.addCookie(delCookie);
+
       try {
         final Connection connection = datasource.getConnection();
         Queries.removeValidLogin(connection, loginCookie.getValue());
-
-        response.sendRedirect(response.encodeRedirectURL(request.getContextPath()));
+        LOGGER.debug("Removed cookie from DB: "
+            + loginCookie.getValue());
       } catch (final SQLException e) {
         throw new RuntimeException(e);
       }
     }
+    response.sendRedirect(response.encodeRedirectURL(request.getContextPath()));
 
   }
 
