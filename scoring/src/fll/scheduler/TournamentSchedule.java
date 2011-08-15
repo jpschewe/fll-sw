32d6f9dd71df049055bbf1fdf673bd557613c755
Jon Schewe <jpschewe@mtu.net>
2011-08-15 08:47:29 -0500

    Make it clear that the parameters are for automatic scheduling

--- scoring/src/fll/scheduler/TournamentSchedule.java
+++ scoring/src/fll/scheduler/TournamentSchedule.java
@@ -140,13 +140,13 @@ public class TournamentSchedule implements Serializable {
     }
   };
 
-  private long performanceDuration = Utilities.convertMinutesToMilliseconds(SchedParams.DEFAULT_PERFORMANCE_MINUTES);
+  private long performanceDuration = Utilities.convertMinutesToMilliseconds(AutoSchedParams.DEFAULT_PERFORMANCE_MINUTES);
 
-  private long subjectiveDuration = Utilities.convertMinutesToMilliseconds(SchedParams.DEFAULT_SUBJECTIVE_MINUTES);
+  private long subjectiveDuration = Utilities.convertMinutesToMilliseconds(AutoSchedParams.DEFAULT_SUBJECTIVE_MINUTES);
 
-  private long changetime = Utilities.convertMinutesToMilliseconds(SchedParams.DEFAULT_CHANGETIME_MINUTES);
+  private long changetime = Utilities.convertMinutesToMilliseconds(AutoSchedParams.DEFAULT_CHANGETIME_MINUTES);
 
-  private long performanceChangetime = Utilities.convertMinutesToMilliseconds(SchedParams.DEFAULT_PERFORMANCE_CHANGETIME_MINUTES);
+  private long performanceChangetime = Utilities.convertMinutesToMilliseconds(AutoSchedParams.DEFAULT_PERFORMANCE_CHANGETIME_MINUTES);
 
   private long specialPerformanceChangetime = Utilities.convertMinutesToMilliseconds(30);
 
