66a1ec1dbff9bf341b52edafcb116017fa583d51
Jon Schewe <jpschewe@mtu.net>
2011-08-17 10:26:51 -0500

    Output the schedule that was found

--- scoring/test-unit/fll/scheduler/autosched/SchedulerTest.java
+++ scoring/test-unit/fll/scheduler/autosched/SchedulerTest.java
@@ -6,21 +6,34 @@
 
 package fll.scheduler.autosched;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Calendar;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import fll.scheduler.autosched.SchedParams.SubjectiveParams;
+import fll.util.LogUtils;
 
 /**
  * Some tests for the automatic scheduler.
  */
 public class SchedulerTest {
 
+  private static final Logger LOGGER = LogUtils.getLogger();
+
+  @Before
+  public void setUp() {
+    LogUtils.initializeLogging();
+  }
+
   @Test
-  public void testBasic() {
+  public void testBasic() throws IOException {
     final List<SubjectiveParams> subjectiveParams = new LinkedList<SubjectiveParams>();
     subjectiveParams.add(new SubjectiveParams("Research", SchedParams.DEFAULT_SUBJECTIVE_MINUTES));
     final List<Integer> teams = new LinkedList<Integer>();
@@ -32,6 +45,12 @@ public class SchedulerTest {
     final Scheduler scheduler = new Scheduler(params);
     final boolean solution = scheduler.solve();
     Assert.assertTrue("Couldn't find a solution", solution);
+    
+    final StringWriter writer = new StringWriter();
+    final Calendar cal = Calendar.getInstance();
+    cal.set(1970, 0, 1, 8, 0);
+    scheduler.outputSchedule(cal.getTime(), writer);
+    LOGGER.info("Schedule: " + writer.toString());
   }
 
 }
