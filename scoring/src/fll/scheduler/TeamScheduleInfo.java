afcc70fe6403047b3d0f000928d82fd85f0bae3b
Jon Schewe <jpschewe@mtu.net>
2011-09-08 20:37:13 -0500

    Add class to represent performance times
    Tracks time, table and side

--- scoring/src/fll/scheduler/TeamScheduleInfo.java
+++ scoring/src/fll/scheduler/TeamScheduleInfo.java
@@ -240,6 +240,70 @@ public final class TeamScheduleInfo implements Serializable {
     public Date getTime() {
       return null == time ? null : new Date(time.getTime());
     }
+  }
+
+  /**
+   * Represents performance judging time.
+   */
+  public static final class PerformanceTime implements Comparable<PerformanceTime> {
+    public PerformanceTime(final Date time,
+                           final String table,
+                           final int side) {
+      this.table = table;
+      this.side = side;
+      this.time = time == null ? null : new Date(time.getTime());
+    }
+
+    private final String table;
+
+    public String getTable() {
+      return table;
+    }
+
+    private final int side;
+
+    public int getSide() {
+      return side;
+    }
+
+    private final Date time;
+
+    public Date getTime() {
+      return null == time ? null : new Date(time.getTime());
+    }
+
+    @Override
+    public int hashCode() {
+      return null == time ? 13 : time.hashCode();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+      if (this == o) {
+        return true;
+      } else if (o.getClass() == PerformanceTime.class) {
+        return 0 == compareTo((PerformanceTime) o);
+      } else {
+        return false;
+      }
+    }
+
+    public int compareTo(final PerformanceTime other) {
+      if (null != this.time) {
+        final int timeCompare = this.time.compareTo(other.time);
+        if (0 == timeCompare) {
+          return this.table.compareTo(other.table);
+        } else {
+          return timeCompare;
+        }
+      } else if (null == this.time
+          && null == other.time) {
+        return this.table.compareTo(other.table);
+      } else {
+        return -1
+            * other.compareTo(this);
+      }
+    }
 
   }
 }
