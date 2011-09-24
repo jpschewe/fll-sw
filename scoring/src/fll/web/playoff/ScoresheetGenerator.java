af5e143c6afe243ed3dfd422fc6ec2b2f617d692
Jon Schewe <jpschewe@mtu.net>
2011-09-24 14:32:48 -0500

    Add copyright information

--- scoring/src/fll/web/playoff/ScoresheetGenerator.java
+++ scoring/src/fll/web/playoff/ScoresheetGenerator.java
@@ -1,5 +1,7 @@
-/**
- * 
+/*
+ * Copyright (c) 2000-2002 INSciTE.  All rights reserved
+ * INSciTE is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
  */
 package fll.web.playoff;
 
