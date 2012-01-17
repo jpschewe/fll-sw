1728347cb1a62ba7936f487ab94f28f4981bbe3e
Jeremy Koletar <jjkoletar@gmail.com>
2012-01-16 18:06:36 -0600

    Clean up JsonBracketData to take in a map, and move bracket ajax query to a servlet.

---
+++ scoring/src/fll/web/playoff/AJAXBracketQueryServlet.java
@@ -0,0 +1,137 @@
+package fll.web.playoff;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import org.w3c.dom.Element;
+
+import fll.db.Queries;
+import fll.web.ApplicationAttributes;
+import fll.web.BaseFLLServlet;
+import fll.web.SessionAttributes;
+
+/**
+ * Talk to client brackets in json.
+ * 
+ * @web.servlet name="AJAXBracketQuery"
+ * @web.servlet-mapping url-pattern="/ajax/BracketQuery"
+ */
+public class AJAXBracketQueryServlet extends BaseFLLServlet {
+  protected void processRequest(final HttpServletRequest request,
+                                final HttpServletResponse response,
+                                final ServletContext application,
+                                final HttpSession session) throws IOException, ServletException {
+    final DataSource datasource = SessionAttributes.getDataSource(session);
+    try {
+      final Connection connection = datasource.getConnection();
+      final ServletOutputStream os = response.getOutputStream();
+      if (request.getParameter("round") != null && request.getParameter("row") != null) {
+          final String displayName = (String) session.getAttribute("displayName");
+          final String division;
+          final int playoffRoundNumber;
+          if (session.getAttribute(displayName+"_"+"playoffDivision") == null) {
+            //No special session attribute stuff
+            division = (String) session.getAttribute("playoffDivision");
+            playoffRoundNumber = ((Number) session.getAttribute("playoffRoundNumber")).intValue();
+          } else {
+            division = (String) session.getAttribute(displayName+"_"+"playoffDivision");
+            playoffRoundNumber = ((Number) session.getAttribute(displayName+"_"+"playoffRoundNumber")).intValue();
+          }
+          JsonBracketData jsonbd = new JsonBracketData(new BracketData(connection, division, playoffRoundNumber, playoffRoundNumber + 2, 4, false, true));
+          response.reset();
+          response.setContentType("text/plain");
+          os.print(jsonbd.getBracketLocationJson(Integer.parseInt(request.getParameter("round")), Integer.parseInt(request.getParameter("row"))));
+      } else if (request.getParameter("multi") != null) {
+        //Make row-round map
+        String[] pairs = request.getParameter("multi").split("\\|");
+        Map<Integer, Integer> pairedMap = new HashMap<Integer, Integer>();
+        for (String pair : pairs) {
+          pairedMap.put(Integer.parseInt(pair.split("\\-")[0]), Integer.parseInt(pair.split("\\-")[1]));
+        }
+        //JsonBD that request!
+        
+        //screw it steal from remoteControlbrackets.jsp
+        final String divisionKey = "playoffDivision";
+        final String roundNumberKey = "playoffRoundNumber";
+        final String displayName = (String)session.getAttribute("displayName");
+
+        final String sessionDivision;
+        final Number sessionRoundNumber;
+        if (null != displayName) {
+          sessionDivision = (String) application.getAttribute(displayName
+              + "_" + divisionKey);
+          sessionRoundNumber = (Number) application.getAttribute(displayName
+              + "_" + roundNumberKey);
+        } else {
+          sessionDivision = null;
+          sessionRoundNumber = null;
+        }
+
+        final String division;
+        if (null != sessionDivision) {
+          division = sessionDivision;
+        } else if (null == application.getAttribute(divisionKey)) {
+          final List<String> divisions = Queries.getEventDivisions(connection);
+          if (!divisions.isEmpty()) {
+            division = divisions.get(0);
+          } else {
+            throw new RuntimeException("No division specified and no divisions in the database!");
+          }
+        } else {
+          division = (String) application.getAttribute(divisionKey);
+        }
+
+        final int playoffRoundNumber;
+        if (null != sessionRoundNumber) {
+          playoffRoundNumber = sessionRoundNumber.intValue();
+        } else if (null == application.getAttribute(roundNumberKey)) {
+          playoffRoundNumber = 1;
+        } else {
+          playoffRoundNumber = ((Number) application.getAttribute(roundNumberKey)).intValue();
+        }
+        //done screwing it
+        
+       // final String displayName = (String) session.getAttribute("displayName");
+       // final boolean hasDisplayName = displayName != null;
+      //  final String division;
+       // final Number nPlayoffRoundNumber;
+       // final int playoffRoundNumber;
+       // if (!hasDisplayName) {
+       //   //No special session attribute stuff
+       //   division = (String) application.getAttribute("playoffDivision");
+       //   nPlayoffRoundNumber = (Number) application.getAttribute("playoffRoundNumber");
+       // } else {
+       //   division = (String) application.getAttribute(displayName+"_playoffDivision");
+       //   nPlayoffRoundNumber = (Number) application.getAttribute(displayName + "_playoffRoundNumber");
+       // }
+        //playoffRoundNumber = nPlayoffRoundNumber.intValue();
+        //playoffRoundNumber = 1;
+        JsonBracketData jsonbd = new JsonBracketData(new BracketData(connection, division, playoffRoundNumber, playoffRoundNumber + 2, 4, false, true));
+        final Element rootElement = ApplicationAttributes.getChallengeDocument(application).getDocumentElement();
+        final Element perfElement = (Element) rootElement.getElementsByTagName("Performance").item(0);  
+        response.reset();
+        response.setContentType("text/plain");
+        os.print(jsonbd.getMultipleBracketLocationsJson(pairedMap, datasource, perfElement));
+      } else {
+        os.print( "{\"_rmsg\": \"Error: No Params\"}");
+      }
+    } catch (final SQLException e) {
+      throw new RuntimeException(e);
+    } catch (final ParseException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
--- scoring/src/fll/web/playoff/JsonBracketData.java
+++ scoring/src/fll/web/playoff/JsonBracketData.java
@@ -3,6 +3,7 @@ package fll.web.playoff;
 import java.util.List;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -14,10 +15,8 @@ import org.apache.log4j.Logger;
 import org.w3c.dom.Element;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import fll.db.Queries;
-import fll.Team;
 import fll.util.LogUtils;
 import fll.web.playoff.BracketData;
 import fll.web.playoff.BracketData.BracketDataType;
@@ -32,113 +31,118 @@ import fll.util.ScoreUtils;
  * 
  * @author jjkoletar
  */
- 
- public class JsonBracketData {
- 
-    private static final Logger LOG = LogUtils.getLogger();
-   
-    private BracketData _bracketdata;
-    
-    private Gson _gson;
-    
-    private Boolean _showFinalsScores = false;
-    
-    private Boolean _showOnlyVerifiedScores = true;
-   
-    private BracketData getBracketData() {
-        return _bracketdata;
+
+public class JsonBracketData {
+
+  private static final Logger LOG = LogUtils.getLogger();
+
+  private BracketData _bracketdata;
+
+  private Gson _gson;
+
+  private Boolean _showFinalsScores = false;
+
+  private Boolean _showOnlyVerifiedScores = true;
+
+  private BracketData getBracketData() {
+    return _bracketdata;
+  }
+
+  public static class ForceJsRefresh {
+    public Boolean refresh = true;
+  }
+
+  public static class BracketLeafResultSet {
+    public BracketDataType leaf;
+
+    public Double score;
+
+    public String originator;
+
+    public BracketLeafResultSet (final BracketDataType bdt, final Double scr, final String origin) {
+      leaf = bdt;
+      score = scr;
+      originator = origin;
     }
-    
-    public static class ForceJsRefresh {
-        public Boolean refresh = true;
+    public BracketLeafResultSet () { //Null constructor to have some sort of item in null lists
+      score = -1.0;
     }
-    
-    public static class BracketLeafResultSet {
-        public BracketDataType leaf;
-        
-        public Double score;
-        
-        public String originator;
-        
-        public BracketLeafResultSet (final BracketDataType bdt, final Double scr, final String origin) {
-            leaf = bdt;
-            score = scr;
-            originator = origin;
-        }
-        public BracketLeafResultSet () { //Null constructor to have some sort of item in null lists
-            score = -1.0;
-        }
+  }
+
+
+  /**
+   * Constructor taking in all we need, a BracketData instance to work with.
+   */
+  public JsonBracketData (final BracketData bd) {
+    _bracketdata = bd;
+    _gson = new Gson();
+  }
+
+  /**
+   * Constructor taking two extra booleans that govern output.
+   */
+  public JsonBracketData(final BracketData bd, final Boolean showFinals, final Boolean showOnlyVerified) {
+    this(bd);
+    _showFinalsScores = showFinals;
+    _showOnlyVerifiedScores = showOnlyVerified;
+  }
+
+
+  public String getBracketLocationJson(final int round, final int row) {
+    if (round < this.getBracketData().getFirstRound())
+    {
+      //Our instance of bracket data doesn't have that round, we better tell the javascript to get its act together.
+      return _gson.toJson(new ForceJsRefresh());
     }
-    
-    
-    /**
-     * Constructor taking in all we need, a BracketData instance to work with.
-     */
-    public JsonBracketData (final BracketData bd) {
-        _bracketdata = bd;
-        _gson = new Gson();
-    }
-    
-    /**
-     * Constructor taking two extra booleans that govern output.
-     */
-    public JsonBracketData(final BracketData bd, final Boolean showFinals, final Boolean showOnlyVerified) {
-        this(bd);
-        _showFinalsScores = showFinals;
-        _showOnlyVerifiedScores = showOnlyVerified;
-    }
-        
-    
-    public String getBracketLocationJson(final int round, final int row) {
-        if (round < this.getBracketData().getFirstRound())
-        {
-            //Our instance of bracket data doesn't have that round, we better tell the javascript to get its act together.
-            return _gson.toJson(new ForceJsRefresh());
-        }
-        return _gson.toJson(this.getBracketData().getData(round, row));
-    }
+    return _gson.toJson(this.getBracketData().getData(round, row));
+  }
   /**
    * Constructs a bracket data object with playoff data from the database.
    * 
-   * @param ids A list of strings consisting of row, a dash (-) symbol, then round.
+   * @param ids A map of key round and value row
    */
-    public String getMultipleBracketLocationsJson(final List<String> ids, DataSource datasource, final Element performanceElement) throws SQLException, ParseException {
-        List<BracketLeafResultSet> datalist = new LinkedList<BracketLeafResultSet>();
-        try {
-            final Connection connection = datasource.getConnection();
-            final int currentTournament = Queries.getCurrentTournament(connection);
-            for (Iterator<String> i = ids.iterator(); i.hasNext(); ) {
-                String item = i.next();
-                String[] params = item.split("-");
-                final BracketDataType bdt = this.getBracketData().getData(Integer.parseInt(params[1]), Integer.parseInt(params[0]));
-                final TeamScore teamScore = new DatabaseTeamScore(performanceElement, currentTournament, ((TeamBracketCell) bdt).getTeam().getTeamNumber(), Queries.getNumSeedingRounds(connection, currentTournament)+Integer.parseInt(params[1]), connection);
-                if (this.getBracketData().getData(Integer.parseInt(params[1]), Integer.parseInt(params[0])) != null && Integer.parseInt(params[1])-1 != Queries.getNumPlayoffRounds(connection, ((TeamBracketCell) bdt).getTeam().getDivision())) {
-                    if (Queries.isNoShow(connection, currentTournament, ((TeamBracketCell) bdt).getTeam().getTeamNumber(), Queries.getNumSeedingRounds(connection, currentTournament)+Integer.parseInt(params[1]))) {
-                        datalist.add(new BracketLeafResultSet(bdt, -2.0, item));
-                    } else if (Double.isNaN(ScoreUtils.computeTotalScore(teamScore)) /*NaN check here is to prevent isVerified from asking about team -1*/
-                        || !_showOnlyVerifiedScores 
-                        || Queries.isVerified(connection, currentTournament, ((TeamBracketCell) bdt).getTeam().getTeamNumber(), Queries.getNumSeedingRounds(connection, currentTournament)+Integer.parseInt(params[1]))) {
-                        if (Integer.parseInt(params[1]) == Queries.getNumPlayoffRounds(connection, ((TeamBracketCell) bdt).getTeam().getDivision()) /* If score of final round*/|| Double.isNaN(ScoreUtils.computeTotalScore(teamScore))) {
-                            datalist.add(new BracketLeafResultSet(bdt, -1.0, item));
-                        } else {
-                            datalist.add(new BracketLeafResultSet(bdt, ScoreUtils.computeTotalScore(teamScore), item));
-                        }
-                    } else {
-                        datalist.add(new BracketLeafResultSet(bdt, -1.0, item));
-                    }
-                }
+  public String getMultipleBracketLocationsJson(final Map<Integer, Integer> ids, DataSource datasource, final Element performanceElement) throws SQLException, ParseException {
+    List<BracketLeafResultSet> datalist = new LinkedList<BracketLeafResultSet>();
+    try {
+      final Connection connection = datasource.getConnection();
+      final int currentTournament = Queries.getCurrentTournament(connection);
+      for (Map.Entry<Integer, Integer> entry : ids.entrySet()) {
+        final TeamBracketCell tbc = (TeamBracketCell) this.getBracketData().getData(entry.getValue(), entry.getKey());
+        if (tbc == null) {
+          throw new RuntimeException("Unable to retrieve data for identifier "+entry.getKey()+"-"+entry.getValue());
+        }
+        final int numSeedingRounds = Queries.getNumSeedingRounds(connection, currentTournament);
+        final int numPlayoffRounds = Queries.getNumPlayoffRounds(connection/*, tbc.getTeam().getDivision()*/); //Should pass division, but code was getting angry
+        final int teamNumber = tbc.getTeam().getTeamNumber();
+        final TeamScore teamScore = new DatabaseTeamScore(performanceElement, currentTournament, teamNumber, numSeedingRounds+entry.getValue(), connection);
+        final double computedTeamScore = ScoreUtils.computeTotalScore(teamScore);
+        final boolean realScore = !Double.isNaN(computedTeamScore);
+        final boolean noShow = Queries.isNoShow(connection, currentTournament, tbc.getTeam().getTeamNumber(), numSeedingRounds+entry.getValue());
+        //Sane request checks
+        if (tbc != null) {
+          if (noShow) {
+            datalist.add(new BracketLeafResultSet(tbc, -2.0, entry.getKey()+"-"+entry.getValue()));
+          } else if (!realScore
+              || !_showOnlyVerifiedScores
+              || Queries.isVerified(connection, currentTournament, teamNumber, Queries.getNumSeedingRounds(connection, currentTournament)+entry.getValue())) {
+            if (entry.getValue() == numPlayoffRounds || !realScore) {
+              datalist.add(new BracketLeafResultSet(tbc, -1.0, entry.getKey()+"-"+entry.getValue()));
+            } else {
+              datalist.add(new BracketLeafResultSet(tbc, computedTeamScore, entry.getKey()+"-"+entry.getValue()));
             }
-        } catch (final SQLException e) {
-            throw new RuntimeException(e);
-        } catch (final ParseException e) {
-            throw new RuntimeException(e);
+          }
         }
-        if (datalist.size() == 0) {
-            //Add some data, so gson's happy.
-            datalist.add(new BracketLeafResultSet());
-        }
-        return _gson.toJson(datalist);
+      }
+    } catch (final SQLException e) {
+      throw new RuntimeException(e);
+    } catch (final ParseException e) {
+      throw new RuntimeException(e);
     }
-    
+    if (datalist.size() == 0) {
+      //Add some data, so gson's happy.
+      datalist.add(new BracketLeafResultSet());
+    }
+    return _gson.toJson(datalist);
+  }
+
 }
-   
