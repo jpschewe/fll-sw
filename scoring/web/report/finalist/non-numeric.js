b847c191d11c272c41f3352588eb4f4be2748cdb
Jon Schewe <jpschewe@mtu.net>
2012-02-16 17:44:25 -0600

    Can add categories now

--- scoring/web/report/finalist/non-numeric.js
+++ scoring/web/report/finalist/non-numeric.js
@@ -8,23 +8,54 @@ $(document).ready(function() {
     //FIXME populate with current values
     
     $("#add-category").click(function() {
-	//FIXME need to add category entry here
-	//var cat_element = $("
+	var catEle = $("<li></li>");
+	$("#categories").append(catEle);
+	
+	var catIdx = $("#categories").children().size() + 1;
+	
+	var nameEle = $("<input class='category_name' type='text' id='name_" + catIdx + "'/>");
+	catEle.append(nameEle);
+	
+	var teamList = $("<ul id='category_" + catIdx + "'></ul>");
+	catEle.append(teamList);
+
+	var addButton = $("<button id='add-team_" + catIdx + "'>Add Team</button>");
+	catEle.append(addButton);
+	addButton.click(function() {
+	    addTeam(catIdx);
+	});
+
     });
 
     
 }); // end ready function
 
+/**
+ * Add a new team element to the category.
+ *
+ * @return the index for the team which can be used to populate the elements later
+ */
 function addTeam(category) {
     var catEle = $("#category_" + category);
     var teamIdx = catEle.children().size() + 1;
     
-    var teamEle = $("<li class='team_" + category + "'>" //
-		    + "<input type='text' id='num_" + category + "_" + teamIdx + "' onblur='lookup(1,2)'/>" //
-		    + "<input id='name_" + category + "_" + teamIdx + "' readonly/>" //
-		    + "<input id='org_" + category + "_" + teamIdx + "' readonly/>" //
-		    + "</li>");
+    var teamEle = $("<li class='team_" + category + "'></li>");
     catEle.append(teamEle);
+    
+    var numEle = $("<input type='text' id='num_" + category + "_" + teamIdx + "'/>");
+    teamEle.append(numEle);
+    //FIXME
+    //teamEle.focusLost(function() {
+    //    alert("Here");
+    //});
+    
+    var nameEle = $("<input id='name_" + category + "_" + teamIdx + "' readonly/>");
+    teamEle.append(nameEle);
+    var orgEle = $("<input id='org_" + category + "_" + teamIdx + "' readonly/>");
+    teamEle.append(orgEle);
+    
+    
+    return teamIdx;
 }
 
 /*
