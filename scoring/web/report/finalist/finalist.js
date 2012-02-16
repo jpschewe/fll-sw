972748c6449d6d733e1ebe9eafcd3e36b0c930d3
Jon Schewe <jpschewe@mtu.net>
2012-02-16 17:26:51 -0600

    Can add teams to categories

--- scoring/web/report/finalist/finalist.js
+++ scoring/web/report/finalist/finalist.js
@@ -1,5 +1,28 @@
-function lookup_name(num_field, name_field) {
-  var team_data = new Array();
-  team_data[1] = 'Team 1';
-  return team_data[num];
-}
+/*
+ * Copyright (c) 2000-2002 INSciTE.  All rights reserved
+ * INSciTE is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+(function($) {
+    if (!$) {
+        throw new Error("jQuery needs to be loaded!");
+    }
+    //if (!$.jStorage) {
+    //    throw new Error("jStorage needs to be loaded!");
+    //}
+
+    //var STORAGE_PREFIX = "time-tracker.";
+    
+
+    // //////////////////////// PUBLIC INTERFACE /////////////////////////
+    $.finalists = {
+	lookup_name : function(num_field, name_field) {
+	    var team_data = new Array();
+	    team_data[1] = 'Team 1';
+	    return team_data[num];
+	}
+    };
+
+    // _load();
+})(window.jQuery || window.$);
