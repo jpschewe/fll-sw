671174b55ac611a69e5122a7119cc546771e08b0
Jon Schewe <jpschewe@mtu.net>
2012-02-17 23:24:36 -0600

    formatting

--- scoring/web/report/finalist/finalist.js
+++ scoring/web/report/finalist/finalist.js
@@ -5,63 +5,63 @@
  */
 
 (function($) {
-    if (!$) {
-        throw new Error("jQuery needs to be loaded!");
-    }
-    if (!$.jStorage) {
-        throw new Error("jStorage needs to be loaded!");
-    }
-
-    var STORAGE_PREFIX = "fll.finalists";
-
-    // //////////////////////// PRIVATE METHODS ////////////////////////
-    var _teams = {};
-
-    /**
-     * Save the current state to local storage.
-     */
-    function _save() {
-        $.jStorage.set(STORAGE_PREFIX + "_teams", _teams);
-    }
-
-    /**
-     * Load the current state from local storage.
-     */
-    function _load() {
-        var value = $.jStorage.get(STORAGE_PREFIX + "_teams");
-        if (null != value) {
-            _teams = value;
-        } else {
-	    //FIXME load some test data
-	    alert("Loading test data");
-	    new Team(1, "Team 1", "Org 1");
+	if (!$) {
+		throw new Error("jQuery needs to be loaded!");
 	}
-    }
-    
-    /**
-     * Constructor for a Team.
-     */
-    function Team(num, name, org) {
-	if(typeof(_teams[num]) != 'undefined') {
-	    throw "Team already exists with number: " + num;
+	if (!$.jStorage) {
+		throw new Error("jStorage needs to be loaded!");
 	}
 
-	this.num = num;
-        this.name = name;
-        this.org = org;
-	_teams[num] = this;
-        _save();
-    }    
+	var STORAGE_PREFIX = "fll.finalists";
 
-    // //////////////////////// PUBLIC INTERFACE /////////////////////////
-    $.finalists = {
+	// //////////////////////// PRIVATE METHODS ////////////////////////
+	var _teams = {};
+
 	/**
-	 * Find a team by number.
+	 * Save the current state to local storage.
 	 */
-	lookupTeam : function(teamNum) {
-	    return _teams[teamNum];
+	function _save() {
+		$.jStorage.set(STORAGE_PREFIX + "_teams", _teams);
 	}
-    };
 
-    _load();
+	/**
+	 * Load the current state from local storage.
+	 */
+	function _load() {
+		var value = $.jStorage.get(STORAGE_PREFIX + "_teams");
+		if (null != value) {
+			_teams = value;
+		} else {
+			// FIXME load some test data
+			alert("Loading test data");
+			new Team(1, "Team 1", "Org 1");
+		}
+	}
+
+	/**
+	 * Constructor for a Team.
+	 */
+	function Team(num, name, org) {
+		if (typeof (_teams[num]) != 'undefined') {
+			throw "Team already exists with number: " + num;
+		}
+
+		this.num = num;
+		this.name = name;
+		this.org = org;
+		_teams[num] = this;
+		_save();
+	}
+
+	// //////////////////////// PUBLIC INTERFACE /////////////////////////
+	$.finalists = {
+		/**
+		 * Find a team by number.
+		 */
+		lookupTeam : function(teamNum) {
+			return _teams[teamNum];
+		}
+	};
+
+	_load();
 })(window.jQuery || window.$);
