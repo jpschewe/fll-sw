039b954e6eb0139c12520f47b61ed302d4de44e2
Jon Schewe <jpschewe@mtu.net>
2012-02-18 08:19:30 -0600

    Add category class

--- scoring/web/report/finalist/finalist.js
+++ scoring/web/report/finalist/finalist.js
@@ -16,12 +16,14 @@
 
 	// //////////////////////// PRIVATE METHODS ////////////////////////
 	var _teams = {};
+	var _categories = {};
 
 	/**
 	 * Save the current state to local storage.
 	 */
 	function _save() {
 		$.jStorage.set(STORAGE_PREFIX + "_teams", _teams);
+		$.jStorage.set(STORAGE_PREFIX + "_categories", _teams);
 	}
 
 	/**
@@ -36,6 +38,23 @@
 			alert("Loading test data");
 			new Team(1, "Team 1", "Org 1");
 		}
+		value = $.jStorage.get(STORAGE_PREFIX + "_categoires");
+		if (null != value) {
+			_categories = value;
+		}
+	}
+
+	/**
+	 * @return true if a category with the specified name exists
+	 */
+	function _check_duplicate_category(name) {
+		var duplicate = false;
+		$.each(_categories, function(i, val) {
+			if (val.name == name) {
+				duplicate = true;
+			}
+		});
+		return duplicate;
 	}
 
 	/**
@@ -53,6 +72,29 @@
 		_save();
 	}
 
+	/**
+	 * Constructor for a category. Finds the first free ID and assigns it to
+	 * this new category.
+	 */
+	function Category(name) {
+		var category_id;
+		// find the next available id
+		for (category_id = 0; category_id < Number.MAX_VALUE
+				&& _categories[category_id]; category_id = category_id + 1)
+			;
+
+		if (category_id == Number.MAX_VALUE
+				|| category_id + 1 == Number.MAX_VALUE) {
+			throw "No free category IDs";
+		}
+
+		this.name = name;
+		this.cat_id = category_id;
+		this.teams = []
+		_categories[this.cat_id] = this;
+		_save();
+	}
+
 	// //////////////////////// PUBLIC INTERFACE /////////////////////////
 	$.finalists = {
 		/**
@@ -60,7 +102,62 @@
 		 */
 		lookupTeam : function(teamNum) {
 			return _teams[teamNum];
+		},
+
+		/**
+		 * Get the categories known to the system.
+		 * 
+		 * @returns {Array} sorted by name
+		 */
+		getCategories : function() {
+			var categories = [];
+			$.each(_categories, function(i, val) {
+				categories.push(val);
+			});
+			categories.sort(function(a, b) {
+				if (a.name == b.name) {
+					return 0;
+				} else if (a.name < b.name) {
+					return -1;
+				} else {
+					return 1;
+				}
+			});
+			return categories;
+		},
+
+		/**
+		 * Get a category by id
+		 * 
+		 * @param toFind
+		 *            the id to find
+		 * @returns the category or null
+		 */
+		getCategoryById : function(toFind) {
+			var category = null;
+			$.each(_categories, function(i, val) {
+				if (val.cat_id == toFind) {
+					category = val;
+				}
+			});
+			return category;
+		},
+
+		addTeamToCategory : function(category, teamNum) {
+			category.teams.push(teamNum);
+		},
+
+		removeTeamFromCategory : function(category, teamNum) {
+			var index = category.teams.indexOf(teamNum);
+			if (index != -1) {
+				category.teams.splice(index, 1);
+			}
+		},
+
+		clearTeamsInCategory : function(category) {
+			category.teams = [];
 		}
+
 	};
 
 	_load();
