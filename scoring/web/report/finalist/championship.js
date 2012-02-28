90a78541992364eb1be09592a5539d28d6e740ce
Jon Schewe <jpschewe@mtu.net>
2012-02-28 08:27:59 -0600

    Track if the championship page has been visited
    If the page has been visited for a given division, 
    then don't initialize which teams are checked.

--- scoring/web/report/finalist/championship.js
+++ scoring/web/report/finalist/championship.js
@@ -4,10 +4,39 @@
  * This code is released under GPL; see LICENSE.txt for details.
  */
 
+function initializeTeamsInCategory(currentCategory, teams, scoreGroups) {
+	var checkedEnoughTeams = false;
+	$.each(teams, function(i, team) {
+		if (team.division == $.finalist.getCurrentDivision()) {
+			if (!checkedEnoughTeams) {
+				$.finalist.addTeamToCategory(currentCategory, team.num);
+				
+				var group = $.finalist.getOverallGroup(team);
+				scoreGroups[group] = scoreGroups[group] - 1;
+
+				checkedEnoughTeams = true;
+				$.each(scoreGroups, function(key, value) {
+					if (value > 0) {
+						checkedEnoughTeams = false;
+					}
+				});
+			}
+		}
+	});
+}
+
 $(document)
 		.ready(
 				function() {
-					$("#division").text($.finalist.getCurrentDivision());
+					var currentCategory = $.finalist
+							.getCategoryByName($.finalist.CHAMPIONSHIP_NAME);
+					var currentDivision = $.finalist.getCurrentDivision();
+					
+					var previouslyVisited = $.finalist
+							.isCategoryVisited(currentCategory, currentDivision);
+					$.finalist.setCategoryVisited(currentCategory, currentDivision);
+
+					$("#division").text(currentDivision);
 					$("#data").empty();
 
 					var headerRow = $("<tr><th>Finalist?</th><th>Score Group</th><th>Team #</th><th>Team Name</th><th>Score</th><th>Num Finalists</th></tr>");
@@ -27,32 +56,23 @@ $(document)
 						}
 					});
 
-					var championshipCategory = $.finalist
-							.getCategoryByName($.finalist.CHAMPIONSHIP_NAME);
-
 					// compute the set of all score groups
 					var scoreGroups = {};
 					$.each(teams, function(i, team) {
-						if (team.division == $.finalist.getCurrentDivision()) {
+						if (team.division == currentDivision) {
 							var group = $.finalist.getOverallGroup(team);
 							scoreGroups[group] = $.finalist
 									.getNumTeamsAutoSelected();
 						}
 					});
 
-					var checkedEnoughTeams = false;
+					if (!previouslyVisited) {
+						// initialize teams in category
+						initializeTeamsInCategory(currentCategory, teams, scoreGroups);
+					}
+
 					$.each(teams, function(i, team) {
 						if (team.division == $.finalist.getCurrentDivision()) {
-							// see if we've checked enough teams
-							if (!checkedEnoughTeams) {
-								checkedEnoughTeams = true;
-								$.each(scoreGroups, function(key, value) {
-									if (value > 0) {
-										checkedEnoughTeams = false;
-									}
-								});
-							}
-
 							var row = $("<tr></tr>");
 							$("#data").append(row);
 
@@ -63,22 +83,21 @@ $(document)
 							finalistCheck.change(function() {
 								if ($(this).attr("checked") == undefined) {
 									$.finalist.removeTeamFromCategory(
-											championshipCategory, team.num);
+											currentCategory, team.num);
 								} else {
 									$.finalist.addTeamToCategory(
-											championshipCategory, team.num);
+											currentCategory, team.num);
 								}
 							});
-							if (!checkedEnoughTeams) {
+							if ($.finalist.isTeamInCategory(currentCategory,
+									team.num)) {
 								finalistCheck.attr("checked", true);
-								finalistCheck.change();
 							}
 
 							var sgCol = $("<td></td>");
 							row.append(sgCol);
 							var group = $.finalist.getOverallGroup(team);
 							sgCol.text(group);
-							scoreGroups[group] = scoreGroups[group] - 1;
 
 							var numCol = $("<td></td>");
 							row.append(numCol);
