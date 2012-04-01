643196d1878fd92856b2a4a08c36bbfab59edeea
Jon Schewe <jpschewe@mtu.net>
2012-03-31 23:04:21 -0500

    Formatting

--- scoring/test-integration/fll/web/playoff/TestPlayoffs.java
+++ scoring/test-integration/fll/web/playoff/TestPlayoffs.java
@@ -30,7 +30,7 @@ public class TestPlayoffs extends SeleneseTestBase {
   public void setUp() throws Exception {
     LogUtils.initializeLogging();
     super.setUp(TestUtils.URL_ROOT
-                + "setup");
+        + "setup");
   }
 
   /**
@@ -43,63 +43,65 @@ public class TestPlayoffs extends SeleneseTestBase {
   @Test
   public void testNotAdvanced() throws IOException {
     try {
-    // initialize database using simple challenge descriptor that just has 1
-    // goal from 1 - 100
-    final InputStream challengeStream = TestPlayoffs.class.getResourceAsStream("data/simple.xml");
-    IntegrationTestUtils.initializeDatabase(selenium, challengeStream, true);
+      // initialize database using simple challenge descriptor that just has 1
+      // goal from 1 - 100
+      final InputStream challengeStream = TestPlayoffs.class.getResourceAsStream("data/simple.xml");
+      IntegrationTestUtils.initializeDatabase(selenium, challengeStream, true);
 
-    IntegrationTestUtils.setTournament(selenium, GenerateDB.DUMMY_TOURNAMENT_NAME);
+      IntegrationTestUtils.setTournament(selenium, GenerateDB.DUMMY_TOURNAMENT_NAME);
 
-    // add 4 teams to dummy tournament
-    for (int teamNumber = 0; teamNumber < 4; ++teamNumber) {
-      IntegrationTestUtils.addTeam(selenium, teamNumber, "team "
-          + teamNumber, "org", "dummy", "1", GenerateDB.DUMMY_TOURNAMENT_NAME);
-    }
+      // add 4 teams to dummy tournament
+      for (int teamNumber = 0; teamNumber < 4; ++teamNumber) {
+        IntegrationTestUtils.addTeam(selenium, teamNumber, "team "
+            + teamNumber, "org", "dummy", "1", GenerateDB.DUMMY_TOURNAMENT_NAME);
+      }
 
-    // set seeding rounds to 1
-    IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
-        + "admin/index.jsp");
-    selenium.select("name=seedingRounds", "1");
-    selenium.click("name=changeSeedingRounds");
-    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
+      // set seeding rounds to 1
+      IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
+          + "admin/index.jsp");
+      selenium.select("name=seedingRounds", "1");
+      selenium.click("name=changeSeedingRounds");
+      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
 
-    // enter 1 score for all teams equal to their team number
-    for (int teamNumber = 0; teamNumber < 4; ++teamNumber) {
-      enterTeamScore(teamNumber);
+      // enter 1 score for all teams equal to their team number
+      for (int teamNumber = 0; teamNumber < 4; ++teamNumber) {
+        enterTeamScore(teamNumber);
 
+        Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
+      }
+
+      // initialize playoffs
+      IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
+          + "playoff");
+      selenium.select("xpath=//form[@name='initialize']//select[@name='division']", "value=1");
+      selenium.click("id=initialize_brackets");
+      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
+      Assert.assertFalse(selenium.isTextPresent("Exception"));
+
+      // enter score for teams 3 and 0 with 3 winning
+      enterTeamScore(3);
       Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
-    }
+      enterTeamScore(0);
+      Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
 
-    // initialize playoffs
-    IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
-        + "playoff");
-    selenium.select("xpath=//form[@name='initialize']//select[@name='division']", "value=1");
-    selenium.click("id=initialize_brackets");
-    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-    Assert.assertFalse(selenium.isTextPresent("Exception"));
+      // enter score for teams 2 and 1 with 1 winning
+      enterTeamScore(2);
+      Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
+      enterTeamScore(1);
+      Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
 
-    // enter score for teams 3 and 0 with 3 winning
-    enterTeamScore(3);
-    Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
-    enterTeamScore(0);
-    Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
+      // attempt to enter score for 0
+      IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
+          + "scoreEntry/select_team.jsp");
+      selenium.select("xpath=//form[@name='selectTeam']//select[@name='TeamNumber']", "value=0");
+      selenium.click("id=enter_submit");
+      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
 
-    // enter score for teams 2 and 1 with 1 winning
-    enterTeamScore(2);
-    Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
-    enterTeamScore(1);
-    Assert.assertFalse("Errors: ", selenium.isElementPresent("name=error"));
-
-    // attempt to enter score for 0
-    IntegrationTestUtils.loadPage(selenium, TestUtils.URL_ROOT
-        + "scoreEntry/select_team.jsp");
-    selenium.select("xpath=//form[@name='selectTeam']//select[@name='TeamNumber']", "value=0");
-    selenium.click("id=enter_submit");
-    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-    // check for error message
-    //Assert.assertTrue("Should have errors", selenium.isElementPresent("name=error"));
-    Assert.assertTrue("Should have errors", selenium.isTextPresent("Selected team has not advanced to the next playoff round."));
+      // check for error message
+      // Assert.assertTrue("Should have errors",
+      // selenium.isElementPresent("name=error"));
+      Assert.assertTrue("Should have errors",
+                        selenium.isTextPresent("Selected team has not advanced to the next playoff round."));
     } catch (final IOException e) {
       IntegrationTestUtils.storeScreenshot(selenium);
       throw e;
