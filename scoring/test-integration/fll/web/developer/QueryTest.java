3c020d770f787cfe207e7ad2766fee0a0baefbe2
Jon Schewe <jpschewe@mtu.net>
2012-06-29 20:20:56 -0500

    Initialize the database

--- scoring/test-integration/fll/web/developer/QueryTest.java
+++ scoring/test-integration/fll/web/developer/QueryTest.java
@@ -9,21 +9,37 @@ package fll.web.developer;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.Test;
 import org.xml.sax.SAXException;
 
+import com.thoughtworks.selenium.SeleneseTestBase;
+
+import fll.TestUtils;
 import fll.util.LogUtils;
+import fll.web.IntegrationTestUtils;
 import fll.web.WebTestUtils;
 
 /**
  * Test that we can get database query results from {@link QueryHandler}.
  */
-public class QueryTest {
+public class QueryTest extends SeleneseTestBase {
 
   private static final Logger LOGGER = LogUtils.getLogger();
 
+  @Override
+  @Before
+  public void setUp() throws Exception {
+    LogUtils.initializeLogging();
+    super.setUp(TestUtils.URL_ROOT
+        + "setup");
+  }
+
   @Test
   public void test0() throws IOException, SAXException {
+    IntegrationTestUtils.initializeDatabaseFromDump(selenium,
+                                                    TestUtils.class.getResourceAsStream("/fll/data/testdb.flldb"));
+
     final String query = "SELECT * FROM Tournaments";
     final QueryHandler.ResultData result = WebTestUtils.executeServerQuery(query);
     for (final String colName : result.columnNames) {
