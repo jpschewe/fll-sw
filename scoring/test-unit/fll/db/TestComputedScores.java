d3c41ce22e585bc1e96faa18919ad7c4908dfea7
Jon Schewe <jpschewe@mtu.net>
2012-06-30 22:25:07 -0500

    Make sure that JSPs only create 1 datasource connection

--- scoring/test-unit/fll/db/TestComputedScores.java
+++ scoring/test-unit/fll/db/TestComputedScores.java
@@ -66,10 +66,10 @@ public class TestComputedScores {
       final InputStream dumpFileIS = TestComputedScores.class.getResourceAsStream("data/plymouth-2009-11-21.zip");
       Assert.assertNotNull("Cannot find test data", dumpFileIS);
       
-
-      ImportDB.loadFromDumpIntoNewDB(new ZipInputStream(dumpFileIS), database);
-
       connection = Utilities.createDataSource(database).getConnection();
+
+      ImportDB.loadFromDumpIntoNewDB(new ZipInputStream(dumpFileIS), connection);
+
       
       final Document document = Queries.getChallengeDocument(connection);
       final Element rootElement = document.getDocumentElement();
