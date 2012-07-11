f11d9ca8ea6e297a5f8ea4e664021e1eee98dc3e
Jon Schewe <jpschewe@mtu.net>
2012-07-11 12:07:25 -0500

    Remove references to old selenium driver

--- scoring/test-integration/fll/web/IntegrationTestUtils.java
+++ scoring/test-integration/fll/web/IntegrationTestUtils.java
@@ -26,8 +26,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-import com.thoughtworks.selenium.Selenium;
-
 import fll.TestUtils;
 import fll.util.LogUtils;
 
@@ -46,31 +44,6 @@ public final class IntegrationTestUtils {
 
   private IntegrationTestUtils() {
     // no instances
-  }
-
-  /**
-   * Load a page and check to make sure the page didn't crash.
-   * 
-   * @param selenium the test controller
-   * @param url the page to load
-   * @throws IOException
-   */
-  public static void loadPage(final Selenium selenium,
-                              final String url) throws IOException {
-    try {
-      selenium.open(url);
-      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-      final boolean error = selenium.isTextPresent("Exception");
-      Assert.assertFalse("Error loading: "
-          + url, error);
-    } catch (final AssertionError e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final RuntimeException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    }
   }
 
   /**
@@ -191,131 +164,6 @@ public final class IntegrationTestUtils {
   }
 
   /**
-   * Initialize the database using the given challenge descriptor.
-   * 
-   * @param selenium the test controller
-   * @param challengeStream the challenge descriptor
-   * @param forceRebuild if true, then force the database to be rebuilt
-   * @throws IOException
-   */
-  public static void initializeDatabase(final Selenium selenium,
-                                        final InputStream challengeStream,
-                                        final boolean forceRebuild) throws IOException {
-    try {
-      Assert.assertNotNull(challengeStream);
-      final File challengeFile = IntegrationTestUtils.storeInputStreamToFile(challengeStream);
-      try {
-        selenium.open(TestUtils.URL_ROOT
-            + "setup/");
-        selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-        if (selenium.isTextPresent("Login to FLL")) {
-          login(selenium);
-
-          selenium.open(TestUtils.URL_ROOT
-              + "setup/");
-          selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-        }
-
-        selenium.attachFile("xmldocument", challengeFile.toURI().toURL().toString());
-        if (forceRebuild) {
-          selenium.click("force_rebuild");
-        }
-        selenium.click("reinitializeDatabase");
-        Assert.assertTrue(selenium.getConfirmation()
-                                  .matches("^This will erase ALL scores in the database fll \\(if it already exists\\), are you sure[\\s\\S]$"));
-        selenium.waitForPageToLoad(WAIT_FOR_PAGE_TIMEOUT);
-
-        final boolean success = selenium.isTextPresent("Successfully initialized database");
-        Assert.assertTrue("Database was not successfully initialized", success);
-
-        // setup user
-        selenium.type("user", TEST_USERNAME);
-        selenium.type("pass", TEST_PASSWORD);
-        selenium.type("pass_check", TEST_PASSWORD);
-        selenium.click("submit_create_user");
-        selenium.waitForPageToLoad(WAIT_FOR_PAGE_TIMEOUT);
-        final boolean userSuccess = selenium.isTextPresent("Successfully created user");
-        Assert.assertTrue("Problem creating user", userSuccess);
-
-        login(selenium);
-      } finally {
-        if (!challengeFile.delete()) {
-          challengeFile.deleteOnExit();
-        }
-      }
-    } catch (final AssertionError e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final RuntimeException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final IOException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    }
-  }
-
-  /**
-   * Initialize a database from a zip file.
-   * 
-   * @param selenium the test controller
-   * @param inputStream input stream that has database to load in it, this input
-   *          stream is closed by this method upon successful completion
-   * @throws IOException
-   */
-  public static void initializeDatabaseFromDump(final Selenium selenium,
-                                                final InputStream inputStream) throws IOException {
-    try {
-      Assert.assertNotNull(inputStream);
-      final File dumpFile = IntegrationTestUtils.storeInputStreamToFile(inputStream);
-      try {
-        selenium.open(TestUtils.URL_ROOT
-            + "setup/");
-        selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-        if (selenium.isTextPresent("Login to FLL")) {
-          login(selenium);
-
-          selenium.open(TestUtils.URL_ROOT
-              + "setup/");
-          selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-        }
-
-        selenium.type("dbdump", dumpFile.getAbsolutePath());
-        selenium.click("createdb");
-        selenium.waitForPageToLoad(WAIT_FOR_PAGE_TIMEOUT);
-        final boolean success = selenium.isTextPresent("Successfully initialized database");
-        Assert.assertTrue("Database was not successfully initialized", success);
-
-        // setup user
-        selenium.type("user", TEST_USERNAME);
-        selenium.type("pass", TEST_PASSWORD);
-        selenium.type("pass_check", TEST_PASSWORD);
-        selenium.click("submit_create_user");
-        selenium.waitForPageToLoad(WAIT_FOR_PAGE_TIMEOUT);
-        final boolean userSuccess = selenium.isTextPresent("Successfully created user");
-        Assert.assertTrue("Problem creating user", userSuccess);
-
-      } finally {
-        if (!dumpFile.delete()) {
-          dumpFile.deleteOnExit();
-        }
-      }
-      login(selenium);
-    } catch (final AssertionError e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final RuntimeException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final IOException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    }
-  }
-
-  /**
    * Initialize a database from a zip file.
    * 
    * @param selenium the test controller
@@ -380,26 +228,7 @@ public final class IntegrationTestUtils {
       throw e;
     }
   }
-
-  public static void storeScreenshot(final Selenium selenium) throws IOException {
-    final File baseFile = File.createTempFile("fll", null, new File("screenshots"));
-    final File screenshot = new File(baseFile.getAbsolutePath()
-        + ".png");
-    selenium.captureScreenshot(screenshot.getAbsolutePath());
-    LOGGER.error("Screenshot saved to "
-        + screenshot.getAbsolutePath());
-
-    final File htmlFile = new File(baseFile.getAbsolutePath()
-        + ".html");
-    final String html = selenium.getHtmlSource();
-    final FileWriter writer = new FileWriter(htmlFile);
-    writer.write(html);
-    writer.close();
-    LOGGER.error("HTML saved to "
-        + htmlFile.getAbsolutePath());
-
-  }
-
+  
   public static void storeScreenshot(final WebDriver driver) throws IOException {
     final File baseFile = File.createTempFile("fll", null, new File("screenshots"));
 
@@ -448,18 +277,6 @@ public final class IntegrationTestUtils {
   /**
    * Login to fll
    */
-  public static void login(final Selenium selenium) {
-    selenium.open(TestUtils.URL_ROOT
-        + "login.jsp");
-    selenium.type("user", TEST_USERNAME);
-    selenium.type("pass", TEST_PASSWORD);
-    selenium.click("submit_login");
-    selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-  }
-
-  /**
-   * Login to fll
-   */
   public static void login(final WebDriver driver) {
     driver.get(TestUtils.URL_ROOT
         + "login.jsp");
@@ -472,57 +289,6 @@ public final class IntegrationTestUtils {
 
     final WebElement submitElement = driver.findElement(By.name("submit_login"));
     submitElement.click();
-  }
-
-  /**
-   * Add a team to a tournament.
-   */
-  public static void addTeam(final Selenium selenium,
-                             final int teamNumber,
-                             final String teamName,
-                             final String organization,
-                             final String division,
-                             final String tournament) throws IOException {
-    try {
-      loadPage(selenium, TestUtils.URL_ROOT
-          + "admin/index.jsp");
-
-      selenium.click("link=Add a team");
-      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-      selenium.type("teamNumber", String.valueOf(teamNumber));
-      selenium.type("teamName", teamName);
-      selenium.type("organization", organization);
-      selenium.click("id=division_text_choice");
-      selenium.type("division_text", division);
-
-      final String[] options = selenium.getSelectOptions("currentTournamentSelect");
-      String tournamentID = null;
-      for (int i = 0; i < options.length; ++i) {
-        if (options[i].equals(tournament)) {
-          tournamentID = options[i];
-        }
-      }
-      Assert.assertNotNull("Could not find tournament with name: "
-          + tournament, tournamentID);
-      selenium.select("currentTournamentSelect", tournamentID);
-
-      selenium.click("name=commit");
-
-      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-      Assert.assertTrue(selenium.isElementPresent("id=success"));
-
-    } catch (final AssertionError e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final RuntimeException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final IOException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    }
-
   }
 
   /**
@@ -575,48 +341,6 @@ public final class IntegrationTestUtils {
       throw e;
     }
 
-  }
-
-  /**
-   * Set the current tournament by name.
-   * 
-   * @param tournamentName the name of the tournament to make the current
-   *          tournament
-   * @throws IOException
-   */
-  public static void setTournament(final Selenium selenium,
-                                   final String tournamentName) throws IOException {
-    try {
-      loadPage(selenium, TestUtils.URL_ROOT
-          + "admin/index.jsp");
-
-      final String[] options = selenium.getSelectOptions("currentTournamentSelect");
-      String tournamentID = null;
-      for (int i = 0; i < options.length; ++i) {
-        if (options[i].endsWith("[ "
-            + tournamentName + " ]")) {
-          tournamentID = options[i];
-        }
-      }
-      Assert.assertNotNull("Could not find tournament with name: "
-          + tournamentName, tournamentID);
-
-      selenium.select("currentTournamentSelect", tournamentID);
-
-      selenium.click("change_tournament");
-      selenium.waitForPageToLoad(IntegrationTestUtils.WAIT_FOR_PAGE_TIMEOUT);
-
-      Assert.assertTrue(selenium.isElementPresent("id=success"));
-    } catch (final AssertionError e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final RuntimeException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    } catch (final IOException e) {
-      IntegrationTestUtils.storeScreenshot(selenium);
-      throw e;
-    }
   }
 
   /**
