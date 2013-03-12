127ede728da00a177aef75391fa11dc04a68ae06
Jon Schewe <jpschewe@mtu.net>
2013-03-11 20:58:12 -0500

    Add PDF version of finalist schedule to the report page
    There is now a private and public schedule available. The private
    schedule shows all categories, the public schedule only shows categories
    marked public.

---
+++ scoring/src/fll/web/report/finalist/PrivateFinalistSchedule.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2013 INSciTE.  All rights reserved
+ * INSciTE is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.web.report.finalist;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Outputs the PDF showing times of all finalist categories.
+ */
+@WebServlet("/report/finalist/PrivateFinalistSchedule")
+public class PrivateFinalistSchedule extends AbstractFinalistReport {
+
+  @Override
+  protected void processRequest(final HttpServletRequest request,
+                                final HttpServletResponse response,
+                                final ServletContext application,
+                                final HttpSession session) throws IOException, ServletException {
+    processRequest(true, response, application);    
+  }
+
+}
