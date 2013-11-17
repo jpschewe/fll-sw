eabbd27c4f8ac0260fda7937550de3474e525d47
Jon Schewe <jpschewe@mtu.net>
2013-11-17 15:48:33 -0600

    Working on why the odd team gets scheduled so late

--- scoring/src/fll/scheduler/GreedySolver.java
+++ scoring/src/fll/scheduler/GreedySolver.java
@@ -842,16 +842,59 @@ public class GreedySolver {
       }
     }
 
+    // TODO find prev team on each table and see if any of them can be assigned,
+    // if so, keep going
+    // not working yet... 
+
     // undo partial assignment if not allowed
     if (null != team1) {
+      boolean foundOtherTeam = false;
+      if (partialPerformanceAssignmentAllowed()) {
+        for (int otable = 0; !foundOtherTeam
+            && otable < getNumTables(); ++otable) {
+          final SchedTeam prevTeamOnTable0 = findPrevTeamOnTable(timeslot, table, 0);
+          if (null != prevTeamOnTable0) {
+            if (assignPerformance(prevTeamOnTable0.getGroup(), prevTeamOnTable0.getIndex(), timeslot, table, 1, false)) {
+              if (LOGGER.isTraceEnabled()) {
+                LOGGER.trace("Choose dummy group: "
+                    + prevTeamOnTable0.getGroup() + " team: " + prevTeamOnTable0.getIndex());
+              }
+              foundOtherTeam = true;
+            }
+          }
+
+          if (!foundOtherTeam) {
+            final SchedTeam prevTeamOnTable1 = findPrevTeamOnTable(timeslot, table, 1);
+            if (null != prevTeamOnTable1) {
+              if (LOGGER.isTraceEnabled()) {
+                LOGGER.trace("Choose dummy group: "
+                    + prevTeamOnTable1.getGroup() + " team: " + prevTeamOnTable1.getIndex());
+              }
+              if (assignPerformance(prevTeamOnTable1.getGroup(), prevTeamOnTable1.getIndex(), timeslot, table, 1, false)) {
+                foundOtherTeam = true;
+              }
+            }
+          }
+        }
+      }
+
       final SchedTeam prevTeamOnTable = findPrevTeamOnTable(timeslot, table, 1);
       if (partialPerformanceAssignmentAllowed()
           && null != prevTeamOnTable
-          /* commenting this out makes search go crazy on odd number of teams */
-          && assignPerformance(prevTeamOnTable.getGroup(), prevTeamOnTable.getIndex(), timeslot, table, 1, false)) {
+          /*
+           * commenting this out makes search go crazy on odd number of teams,
+           * leaving it in seems to cause problems as well
+           */
+          && foundOtherTeam
+      /*
+       * && assignPerformance(prevTeamOnTable.getGroup(),
+       * prevTeamOnTable.getIndex(), timeslot, table, 1, false)
+       */
+      ) {
         // use a dummy team as the other team
+
         dummyPerformanceSlotUsed = true;
-        if(LOGGER.isTraceEnabled()) {
+        if (LOGGER.isTraceEnabled()) {
           LOGGER.trace("Scheduling dummy slot");
         }
 
@@ -865,14 +908,15 @@ public class GreedySolver {
         }
 
       } else {
-        if(LOGGER.isTraceEnabled()) {
-          LOGGER.trace("Partial table assignment, unassigning group: " + team1.getGroup() //
-                       + " team: " + team1.getIndex() //
-                       + " slot: " + timeslot //
-                       + " table: " + table //
-                       + " prevTeamOnTable null?: " + (null == prevTeamOnTable) //
-                       + " partial allowed: " + partialPerformanceAssignmentAllowed() //
-                       );
+        if (LOGGER.isTraceEnabled()) {
+          LOGGER.trace("Partial table assignment, unassigning group: "
+              + team1.getGroup() //
+              + " team: " + team1.getIndex() //
+              + " slot: " + timeslot //
+              + " table: " + table //
+              + " prevTeamOnTable null?: " + (null == prevTeamOnTable) //
+              + " partial allowed: " + partialPerformanceAssignmentAllowed() //
+          );
         }
         unassignPerformance(team1.getGroup(), team1.getIndex(), timeslot, table, 0);
         team1 = null;
@@ -913,6 +957,10 @@ public class GreedySolver {
 
     return false;
   }
+
+  /**
+   *  
+   */
 
   /**
    * Find the team that is on the table and side prior to timeslot.
