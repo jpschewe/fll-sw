2fdd0e99ae138e35b8fe5927ce6d9719340c6b4b
Jon Schewe <jpschewe@mtu.net>
2013-12-30 21:54:34 -0600

    Start of subjective webapp
    This loads the teams, current tournament and subjective categories
    
    Still need the schedule, and current scores
    
    task 93: Mobile version of the subjective application 
    https://sourceforge.net/p/fll-sw/issues/93

---
+++ scoring/web/subjective/subjective.js
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2014 INSciTE.  All rights reserved
+ * INSciTE is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+(function($) {
+	if (!$) {
+		throw new Error("jQuery needs to be loaded!");
+	}
+	if (!$.jStorage) {
+		throw new Error("jStorage needs to be loaded!");
+	}
+
+	var STORAGE_PREFIX = "fll.subjective";
+
+	// //////////////////////// PRIVATE INTERFACE ////////////////////////
+
+	var _subjectiveCategories;
+	var _tournament;
+	var _teams;
+
+	function _init_variables() {
+		_subjectiveCategories = {};
+		_tournament = null;
+		_teams = {};
+	}
+
+	function _log(str) {
+		if (typeof (console) != 'undefined') {
+			console.log(str);
+		}
+	}
+
+	/**
+	 * Clear anything from local storage with a prefix of STORAGE_PREFIX.
+	 */
+	function _clear_local_storage() {
+		$.each($.jStorage.index(), function(index, value) {
+			if (value.substring(0, STORAGE_PREFIX.length) == STORAGE_PREFIX) {
+				$.jStorage.deleteKey(value);
+			}
+		});
+	}
+
+	function _loadFromDisk() {
+		_init_variables();
+
+		_log("Loading from disk");
+
+		var value = $.jStorage.get(STORAGE_PREFIX + "_subjectiveCategories");
+		if (null != value) {
+			_subjectiveCategories = value;
+		}
+	}
+
+	function _save() {
+		_log("Save not yet implemented");
+	}
+
+	function _loadTournament() {
+		_tournament = null;
+
+		return $.getJSON("../api/Tournaments/current", function(tournament) {
+			_tournament = tournament;
+		});
+	}
+
+	function _loadTeams() {
+		_teams = {};
+
+		return $.getJSON("../api/TournamentTeams", function(teams) {
+			$.each(teams, function(i, team) {
+				_teams[team.teamNumber] = team;
+			});
+		});
+	}
+
+	/**
+	 * Load the subjective categories.
+	 * 
+	 * @returns the promise for the ajax query
+	 */
+	function _loadSubjectiveCategories() {
+		_subjectiveCategories = {};
+
+		return $
+				.getJSON(
+						"../api/ChallengeDescription/SubjectiveCategories",
+						function(subjectiveCategories) {
+							$
+									.each(
+											subjectiveCategories,
+											function(i, scoreCategory) {
+												_subjectiveCategories[scoreCategory.name] = scoreCategory;
+											});
+						});
+	}
+
+	// //////////////////////// PUBLIC INTERFACE /////////////////////////
+	$.subjective = {
+
+		clearAllData : function() {
+			_clear_local_storage();
+			_init_variables();
+		},
+
+		loadFromServer : function(doneCallback, failCallback) {
+			_init_variables();
+
+			console.log("Loading from server");
+
+			var waitList = []
+			waitList.push(_loadSubjectiveCategories());
+			waitList.push(_loadTournament());
+			waitList.push(_loadTeams());
+
+			$.when.apply($, waitList).done(function() {
+				doneCallback();
+			}).fail(function() {
+				failCallback();
+			});
+		},
+
+		getSubjectiveCategories : function() {
+			var retval = [];
+			$.each(_subjectiveCategories, function(i, val) {
+				retval.push(val);
+			});
+			return retval;
+		},
+
+		getTeams : function() {
+			var retval = [];
+			$.each(_teams, function(i, val) {
+				retval.push(val);
+			});
+			return retval;
+		},
+
+		/**
+		 * Find a team by number.
+		 */
+		lookupTeam : function(teamNum) {
+			return _teams[teamNum];
+		},
+
+		log : function(str) {
+			_log(str);
+		},
+
+		getTournament : function() {
+			return _tournament;
+		},
+
+		setTournament : function(tournament) {
+			_tournament = tournament;
+			_save();
+		},
+
+	};
+
+	// ///// Load data //////////
+	_loadFromDisk();
+})(window.jQuery || window.$);
