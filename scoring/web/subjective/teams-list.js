f254da62b72da0ba7589229fa57c90e9fb83f85e
Jon Schewe <jpschewe@mtu.net>
2014-01-01 18:05:11 -0600

    Have teams sorted by time
    task 93: Mobile version of the subjective application 
    https://sourceforge.net/p/fll-sw/issues/93

--- scoring/web/subjective/teams-list.js
+++ scoring/web/subjective/teams-list.js
@@ -4,22 +4,29 @@
  * This code is released under GPL; see LICENSE.txt for details.
  */
 
+function selectTeam(team) {
+	alert("Selected team: " + team.teamNumber);
+	//FIXME
+}
+
 $("#teams-list").live(
 		"pagebeforecreate",
 		function(event) {
 
-			/*
-			var categories = $.subjective.getSubjectiveCategories();
-			$.each(categories, function(i, category) {
-				var button = $("<button class='ui-btn ui-corner-all'>"
-						+ category.title + "</button>");
-				$("#categories").append(button);
+			
+			var teams = $.subjective.getCurrentTeams();
+			$.each(teams, function(i, team) {
+				var time = $.subjective.getScheduledTime(team.teamNumber);
+				var timeStr = time.getHours() + ":" + time.getMinutes();
+				
+				var button = $("<button class='ui-btn ui-corner-all'>" + timeStr + " "
+						+ team.teamNumber + " " + team.teamName + "</button>");
+				$("#teams").append(button);
 				button.click(function() {
-					selectCategory(category);
+					selectTeam(team);
 				});
 
 			});
-*/
 			
 			var currentJudgingGroup = $.subjective.getCurrentJudgingGroup();
 			$("#judging-group").text(currentJudgingGroup);
