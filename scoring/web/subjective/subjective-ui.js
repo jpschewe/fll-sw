c303cc62440fa4bcda1e00f90769f31538f5120f
Jon Schewe <jpschewe@mtu.net>
2014-02-01 16:49:10 -0600

    Add the teams list to the single file
    Links back from the rubric page are currently broken, although forward
    navigation works.
    
    task 93: Mobile version of the subjective application 
    https://sourceforge.net/p/fll-sw/issues/93

--- scoring/web/subjective/subjective-ui.js
+++ scoring/web/subjective/subjective-ui.js
@@ -6,8 +6,6 @@
 
 function selectJudgingGroup(group) {
 	$.subjective.setCurrentJudgingGroup(group);
-	//location.href = "choose-category.html";
-	//$( ":mobile-pagecontainer" ).pagecontainer( "change", "#choose-category-page");
 	$.mobile.navigate("#choose-category-page");
 }
 
@@ -35,9 +33,6 @@ $(document).on(
 
 function selectCategory(category) {
 	$.subjective.setCurrentCategory(category);
-	//location.href = "choose-judge.html";
-	//$( ":mobile-pagecontainer" ).pagecontainer( "change", "#choose-judge-page");
-	
 	$.mobile.navigate("#choose-judge-page");
 }
 
@@ -148,5 +143,124 @@ function setJudge() {
 
 	$.subjective.setCurrentJudge(judgeID);
 
-	location.href = "teams-list.html";
+	$.mobile.navigate("#teams-list-page");
 }
+
+
+function selectTeam(team) {
+	$.subjective.setCurrentTeam(team);
+
+	location.href = "enter-score.html";
+}
+
+function populateTeams() {
+	$("#teams-list_teams").empty();
+	var teams = $.subjective.getCurrentTeams();
+	$.each(teams, function(i, team) {
+		var time = $.subjective.getScheduledTime(team.teamNumber);
+		var timeStr = time.getHours() + ":" + time.getMinutes();
+
+		var scoreStr;
+		var score = $.subjective.getScore(team.teamNumber);
+		if (!$.subjective.isScoreCompleted(score)) {
+			scoreStr = "";
+		} else if (score.noShow) {
+			scoreStr = "No Show";
+		} else {
+			var computedScore = $.subjective.computeScore(score);
+			scoreStr = computedScore;
+		}
+		var button = $("<button class='ui-btn ui-corner-all'>" + timeStr //
+				+ " " + team.teamNumber //
+				+ " " + team.teamName //
+				+ " " + team.organization //
+				+ " " + scoreStr //
+				+ "</button>");
+		$("#teams-list_teams").append(button);
+		button.click(function() {
+			selectTeam(team);
+		});
+
+	});
+}
+
+$(document).on("pagebeforecreate", "#teams-list-page", function(event) {
+
+	populateTeams();
+
+	var currentJudgingGroup = $.subjective.getCurrentJudgingGroup();
+	$("#teams-list_judging-group").text(currentJudgingGroup);
+
+	var currentCategory = $.subjective.getCurrentCategory();
+	$("#teams-list_category").text(currentCategory.title);
+
+});
+
+function uploadScoresSuccess(result) {
+	populateTeams();
+
+	alert("Uploaded " + result.numModified + " scores. message: " + result.message);
+}
+
+function uploadScoresFail(result) {
+	populateTeams();
+
+	var message;
+	if (null == result) {
+		message = "Unknown server error";
+	} else {
+		message = result.message;
+	}
+
+	alert("Failed to upload scores: " + message);
+}
+
+function uploadJudgesSuccess(result) {
+	$.subjective.log("Judges modified: " + result.numModifiedJudges + " new: " + result.numNewJudges);
+}
+
+function uploadJudgesFail(result) {
+	var message;
+	if (null == result) {
+		message = "Unknown server error";
+	} else {
+		message = result.message;
+	}
+
+	alert("Failed to upload judges: " + message);
+}
+
+function loadScoresSuccess() {
+	$("#upload-scores-wait").hide();
+}
+
+function loadScoresFail(message) {
+	$("#upload-scores-wait").hide();
+
+	alert("Failed to load scores from server: " + message);
+}
+
+$(document).on("pageinit", "#teams-list-page", function(event) {
+	$("#upload-scores-wait").hide();
+
+	$("#nav-top").click(function() {
+		location.href = "index.html";
+	});
+	$("#nav-choose-judging-group").click(function() {
+		$.mobile.navigate("#choose-judge-page");
+	});
+	$("#nav-choose-category").click(function() {
+		$.mobile.navigate("#choose-category-page");
+	});
+	$("#nav-choose-judge").click(function() {
+		$.mobile.navigate("#choose-judge-page");
+	});
+
+	$("#upload-scores").click(function() {
+		$("#upload-scores-wait").show();
+
+		$.subjective.uploadData(uploadScoresSuccess, uploadScoresFail,
+				uploadJudgesSuccess, uploadJudgesFail,
+				loadScoresSuccess, loadScoresFail);
+	});
+});
