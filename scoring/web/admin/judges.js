242fa0e1f38ab35c8b50acba8bcee65e4b51cd7b
Jon Schewe <jpschewe@mtu.net>
2014-02-09 20:16:46 -0600

    Make sure to insert phone column when adding rows
    The javascript wasn't adding the phone column.
    
    task 93: Mobile version of the subjective application 
    https://sourceforge.net/p/fll-sw/issues/93

--- scoring/web/admin/judges.js
+++ scoring/web/admin/judges.js
@@ -48,6 +48,12 @@ function addRows(numRows) {
 		var id = $("<input type='text' name='id" + judgeIdx + "'>");
 		idCol.append(id);
 
+		var phoneCol = $("<td></td>");
+		row.append(phoneCol);
+		var phone = $("<input type='text' name='phone" + judgeIdx + "'>");
+		phoneCol.append(phone);
+
+		
 		var categoryCol = $("<td></td>");
 		row.append(categoryCol);
 		var category = $("<select name='cat" + judgeIdx + "'></select>");
