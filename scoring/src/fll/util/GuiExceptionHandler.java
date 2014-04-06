a704dc4a744ac7056ca3476823b3e63e58f0f9f4
Jon Schewe <jpschewe@mtu.net>
2014-04-06 18:13:56 -0500

    Install a default exception handler for the GUI applications
    This will bring up a message dialog displaying the error and then exit
    the application.
    
    task 7: Catch scheduling errors and display them 
    http://github.com/jpschewe/fll-sw/issues/issue/7

---
+++ scoring/src/fll/util/GuiExceptionHandler.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2014 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.util;
+
+import java.lang.Thread.UncaughtExceptionHandler;
+
+import javax.swing.JOptionPane;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Display exception information in a dialog and then exit when the dialog is
+ * closed.
+ * Setup with
+ * {@link Thread#setUncaughtExceptionHandler(UncaughtExceptionHandler)}.
+ */
+public class GuiExceptionHandler implements UncaughtExceptionHandler {
+
+  private static final Logger LOGGER = LogUtils.getLogger();
+
+  public GuiExceptionHandler() {
+  }
+
+  /**
+   * @see java.lang.Thread.UncaughtExceptionHandler#uncaughtException(java.lang.Thread,
+   *      java.lang.Throwable)
+   */
+  @Override
+  public void uncaughtException(final Thread t,
+                                final Throwable ex) {
+    final String message = String.format("Unexpected error on thread %s: %s", t.getName(), ex.getMessage());
+    LOGGER.fatal(message, ex);
+    JOptionPane.showMessageDialog(null, message, "Unexpected Error", JOptionPane.ERROR_MESSAGE);
+    System.exit(1);
+  }
+
+}
