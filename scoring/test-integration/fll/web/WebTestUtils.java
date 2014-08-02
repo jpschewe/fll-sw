989b545a80a1b4134c00254f604d65ba00f3c684
Jon Schewe <jpschewe@mtu.net>
2014-08-02 18:44:14 -0500

    Migrate FullTournamentTest to HtmlUnit
    task #299: Migrate to HtmlUnit 
    http://github.com/jpschewe/fll-sw/issues/issue/299

--- scoring/test-integration/fll/web/WebTestUtils.java
+++ scoring/test-integration/fll/web/WebTestUtils.java
@@ -9,7 +9,6 @@ package fll.web;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Collections;
 
@@ -23,17 +22,13 @@ import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.TextPage;
 import com.gargoylesoftware.htmlunit.UnexpectedPage;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebRequest;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 import com.gargoylesoftware.htmlunit.util.NameValuePair;
-import com.meterware.httpunit.GetMethodWebRequest;
-import com.meterware.httpunit.WebConversation;
-import com.meterware.httpunit.WebForm;
-import com.meterware.httpunit.WebRequest;
-import com.meterware.httpunit.WebResponse;
 
 import fll.TestUtils;
 import fll.web.developer.QueryHandler;
@@ -45,27 +40,6 @@ public final class WebTestUtils {
 
   private WebTestUtils() {
     // no instances
-  }
-
-  /**
-   * Load a page and return the response. If there is an HttpException, call
-   * {@link Assert#fail(String)} with a reasonable message.
-   */
-  public static WebResponse loadPageOld(final WebConversation conversation,
-                                        final WebRequest request) throws IOException, SAXException {
-    final boolean exceptionOnError = conversation.getExceptionsThrownOnErrorStatus();
-    conversation.setExceptionsThrownOnErrorStatus(false);
-    try {
-      final WebResponse response = conversation.getResponse(request);
-
-      // check response code here and fail with useful message
-      checkForServerErrorOld(response);
-
-      return response;
-    } finally {
-      // restore value
-      conversation.setExceptionsThrownOnErrorStatus(exceptionOnError);
-    }
   }
 
   public static Page loadPage(final WebClient conversation,
@@ -83,32 +57,6 @@ public final class WebTestUtils {
       // restore value
       conversation.getOptions().setThrowExceptionOnFailingStatusCode(exceptionOnError);
     }
-  }
-
-  private static void checkForServerErrorOld(final WebResponse response) throws IOException {
-    final int code = response.getResponseCode();
-    final boolean error;
-    if (response.getResponseCode() == HttpURLConnection.HTTP_INTERNAL_ERROR) {
-      error = true;
-    } else if (response.getResponseCode() == HttpURLConnection.HTTP_NOT_FOUND) {
-      error = true;
-    } else if (response.getResponseCode() >= HttpURLConnection.HTTP_BAD_REQUEST) {
-      error = true;
-    } else {
-      error = false;
-    }
-    if (error) {
-      final String responseMessage = response.getResponseMessage();
-      final String text = response.getText();
-      final File output = File.createTempFile("server-error", ".html", new File("screenshots"));
-      final FileWriter writer = new FileWriter(output);
-      writer.write(text);
-      writer.close();
-      Assert.fail("Error loading page: "
-          + response.getURL() + " code: " + code + " message: " + responseMessage
-          + " Contents of error page written to: " + output.getAbsolutePath());
-    }
-
   }
 
   private static void checkForServerError(final Page page) throws IOException {
@@ -150,54 +98,16 @@ public final class WebTestUtils {
     }
   }
 
-  /**
-   * Create a new web conversation that is logged in.
-   * 
-   * @return a web conversation to use
-   * @throws SAXException
-   * @throws IOException
-   */
-  public static WebConversation getConversationOld() throws IOException, SAXException {
-    final WebConversation conversation = new WebConversation();
-
-    // always login first
-    WebRequest request = new GetMethodWebRequest(TestUtils.URL_ROOT
-        + "login.jsp");
-    WebResponse response = conversation.getResponse(request);
-    Assert.assertTrue("Received non-HTML response from web server", response.isHTML());
-
-    WebForm form = response.getFormWithName("login");
-    Assert.assertNotNull("Cannot find login form", form);
-    request = form.getRequest();
-    request.setParameter("user", IntegrationTestUtils.TEST_USERNAME);
-    request.setParameter("pass", IntegrationTestUtils.TEST_PASSWORD);
-    response = conversation.getResponse(request);
-    Assert.assertTrue("Received non-HTML response from web server", response.isHTML());
-
-    final URL responseURL = response.getURL();
-    final String address = responseURL.getPath();
-    final boolean correctAddress;
-    if (address.contains("login.jsp")) {
-      correctAddress = false;
-    } else {
-      correctAddress = true;
-    }
-    Assert.assertTrue("Unexpected URL after login: "
-        + address, correctAddress);
-
-    return conversation;
-  }
-
   public static WebClient getConversation() throws IOException, SAXException {
     final WebClient conversation = new WebClient();
 
     // always login first
-    HtmlPage response = conversation.getPage(TestUtils.URL_ROOT
+    final Page loginPage = conversation.getPage(TestUtils.URL_ROOT
         + "login.jsp");
-    // Assert.assertTrue("Received non-HTML response from web server",
-    // response.isHTML());
+    Assert.assertTrue("Received non-HTML response from web server", loginPage.isHtmlPage());
 
-    HtmlForm form = response.getFormByName("login");
+    final HtmlPage loginHtml = (HtmlPage)loginPage;
+    HtmlForm form = loginHtml.getFormByName("login");
     Assert.assertNotNull("Cannot find login form", form);
 
     final HtmlTextInput userTextField = form.getInputByName("user");
@@ -207,7 +117,7 @@ public final class WebTestUtils {
     passTextField.setValueAttribute(IntegrationTestUtils.TEST_PASSWORD);
 
     final HtmlSubmitInput button = form.getInputByName("submit_login");
-    response = button.click();
+    final Page response = button.click();
 
     final URL responseURL = response.getUrl();
     final String address = responseURL.getPath();
@@ -231,7 +141,7 @@ public final class WebTestUtils {
 
     final URL url = new URL(TestUtils.URL_ROOT
         + "developer/QueryHandler");
-    final com.gargoylesoftware.htmlunit.WebRequest request = new com.gargoylesoftware.htmlunit.WebRequest(url);
+    final WebRequest request = new WebRequest(url);
     request.setRequestParameters(Collections.singletonList(new NameValuePair(QueryHandler.QUERY_PARAMETER, query)));
 
     final Page response = loadPage(conversation, request);
