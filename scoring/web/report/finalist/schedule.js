b98c71a2e61de2f03311eeff94cd32c9783bb538
Jon Schewe <jpschewe@mtu.net>
2014-08-13 22:53:04 -0500

    Add finalist rooms to the schedules seen from the server
    This pushes the room information to the server, stores it in the
    database and then displays that information in the various finalist
    schedules that can be seen on the server.
    
    Fixes #97.
    
    task #97: Add room numbers to the finalist schedule [sf#62] 
    http://github.com/jpschewe/fll-sw/issues/issue/97

--- scoring/web/report/finalist/schedule.js
+++ scoring/web/report/finalist/schedule.js
@@ -41,9 +41,10 @@ function FinalistDBRow(categoryName, hour, minute, teamNumber) {
  * Used for packaging up and sending to the server to put in the database. Needs
  * to match fll.web.report.finalist.FinalistCategoryRow.
  */
-function FinalistCategoryRow(categoryName, isPublic) {
+function FinalistCategoryRow(categoryName, isPublic, room) {
 	this.categoryName = categoryName;
 	this.isPublic = isPublic;
+	this.room = room;
 }
 
 $(document).ready(
@@ -105,7 +106,7 @@ $(document).ready(
 			var categoryRows = [];
 			$.each($.finalist.getAllCategories(), function(i, category) {
 				var cat = new FinalistCategoryRow(category.name,
-						category.isPublic);
+						category.isPublic, $.finalist.getRoom(category, currentDivision));
 				categoryRows.push(cat);
 			}); // foreach category
 			$('#category_data').val($.toJSON(categoryRows));
