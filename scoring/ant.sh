009639b27f47c9d36b24b5d4c670a5d8e5ab3036
Eric Engstrom <engstrom@mtu.net>
2014-12-29 23:43:56 -0600

    Minor updates to ant configuration at top level.
    * Make ant.sh more portable in how it finds the current working
      directory of the ant.sh to be able to call "our" ant.
    * Make ant.sh properly pass all the arguments to ant.
    * Ignore "ant" - leave open symlink option
    * Ignore ant bash completion targets file

--- scoring/ant.sh
+++ scoring/ant.sh
@@ -4,7 +4,6 @@
 #ant --noconfig -lib lib -lib lib/ant -lib lib/test $*
 #ant --noconfig $*
 
-
 # unset some variables to ensure that we don't have problems with an existing tomcat
 unset JASPER_HOME
 unset TOMCAT_USER
@@ -15,21 +14,23 @@ unset TOMCAT_HOME
 unset CATALINA_HOME
 unset CATALINA_BASE
 
-pushd . > /dev/null 2>&1
-
-## this may not be portable:
-mypath=`dirname $0`
-cd "${mypath}"
-mypath=`pwd`
-
-cd "${mypath}"
-popd > /dev/null 2>&1
-
-
-#ANT_OPTS="-XX:MaxPermSize=128m"
-#export ANT_OPTS
-
 #Make sure that the right ant is being used
 unset ANT_HOME
 
-"${mypath}"/tools/ant/bin/ant --noconfig $*
+# Set ANT options
+#ANT_OPTS="-XX:MaxPermSize=128m"
+#export ANT_OPTS
+
+# Get the current working directory of this script
+MYDIR=$(cd ${0%/*} && builtin pwd)
+
+### this may not be portable:
+#pushd . > /dev/null 2>&1
+#mypath=`dirname $0`
+#cd "${mypath}"
+#mypath=`pwd`
+#cd "${mypath}"
+#popd > /dev/null 2>&1
+
+# Run _our_ ant, properly passing all arguments
+exec ${MYDIR}/tools/ant/bin/ant --noconfig "$@"
