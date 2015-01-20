85107737f3c69b18975ade5982c432deb79a1c7f
Jon Schewe <jpschewe@mtu.net>
2015-01-20 06:47:28 -0600

    Cut down number of round trips to web server
    Put judge summary information in page context and call directly from
    jsp. Reduces 1 round trip and less session variables.
    
    task #367: Problem with 1 judge and 2 judging groups 
    http://github.com/jpschewe/fll-sw/issues/issue/367

--- scoring/src/fll/web/report/SummarizePhase1.java
+++ scoring/src/fll/web/report/SummarizePhase1.java
@@ -17,10 +17,7 @@ import java.util.LinkedList;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.jsp.PageContext;
 import javax.sql.DataSource;
 
 import net.mtu.eggplant.util.sql.SQLFunctions;
@@ -28,27 +25,22 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import fll.ScoreStandardization;
 import fll.db.Queries;
 import fll.web.ApplicationAttributes;
-import fll.web.BaseFLLServlet;
 import fll.xml.ChallengeDescription;
 
 /**
  * Do first part of summarizing scores and gather information to show the user
  * about where we are.
  */
-@WebServlet("/report/SummarizePhase1")
-public class SummarizePhase1 extends BaseFLLServlet {
+public class SummarizePhase1 {
 
   /**
-   * Session key for judge information. Type is Collection of JudgeSummary.
+   * Page key for judge information. Type is Collection of JudgeSummary.
    */
   public static final String JUDGE_SUMMARY = "judgeSummary";
 
-  @Override
   @SuppressFBWarnings(value = { "SQL_PREPARED_STATEMENT_GENERATED_FROM_NONCONSTANT_STRING" }, justification = "Need to generate table name from category")
-  protected void processRequest(final HttpServletRequest request,
-                                final HttpServletResponse response,
-                                final ServletContext application,
-                                final HttpSession session) throws IOException, ServletException {
+  public static void populateContext(final ServletContext application,
+                                     final PageContext pageContext) throws IOException, ServletException {
     final ChallengeDescription challengeDescription = ApplicationAttributes.getChallengeDescription(application);
     final DataSource datasource = ApplicationAttributes.getDataSource(application);
     PreparedStatement getJudges = null;
@@ -105,9 +97,7 @@ public class SummarizePhase1 extends BaseFLLServlet {
 
       }
 
-      session.setAttribute(JUDGE_SUMMARY, summary);
-
-      response.sendRedirect(response.encodeRedirectURL("summarizePhase1.jsp"));
+      pageContext.setAttribute(JUDGE_SUMMARY, summary);
 
     } catch (final SQLException e) {
       throw new RuntimeException(e);
