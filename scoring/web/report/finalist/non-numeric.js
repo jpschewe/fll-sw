33fe04cdf129906d1af3c2da172eea64cfc19986
Jon Schewe <jpschewe@mtu.net>
2015-04-13 18:37:43 -0500

    Indent javascript files
    Fixes  #355: Fix indentation in javascript files 
    http://github.com/jpschewe/fll-sw/issues/issue/355

--- scoring/web/report/finalist/non-numeric.js
+++ scoring/web/report/finalist/non-numeric.js
@@ -5,102 +5,99 @@
  */
 
 function handleDivisionChange() {
-	var divIndex = $("#divisions").val();
-	var div = $.finalist.getDivisionByIndex(divIndex);
-	$.finalist.setCurrentDivision(div);
-	updateTeams();
+  var divIndex = $("#divisions").val();
+  var div = $.finalist.getDivisionByIndex(divIndex);
+  $.finalist.setCurrentDivision(div);
+  updateTeams();
 }
 
 $(document).ready(
-		function() {
+    function() {
 
-			$("#divisions").empty();
-			$.each($.finalist.getDivisions(), function(i, division) {
-				var selected = "";
-				if (division == $.finalist.getCurrentDivision()) {
-					selected = " selected ";
-				}
-				var divisionOption = $("<option value='" + i + "'" + selected
-						+ ">" + division + "</option>");
-				$("#divisions").append(divisionOption);
-			}); // foreach division
-			$("#divisions").change(function() {
-				handleDivisionChange();
-			});
-			handleDivisionChange();
+      $("#divisions").empty();
+      $.each($.finalist.getDivisions(), function(i, division) {
+        var selected = "";
+        if (division == $.finalist.getCurrentDivision()) {
+          selected = " selected ";
+        }
+        var divisionOption = $("<option value='" + i + "'" + selected + ">"
+            + division + "</option>");
+        $("#divisions").append(divisionOption);
+      }); // foreach division
+      $("#divisions").change(function() {
+        handleDivisionChange();
+      });
+      handleDivisionChange();
 
-			updateTeams();
+      updateTeams();
 
-			$("#add-category").click(function() {
-				addCategory();
-			});
+      $("#add-category").click(function() {
+        addCategory();
+      });
 
-			$.finalist.displayNavbar();
-		}); // end ready function
+      $.finalist.displayNavbar();
+    }); // end ready function
 
 function updateTeams() {
-	$("#categories").empty();
-	$.each($.finalist.getNonNumericCategories(), function(i, category) {
-		addCategoryElement(category);
-		addedCategory = true;
+  $("#categories").empty();
+  $.each($.finalist.getNonNumericCategories(), function(i, category) {
+    addCategoryElement(category);
+    addedCategory = true;
 
-		var addedTeam = false;
-		$.each(category.teams, function(j, teamNum) {
-			var team = $.finalist.lookupTeam(teamNum);
-			if ($.finalist.isTeamInDivision(team, $.finalist
-					.getCurrentDivision())) {
-				addedTeam = true;
-				var teamIdx = addTeam(category);
-				populateTeamInformation(category, teamIdx, team);
-			}
+    var addedTeam = false;
+    $.each(category.teams, function(j, teamNum) {
+      var team = $.finalist.lookupTeam(teamNum);
+      if ($.finalist.isTeamInDivision(team, $.finalist.getCurrentDivision())) {
+        addedTeam = true;
+        var teamIdx = addTeam(category);
+        populateTeamInformation(category, teamIdx, team);
+      }
 
-		});
-		if (!addedTeam) {
-			addTeam(category);
-		}
-	});
+    });
+    if (!addedTeam) {
+      addTeam(category);
+    }
+  });
 }
 
 function addCategoryElement(category) {
-	var catEle = $("<li></li>");
-	$("#categories").append(catEle);
+  var catEle = $("<li></li>");
+  $("#categories").append(catEle);
 
-	var nameEle = $("<input class='category_name' type='text' id='name_"
-			+ category.catId + "'/>");
-	catEle.append(nameEle);
-	nameEle.change(function() {
-		var newName = nameEle.val();
-		if (null == newName || "" == newName) {
-			alert("All categories must have non-empty names");
-			nameEle.val(category.name);
-		}
-		if (!$.finalist.setCategoryName(category, newName)) {
-			alert("There already exists a category with the name '" + newName
-					+ "'");
-			nameEle.val(category.name);
-		}
-	});
-	nameEle.val(category.name);
+  var nameEle = $("<input class='category_name' type='text' id='name_"
+      + category.catId + "'/>");
+  catEle.append(nameEle);
+  nameEle.change(function() {
+    var newName = nameEle.val();
+    if (null == newName || "" == newName) {
+      alert("All categories must have non-empty names");
+      nameEle.val(category.name);
+    }
+    if (!$.finalist.setCategoryName(category, newName)) {
+      alert("There already exists a category with the name '" + newName + "'");
+      nameEle.val(category.name);
+    }
+  });
+  nameEle.val(category.name);
 
-	catEle.append("Room number: ");
-	var roomEle = $("<input type='text' id='room_" + category.catId + "'/>");
-	catEle.append(roomEle);
-	roomEle.change(function() {
-		var roomNumber = roomEle.val();
-		$.finalist.setRoom(category, $.finalist.getCurrentDivision(),
-				roomNumber);
-	});
-	roomEle.val($.finalist.getRoom(category, $.finalist.getCurrentDivision()));
+  catEle.append("Room number: ");
+  var roomEle = $("<input type='text' id='room_" + category.catId + "'/>");
+  catEle.append(roomEle);
+  roomEle.change(function() {
+    var roomNumber = roomEle.val();
+    $.finalist.setRoom(category, $.finalist.getCurrentDivision(), roomNumber);
+  });
+  roomEle.val($.finalist.getRoom(category, $.finalist.getCurrentDivision()));
 
-	var teamList = $("<ul id='category_" + category.catId + "'></ul>");
-	catEle.append(teamList);
+  var teamList = $("<ul id='category_" + category.catId + "'></ul>");
+  catEle.append(teamList);
 
-	var addButton = $("<button id='add-team_" + category.catId
-			+ "'>Add Team</button>");
-	catEle.append(addButton);
-	addButton.click(function() {
-		addTeam(category);
-	});
+  var addButton = $("<button id='add-team_" + category.catId
+      + "'>Add Team</button>");
+  catEle.append(addButton);
+  addButton.click(function() {
+    addTeam(category);
+  });
 }
 
 /**
@@ -109,41 +106,41 @@ function addCategoryElement(category) {
  * @return the category index
  */
 function addCategory() {
-	var category = $.finalist.addCategory("", false);
-	if (null == category) {
-		return;
-	}
-	$.finalist.setCategoryName(category, "Category " + category.catId);
+  var category = $.finalist.addCategory("", false);
+  if (null == category) {
+    return;
+  }
+  $.finalist.setCategoryName(category, "Category " + category.catId);
 
-	addCategoryElement(category);
+  addCategoryElement(category);
 
-	addTeam(category);
-	return category;
+  addTeam(category);
+  return category;
 }
 
 function teamNumId(category, teamIdx) {
-	return "num_" + category + "_" + teamIdx;
+  return "num_" + category + "_" + teamIdx;
 }
 
 function teamNameId(category, teamIdx) {
-	return "name_" + category + "_" + teamIdx;
+  return "name_" + category + "_" + teamIdx;
 }
 
 function teamOrgId(category, teamIdx) {
-	return "org_" + category + "_" + teamIdx;
+  return "org_" + category + "_" + teamIdx;
 }
 
 function teamJudgingStationId(category, teamIdx) {
-	return "judgingStation_" + category + "_" + teamIdx;
+  return "judgingStation_" + category + "_" + teamIdx;
 }
 
 function populateTeamInformation(category, teamIdx, team) {
-	$("#" + teamNumId(category.catId, teamIdx)).val(team.num);
-	$("#" + teamNumId(category.catId, teamIdx)).data('oldVal', team.num);
-	$("#" + teamNameId(category.catId, teamIdx)).val(team.name);
-	$("#" + teamOrgId(category.catId, teamIdx)).val(team.org);
-	$("#" + teamJudgingStationId(category.catId, teamIdx)).val(
-			team.judgingStation);
+  $("#" + teamNumId(category.catId, teamIdx)).val(team.num);
+  $("#" + teamNumId(category.catId, teamIdx)).data('oldVal', team.num);
+  $("#" + teamNameId(category.catId, teamIdx)).val(team.name);
+  $("#" + teamOrgId(category.catId, teamIdx)).val(team.org);
+  $("#" + teamJudgingStationId(category.catId, teamIdx)).val(
+      team.judgingStation);
 }
 
 /**
@@ -153,46 +150,46 @@ function populateTeamInformation(category, teamIdx, team) {
  *         later
  */
 function addTeam(category) {
-	var catEle = $("#category_" + category.catId);
-	var teamIdx = catEle.children().size() + 1;
+  var catEle = $("#category_" + category.catId);
+  var teamIdx = catEle.children().size() + 1;
 
-	var teamEle = $("<li></li>");
-	catEle.append(teamEle);
+  var teamEle = $("<li></li>");
+  catEle.append(teamEle);
 
-	var numEle = $("<input type='text' id='"
-			+ teamNumId(category.catId, teamIdx) + "'/>");
-	teamEle.append(numEle);
-	numEle.change(function() {
-		var teamNum = $(this).val();
-		var prevTeam = $(this).data('oldVal');
-		if ("" == teamNum) {
-			$.finalist.removeTeamFromCategory(category, prevTeam);
-			$("#" + teamNameId(category.catId, teamIdx)).val("");
-			$("#" + teamOrgId(category.catId, teamIdx)).val("");
-		} else {
-			var team = $.finalist.lookupTeam(teamNum);
-			if (typeof (team) == 'undefined') {
-				alert("Team number " + teamNum + " does not exist");
-				$(this).val(prevTeam);
-				teamNum = prevTeam; // for the set of oldVal below
-			} else {
-				populateTeamInformation(category, teamIdx, team);
-				$.finalist.addTeamToCategory(category, teamNum);
-			}
-		}
-		$(this).data('oldVal', teamNum);
-	});
+  var numEle = $("<input type='text' id='" + teamNumId(category.catId, teamIdx)
+      + "'/>");
+  teamEle.append(numEle);
+  numEle.change(function() {
+    var teamNum = $(this).val();
+    var prevTeam = $(this).data('oldVal');
+    if ("" == teamNum) {
+      $.finalist.removeTeamFromCategory(category, prevTeam);
+      $("#" + teamNameId(category.catId, teamIdx)).val("");
+      $("#" + teamOrgId(category.catId, teamIdx)).val("");
+    } else {
+      var team = $.finalist.lookupTeam(teamNum);
+      if (typeof (team) == 'undefined') {
+        alert("Team number " + teamNum + " does not exist");
+        $(this).val(prevTeam);
+        teamNum = prevTeam; // for the set of oldVal below
+      } else {
+        populateTeamInformation(category, teamIdx, team);
+        $.finalist.addTeamToCategory(category, teamNum);
+      }
+    }
+    $(this).data('oldVal', teamNum);
+  });
 
-	var nameEle = $("<input id='" + teamNameId(category.catId, teamIdx)
-			+ "' readonly/>");
-	teamEle.append(nameEle);
-	var orgEle = $("<input id='" + teamOrgId(category.catId, teamIdx)
-			+ "' readonly/>");
-	teamEle.append(orgEle);
+  var nameEle = $("<input id='" + teamNameId(category.catId, teamIdx)
+      + "' readonly/>");
+  teamEle.append(nameEle);
+  var orgEle = $("<input id='" + teamOrgId(category.catId, teamIdx)
+      + "' readonly/>");
+  teamEle.append(orgEle);
 
-	var judgingStationEle = $("<input id='"
-			+ teamJudgingStationId(category.catId, teamIdx) + "' readonly/>");
-	teamEle.append(judgingStationEle);
+  var judgingStationEle = $("<input id='"
+      + teamJudgingStationId(category.catId, teamIdx) + "' readonly/>");
+  teamEle.append(judgingStationEle);
 
-	return teamIdx;
+  return teamIdx;
 }
