7c5f63c8ec424ffdc3a98482ac432b9ab81adf4e
Jon Schewe <jpschewe@mtu.net>
2015-07-24 16:22:03 -0500

    Cobertura 2.1.1

--- scoring/lib/cobertura/cobertura-merge.sh
+++ scoring/lib/cobertura/cobertura-merge.sh
@@ -1,1 +1,2 @@
-java -cp `dirname $0`/cobertura.jar:`dirname $0`/lib/asm-3.3.1.jar:`dirname $0`/lib/asm-tree-3.3.1.jar:`dirname $0`/lib/asm-commons-3.3.1.jar:`dirname $0`/lib/log4j-1.2.9.jar:`dirname $0`/lib/jakarta-oro-2.0.8.jar net.sourceforge.cobertura.merge.Main $*
+BASEDIR=`dirname $0`
+java -cp $BASEDIR/cobertura-2.1.1.jar:$BASEDIR/lib/asm-5.0.1.jar:$BASEDIR/lib/asm-tree-5.0.1.jar:$BASEDIR/lib/asm-commons-5.0.1.jar:$BASEDIR/lib/slf4j-api-1.7.5.jar:$BASEDIR/lib/logback-core-1.0.13.jar:$BASEDIR/lib/logback-classic-1.0.13.jar:$BASEDIR/lib/oro-2.0.8.jar net.sourceforge.cobertura.merge.MergeMain $*
