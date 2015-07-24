90d6c28475b6e657ea904d73a41e8b6fe5517ab4
Jon Schewe <jpschewe@mtu.net>
2015-07-24 16:22:03 -0500

    Cobertura 2.1.1

--- scoring/lib/cobertura/cobertura-report.bat
+++ scoring/lib/cobertura/cobertura-report.bat
@@ -18,4 +18,4 @@ shift
 goto getArgs
 :doneStart
 
-java -cp "%COBERTURA_HOME%cobertura.jar;%COBERTURA_HOME%lib\asm-3.3.1.jar;%COBERTURA_HOME%lib\asm-tree-3.3.1.jar;%COBERTURA_HOME%lib\asm-commons-3.3.1.jar;%COBERTURA_HOME%lib\log4j-1.2.9.jar;%COBERTURA_HOME%lib\jakarta-oro-2.0.8.jar" net.sourceforge.cobertura.reporting.Main %CMD_LINE_ARGS%
+java -cp "%COBERTURA_HOME%cobertura-2.1.1.jar;%COBERTURA_HOME%lib\asm-5.0.1.jar;%COBERTURA_HOME%lib\asm-tree-5.0.1.jar;%COBERTURA_HOME%lib\asm-commons-5.0.1.jar;%COBERTURA_HOME%lib\asm-util-5.0.1.jar;%COBERTURA_HOME%lib\commons-lang3-3.3.2.jar;%COBERTURA_HOME%lib\slf4j-api-1.7.5.jar;%COBERTURA_HOME%lib\logback-core-1.0.13.jar;%COBERTURA_HOME%lib\logback-classic-1.0.13.jar;%COBERTURA_HOME%lib\oro-2.0.8.jar" net.sourceforge.cobertura.reporting.ReportMain %CMD_LINE_ARGS%
