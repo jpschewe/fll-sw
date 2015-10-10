b87c92f0258f57378a949b3b484b13fd8921f810
Jon Schewe <jpschewe@mtu.net>
2015-10-10 16:29:52 -0500

    Fix FindBugs warnings
    task #394: PDF subjective score sheets 
    http://github.com/jpschewe/fll-sw/issues/issue/394

--- scoring/src/fll/documents/elements/SheetElement.java
+++ scoring/src/fll/documents/elements/SheetElement.java
@@ -9,67 +9,78 @@ import fll.xml.Goal;
 import fll.xml.ScoreCategory;
 
 /**
- * This object represents the data element between the fll.xml and the subjective PDF file.
+ * This object represents the data element between the fll.xml and the
+ * subjective PDF file.
  * The structure of the SheetElement is
  * <blockquote>
- *  Hashtable&lt;String, TableElement&gt;
- *    The <code>String</code> is the category of the table on the sheet.  The category is what is printed
- *    vertically on the left side of the table and is a logical grouping of topics.<br>
- *    The <code>TableElement</code> is the data structure that holds the 2 to 4 rows of information
+ * Hashtable&lt;String, TableElement&gt;
+ * The <code>String</code> is the category of the table on the sheet. The
+ * category is what is printed
+ * vertically on the left side of the table and is a logical grouping of topics.
+ * <br>
+ * The <code>TableElement</code> is the data structure that holds the 2 to 4
+ * rows of information
  * </blockquote>
- * 
- *
  */
 public class SheetElement {
 
-	String sheetName = null;
-	Hashtable<String, TableElement> tables = new Hashtable<String, TableElement>();
-	ScoreCategory sheetData = null;
-	
-	public SheetElement(ScoreCategory sheetData) {
-		//Sheet name will be Programming, Project, Robot Design or Core Values
-		this.sheetName = sheetData.getName();
-		this.sheetData = sheetData;
-	}
-	
-	/**
-	 * An abstractGoal is a single row in a table.<br>
-	 * It has the title, the description and the 4 levels of textual description of 
-	 * what it takes to meet that level of success<br>
-	 * <code>beginning, developing, accomplished, exemplary </code><br>
-	 * There is no guarantee the abstractGoals are in order for the tables
-	 */
-	public void processSheet() {
-		List<AbstractGoal> goalsList =  sheetData.getGoals();
-		TableElement tableElement = null;
-		String tableCategory = null;
-		
-		//Go thru the sheet (ScoreCategory) and put all the rows (abstractGoal) into the right tables (Category)
-		//The assumption is made that the order in the xml is the proper order of the abstractGoals
-		for (AbstractGoal abstractGoal : goalsList) {
-			tableCategory = ((Goal)abstractGoal).getCategory();
-			tableElement = tables.get(tableCategory);
-			if (null == tableElement) {
-				tableElement = new TableElement(((Goal)abstractGoal).getCategory());
-				tables.put(tableCategory, tableElement);
-			} 
-			tableElement.addRowElement(new RowElement(abstractGoal));				
-		}
-	}
-	
-	public String getSheetName() {
-		return this.sheetName;
-	}
-	
-	public TableElement getTableElement(String table) {
-		return tables.get(table);
-	}
-	
-	public String toString() {
-		Set<String> keys = tables.keySet();
-        for(String key: keys){
-            System.out.println("Table[" + key + "]: " + tables.get(key));
+  String sheetName = null;
+
+  Hashtable<String, TableElement> tables = new Hashtable<String, TableElement>();
+
+  ScoreCategory sheetData = null;
+
+  public SheetElement(ScoreCategory sheetData) {
+    // Sheet name will be Programming, Project, Robot Design or Core Values
+    this.sheetName = sheetData.getName();
+    this.sheetData = sheetData;
+  }
+
+  /**
+   * An abstractGoal is a single row in a table.<br>
+   * It has the title, the description and the 4 levels of textual description
+   * of
+   * what it takes to meet that level of success<br>
+   * <code>beginning, developing, accomplished, exemplary </code><br>
+   * There is no guarantee the abstractGoals are in order for the tables
+   */
+  public void processSheet() {
+    List<AbstractGoal> goalsList = sheetData.getGoals();
+    TableElement tableElement = null;
+    String tableCategory = null;
+
+    // Go thru the sheet (ScoreCategory) and put all the rows (abstractGoal)
+    // into the right tables (Category)
+    // The assumption is made that the order in the xml is the proper order of
+    // the abstractGoals
+    for (AbstractGoal abstractGoal : goalsList) {
+      if (abstractGoal instanceof Goal) {
+        tableCategory = ((Goal) abstractGoal).getCategory();
+        tableElement = tables.get(tableCategory);
+        if (null == tableElement) {
+          tableElement = new TableElement(((Goal) abstractGoal).getCategory());
+          tables.put(tableCategory, tableElement);
         }
-		return "Subjective Category: " + this.sheetName;
-	}
+        tableElement.addRowElement(new RowElement(abstractGoal));
+      }
+    }
+  }
+
+  public String getSheetName() {
+    return this.sheetName;
+  }
+
+  public TableElement getTableElement(String table) {
+    return tables.get(table);
+  }
+
+  public String toString() {
+    Set<String> keys = tables.keySet();
+    for (String key : keys) {
+      System.out.println("Table["
+          + key + "]: " + tables.get(key));
+    }
+    return "Subjective Category: "
+        + this.sheetName;
+  }
 }
--- scoring/src/fll/documents/writers/SubjectivePdfManager.java
+++ scoring/src/fll/documents/writers/SubjectivePdfManager.java
@@ -1,216 +1,162 @@
 package fll.documents.writers;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
 import java.net.MalformedURLException;
-import java.util.Date;
 
 import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.pdf.PdfPTable;
-import com.itextpdf.text.pdf.PdfWriter;
 
 import fll.documents.elements.SheetElement;
-import fll.scheduler.SubjectiveTime;
 import fll.scheduler.TeamScheduleInfo;
-import fll.scheduler.TournamentSchedule;
-import fll.xml.ChallengeDescription;
-import fll.xml.ChallengeParser;
+import fll.util.FLLInternalException;
 
 public class SubjectivePdfManager {
-	public static SubjectivePdfWriter writer = SubjectivePdfWriter.getInstance();
-		
-	SheetElement coreValuesSheet = null;
-	SheetElement projectSheet = null;
-	SheetElement robotDesignSheet = null;
-	SheetElement programmingSheet = null;
+  public static final SubjectivePdfWriter writer = SubjectivePdfWriter.getInstance();
 
-	public static void main(String[] args) throws BadElementException, MalformedURLException, IOException {
-		SubjectivePdfManager pdfManager = new SubjectivePdfManager();		
-		String[] subjects = {SubjectiveConstants.PROJECT_NAME, SubjectiveConstants.CORE_VALUES_NAME, SubjectiveConstants.ROBOT_DESIGN_NAME, SubjectiveConstants.PROGRAMMING_NAME};
-		
-		//get the xml sucked in
-		final Reader descriptorReader = new FileReader("C:\\eclipse_4.4.2\\gitfll-sw\\scoring\\src\\fll\\resources\\challenge-descriptors\\fll-2014.xml");
-	  final org.w3c.dom.Document document = ChallengeParser.parse(descriptorReader);
-    final ChallengeDescription description = new ChallengeDescription(document.getDocumentElement());
-		
-		//setup the sheets from the sucked in xml
-		for (String subject : subjects) {
-		  final SheetElement sheetElement = TournamentSchedule.createSubjectiveSheetElement(subject, description);
-			pdfManager.setSheetElement(sheetElement);
-			
-			//This document will be all of the subjective pdf sheets in a single file.
-			Document pdf = SubjectivePdfManager.writer.createStandardDocument();
-	
-			try {
-				PdfWriter.getInstance(pdf, new FileOutputStream("C:\\FLL\\writeZone\\" + subject + ".pdf"));
-			} catch (FileNotFoundException | DocumentException e) {
-				System.err.println("Unable to create document.");
-				e.printStackTrace(System.err);
-			}
-	
-			pdf.open();
-			
-			///////////////////////////////////
-			//Mock up a TeamScheduleInfo object since that will be passed to the writeTeamSubjectiveFeedbackPdf method
-			
-			String title = null;
-			
-			switch (subject) {
-				case SubjectiveConstants.PROJECT_NAME:
-					title = SubjectiveConstants.PROJECT_TITLE;
-					break;
-				case SubjectiveConstants.CORE_VALUES_NAME:
-					title = SubjectiveConstants.CORE_VALUES_TITLE;
-					break;
-				case SubjectiveConstants.ROBOT_DESIGN_NAME:
-					title = SubjectiveConstants.ROBOT_DESIGN_TITLE;
-					break;
-				case SubjectiveConstants.PROGRAMMING_NAME:
-					title = SubjectiveConstants.PROGRAMMING_TITLE;
-					break;
-			}
-	
-			TeamScheduleInfo teamInfo = new TeamScheduleInfo(3,  66666);
-			teamInfo.setDivision("Woods");
-			teamInfo.setJudgingStation(title);
-			teamInfo.setOrganization("Willow Creek Middle School");
-			teamInfo.setTeamName("Masters of Disasters 2.0");
-			SubjectiveTime s = new SubjectiveTime(title, new Date());
-			teamInfo.addSubjectiveTime(s);
-			
-			//to here
-			/////////////////////////////////////////////////////////
-			
-			//call the method simulating a number of TeamScheduleInfo sheets
-			for (int a = 0; a < 4; a++) {
-				pdfManager.writeTeamSubjectivePdf(pdf, teamInfo, teamInfo.getJudgingStation());
-			}
-			
-			//This closes the single pdf document with all of the subjective sheets in it.
-			pdf.close();
-		}
-	}
-	
-	public void writeTeamSubjectivePdf(Document doc, TeamScheduleInfo teamInfo, String subjectiveStation) throws BadElementException, MalformedURLException, IOException {
-		switch (subjectiveStation) {
-			case SubjectiveConstants.CORE_VALUES_NAME:
-				writeCoreValuesSheet(doc, teamInfo);
-				break;
-			case SubjectiveConstants.PROJECT_NAME:
-				writeProjectSheet(doc, teamInfo);
-				break;
-			case SubjectiveConstants.ROBOT_DESIGN_NAME:
-				writeDesignSheet(doc, teamInfo);
-				break;
-			case SubjectiveConstants.PROGRAMMING_NAME:
-				writeProgrammingSheet(doc, teamInfo);
-				break;
-		}
-	}
+  private SheetElement coreValuesSheet = null;
 
-	private void writeDesignSheet(Document doc, TeamScheduleInfo teamInfo) throws BadElementException, MalformedURLException, IOException {
-		PdfPTable table = writer.createStandardRubricTable();
-		writer.setSheetProperties(SubjectiveConstants.ROBOT_DESIGN_TITLE);
-		writer.writeHeader(doc, teamInfo);
-		writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.MD));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.MND));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.SI));
-		writer.writeCommentsSection(table);
+  private SheetElement projectSheet = null;
 
-		try {
-			doc.add(table);
-		} catch (DocumentException e) {
-			System.err.println("unable to write to the document for the bottom section.");
-			e.printStackTrace(System.err);
-		}
+  private SheetElement robotDesignSheet = null;
 
-		writer.writeEndOfPageRow(doc);
-	}
+  private SheetElement programmingSheet = null;
 
-	private void writeProgrammingSheet(Document doc, TeamScheduleInfo teamInfo) throws BadElementException, MalformedURLException, IOException {
-		PdfPTable table = writer.createStandardRubricTable();
-		writer.setSheetProperties(SubjectiveConstants.PROGRAMMING_TITLE);
-		writer.writeHeader(doc, teamInfo);
-		writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.MNP));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.PROG));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.SI));
-		writer.writeCommentsSection(table);
+  public void writeTeamSubjectivePdf(Document doc,
+                                     TeamScheduleInfo teamInfo,
+                                     String subjectiveStation)
+                                         throws BadElementException, MalformedURLException, IOException {
+    //FIXME make this generic
+    switch (subjectiveStation) {
+    case SubjectiveConstants.CORE_VALUES_NAME:
+      writeCoreValuesSheet(doc, teamInfo);
+      break;
+    case SubjectiveConstants.PROJECT_NAME:
+      writeProjectSheet(doc, teamInfo);
+      break;
+    case SubjectiveConstants.ROBOT_DESIGN_NAME:
+      writeDesignSheet(doc, teamInfo);
+      break;
+    case SubjectiveConstants.PROGRAMMING_NAME:
+      writeProgrammingSheet(doc, teamInfo);
+      break;
+    default:
+      throw new FLLInternalException("Unknown expected subjective station name: "
+          + subjectiveStation);
+    }
+  }
 
-		try {
-			doc.add(table);
-		} catch (DocumentException e) {
-			System.err.println("unable to write to the document for the bottom section.");
-			e.printStackTrace(System.err);
-		}
+  private void writeDesignSheet(Document doc,
+                                TeamScheduleInfo teamInfo)
+                                    throws BadElementException, MalformedURLException, IOException {
+    PdfPTable table = writer.createStandardRubricTable();
+    writer.setSheetProperties(SubjectiveConstants.ROBOT_DESIGN_TITLE);
+    writer.writeHeader(doc, teamInfo);
+    writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.MD));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.MND));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, robotDesignSheet.getTableElement(SubjectiveConstants.SI));
+    writer.writeCommentsSection(table);
 
-		writer.writeEndOfPageRow(doc);
-	}
+    try {
+      doc.add(table);
+    } catch (DocumentException e) {
+      System.err.println("unable to write to the document for the bottom section.");
+      e.printStackTrace(System.err);
+    }
 
-	private void writeProjectSheet(Document doc, TeamScheduleInfo teamInfo) throws BadElementException, MalformedURLException, IOException {
-		PdfPTable table = writer.createStandardRubricTable();
-		writer.setSheetProperties(SubjectiveConstants.PROJECT_TITLE);
-		writer.writeHeader(doc, teamInfo);
-		writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.RS));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.IS));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.PS));
-		writer.writeCommentsSection(table);
+    writer.writeEndOfPageRow(doc);
+  }
 
-		try {
-			doc.add(table);
-		} catch (DocumentException e) {
-			System.err.println("unable to write to the document for the bottom section.");
-			e.printStackTrace(System.err);
-		}
+  private void writeProgrammingSheet(Document doc,
+                                     TeamScheduleInfo teamInfo)
+                                         throws BadElementException, MalformedURLException, IOException {
+    PdfPTable table = writer.createStandardRubricTable();
+    writer.setSheetProperties(SubjectiveConstants.PROGRAMMING_TITLE);
+    writer.writeHeader(doc, teamInfo);
+    writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.MNP));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.PROG));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, programmingSheet.getTableElement(SubjectiveConstants.SI));
+    writer.writeCommentsSection(table);
 
-		writer.writeEndOfPageRow(doc);
-	}
+    try {
+      doc.add(table);
+    } catch (DocumentException e) {
+      System.err.println("unable to write to the document for the bottom section.");
+      e.printStackTrace(System.err);
+    }
 
-	private void writeCoreValuesSheet(Document doc, TeamScheduleInfo teamInfo) throws BadElementException, MalformedURLException, IOException {
-		PdfPTable table = writer.createStandardRubricTable();
-		writer.setSheetProperties(SubjectiveConstants.CORE_VALUES_TITLE);
-		writer.writeHeader(doc, teamInfo);
-		writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.ISP));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.TW));
-		writer.writeCommentsSection(table);
-		writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.GP));
-		writer.writeCommentsSection(table);
+    writer.writeEndOfPageRow(doc);
+  }
 
-		try {
-			doc.add(table);
-		} catch (DocumentException e) {
-			System.err.println("unable to write to the document for the bottom section.");
-			e.printStackTrace(System.err);
-		}
+  private void writeProjectSheet(Document doc,
+                                 TeamScheduleInfo teamInfo)
+                                     throws BadElementException, MalformedURLException, IOException {
+    PdfPTable table = writer.createStandardRubricTable();
+    writer.setSheetProperties(SubjectiveConstants.PROJECT_TITLE);
+    writer.writeHeader(doc, teamInfo);
+    writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.RS));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.IS));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, projectSheet.getTableElement(SubjectiveConstants.PS));
+    writer.writeCommentsSection(table);
 
-		writer.writeEndOfPageRow(doc);
-	}
-	
-	public void setSheetElement(SheetElement sheet) {
-		switch (sheet.getSheetName()) {
-			case SubjectiveConstants.CORE_VALUES_NAME:
-				this.coreValuesSheet = sheet;
-				break;
-			case SubjectiveConstants.PROGRAMMING_NAME:
-				this.programmingSheet = sheet;
-				break;
-			case SubjectiveConstants.PROJECT_NAME:
-				this.projectSheet = sheet;
-				break;
-			case SubjectiveConstants.ROBOT_DESIGN_NAME:
-				this.robotDesignSheet = sheet;
-				break;
-		}
-	}
+    try {
+      doc.add(table);
+    } catch (DocumentException e) {
+      System.err.println("unable to write to the document for the bottom section.");
+      e.printStackTrace(System.err);
+    }
+
+    writer.writeEndOfPageRow(doc);
+  }
+
+  private void writeCoreValuesSheet(Document doc,
+                                    TeamScheduleInfo teamInfo)
+                                        throws BadElementException, MalformedURLException, IOException {
+    PdfPTable table = writer.createStandardRubricTable();
+    writer.setSheetProperties(SubjectiveConstants.CORE_VALUES_TITLE);
+    writer.writeHeader(doc, teamInfo);
+    writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.ISP));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.TW));
+    writer.writeCommentsSection(table);
+    writer.writeRubricTable(table, coreValuesSheet.getTableElement(SubjectiveConstants.GP));
+    writer.writeCommentsSection(table);
+
+    try {
+      doc.add(table);
+    } catch (DocumentException e) {
+      System.err.println("unable to write to the document for the bottom section.");
+      e.printStackTrace(System.err);
+    }
+
+    writer.writeEndOfPageRow(doc);
+  }
+
+  public void setSheetElement(SheetElement sheet) {
+    //FIXME make this generic
+    switch (sheet.getSheetName()) {
+    case SubjectiveConstants.CORE_VALUES_NAME:
+      this.coreValuesSheet = sheet;
+      break;
+    case SubjectiveConstants.PROGRAMMING_NAME:
+      this.programmingSheet = sheet;
+      break;
+    case SubjectiveConstants.PROJECT_NAME:
+      this.projectSheet = sheet;
+      break;
+    case SubjectiveConstants.ROBOT_DESIGN_NAME:
+      this.robotDesignSheet = sheet;
+      break;
+    default:
+      throw new FLLInternalException("Unknown expected subjective station name: "
+          + sheet.getSheetName());
+    }
+  }
 }
