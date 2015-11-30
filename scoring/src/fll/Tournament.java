4b0e034e4c5e8d8b563eaf2c7d9e6e01729cdec5
Jon Schewe <jpschewe@mtu.net>
2015-11-29 18:51:33 -0600

    Update changes and remove extra imports
    fixes #408: How to handle advancing teams to sectionals? 
    http://github.com/jpschewe/fll-sw/issues/issue/408

--- scoring/src/fll/Tournament.java
+++ scoring/src/fll/Tournament.java
@@ -12,7 +12,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.sql.Types;
 import java.util.LinkedList;
 import java.util.List;
 
