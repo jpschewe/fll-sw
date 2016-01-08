46c1bc31e38a90f932c8dcc9325bb0ee53c87d0a
Jon Schewe <jpschewe@mtu.net>
2016-01-07 18:11:16 -0600

    Take the footer off of the slideshow page
    There is no reason to show the footer on any of the display pages.

--- scoring/src/fll/web/FooterFilter.java
+++ scoring/src/fll/web/FooterFilter.java
@@ -144,6 +144,8 @@ public class FooterFilter implements Filter {
       return true;
     } else if (url.indexOf("report/finalist/PublicFinalistDisplaySchedule.jsp") != -1) {
       return true;
+    } else if(url.indexOf("slideshow/index.jsp") != -1) {
+      return true;
     } else {
       return false;
     }
