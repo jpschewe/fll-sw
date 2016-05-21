2148c01614fee25de90b05ba3370479133c4daf7
Jon Schewe <jpschewe@mtu.net>
2016-05-21 17:08:55 -0500

    Names of subjective stations cannot be empty and durations are sane
    Still TODO:
    * Make sure the value set to the table model for the durations is always
        correct. Sometimes I'm getting the wrong value back.
    * Implement delete button

--- scoring/src/fll/scheduler/SubjectiveStationListEditor.java
+++ scoring/src/fll/scheduler/SubjectiveStationListEditor.java
@@ -12,9 +12,14 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JTable;
+import javax.swing.JTextField;
+
+import fll.util.FormatterUtils;
 
 /**
  * Edit a list of {@link SubjectiveStation} objects.
@@ -22,6 +27,7 @@ import javax.swing.JTable;
 /* package */ class SubjectiveStationListEditor extends JComponent {
 
   private final SubjectiveStationModel tableModel;
+
   private final JTable table;
 
   public SubjectiveStationListEditor() {
@@ -32,21 +38,20 @@ import javax.swing.JTable;
 
     tableModel = new SubjectiveStationModel();
     table = new JTable(this.tableModel);
+    table.setDefaultEditor(String.class, new NameCellEditor());
+    table.setDefaultEditor(Integer.class, new DurationCellEditor());
 
-    //FIXME make sure names are not empty
-    //FIXME make sure durations are greater than 0
-    
     final JButton addButton = new JButton("Add Row");
     addButton.addActionListener(new ActionListener() {
       public void actionPerformed(final ActionEvent ae) {
         tableModel.addNewRow();
       }
     });
-    
-    add(addButton, BorderLayout.SOUTH);    
+
+    add(addButton, BorderLayout.SOUTH);
     add(table, BorderLayout.CENTER);
     add(table.getTableHeader(), BorderLayout.NORTH);
-    
+
   }
 
   public void setStations(final List<SubjectiveStation> stations) {
@@ -59,4 +64,50 @@ import javax.swing.JTable;
   public List<SubjectiveStation> getStations() {
     return this.tableModel.getData();
   }
+
+  private static final class NameCellEditor extends DefaultCellEditor {
+
+    public NameCellEditor() {
+      super(new JTextField()); // perhaps could use a formatted text field with
+                               // a verifier for null
+    }
+
+    @Override
+    public boolean stopCellEditing() {
+      final String value = getCellEditorValue().toString();
+      if (value.isEmpty()) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  private static final class DurationCellEditor extends DefaultCellEditor {
+    public DurationCellEditor() {
+      super(FormatterUtils.createIntegerField(1, 1000));
+    }
+
+    @Override
+    public Object getCellEditorValue() {
+      final Object v = getComponent().getValue();
+      return v;
+    }
+
+    @Override
+    public boolean stopCellEditing() {
+      if (!getComponent().getInputVerifier().shouldYieldFocus(getComponent())) {
+        return false;
+      } else {
+        return super.stopCellEditing();
+      }
+    }
+
+    @Override
+    public JFormattedTextField getComponent() {
+      return (JFormattedTextField) super.getComponent();
+    }
+
+  }
+
 }
