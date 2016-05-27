f3522aaec94e17a51b0d29dd5d14a7e2ca7052cb
Jon Schewe <jpschewe@mtu.net>
2016-05-27 06:13:59 -0500

    Notify the user what the valid range for integers is
    When the value entered into an integer table cell isn't valid, let the
    user know what the valid values are.

--- scoring/src/fll/scheduler/SubjectiveStationListEditor.java
+++ scoring/src/fll/scheduler/SubjectiveStationListEditor.java
@@ -15,16 +15,20 @@ import javax.swing.BorderFactory;
 import javax.swing.DefaultCellEditor;
 import javax.swing.JButton;
 import javax.swing.JComponent;
-import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
-import fll.util.FormatterUtils;
+import org.apache.log4j.Logger;
+
+import fll.util.LogUtils;
 
 /**
  * Edit a list of {@link SubjectiveStation} objects.
  */
 /* package */ class SubjectiveStationListEditor extends JComponent {
+
+  private static final Logger LOGGER = LogUtils.getLogger();
 
   private final SubjectiveStationModel tableModel;
 
@@ -39,7 +43,7 @@ import fll.util.FormatterUtils;
     tableModel = new SubjectiveStationModel();
     table = new JTable(this.tableModel);
     table.setDefaultEditor(String.class, new NameCellEditor());
-    table.setDefaultEditor(Integer.class, new DurationCellEditor());
+    table.setDefaultEditor(Integer.class, new IntegerCellEditor(1, 1000));
 
     final JButton addButton = new JButton("Add Row");
     addButton.addActionListener(new ActionListener() {
@@ -83,29 +87,65 @@ import fll.util.FormatterUtils;
     }
   }
 
-  private static final class DurationCellEditor extends DefaultCellEditor {
-    public DurationCellEditor() {
-      super(FormatterUtils.createIntegerField(1, 1000));
+  private static final class IntegerCellEditor extends DefaultCellEditor {
+
+    private final int minValue;
+
+    private final int maxValue;
+
+    /**
+     * Create a cell editor for integers.
+     * 
+     * @param min the minimum allowed value (inclusive)
+     * @param max the maximum allowed value (inclusive)
+     */
+    public IntegerCellEditor(final int min,
+                             final int max) {
+      super(new JTextField());
+      minValue = min;
+      maxValue = max;
+    }
+
+    @Override
+    public JTextField getComponent() {
+      return (JTextField) super.getComponent();
     }
 
     @Override
     public Object getCellEditorValue() {
-      final Object v = getComponent().getValue();
-      return v;
+      final String str = super.getCellEditorValue().toString();
+      final Integer value = Integer.parseInt(str);
+      return value;
     }
 
     @Override
     public boolean stopCellEditing() {
-      if (!getComponent().getInputVerifier().shouldYieldFocus(getComponent())) {
-        return false;
-      } else {
-        return super.stopCellEditing();
+      final String str = getComponent().getText();
+      boolean stop;
+      try {
+        final Integer value = Integer.parseInt(str);
+        if (minValue <= value
+            && value <= maxValue) {
+          stop = true;
+        } else {
+          stop = false;
+        }
+      } catch (final NumberFormatException e) {
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Could not parse as integer", e);
+        }
+        stop = false;
       }
-    }
 
-    @Override
-    public JFormattedTextField getComponent() {
-      return (JFormattedTextField) super.getComponent();
+      if (stop) {
+        return super.stopCellEditing();
+      } else {
+        // dialog
+        JOptionPane.showMessageDialog(null,
+                                      String.format("You must enter an integer between %d and %d", minValue, maxValue),
+                                      "Error", JOptionPane.WARNING_MESSAGE);
+        return false;
+      }
     }
 
   }
