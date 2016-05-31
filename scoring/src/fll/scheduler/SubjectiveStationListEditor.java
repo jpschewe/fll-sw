de0ecf3cb097de0a4265c3fd569222028e60a94a
Jon Schewe <jpschewe@mtu.net>
2016-05-30 20:01:42 -0500

    Implement delete button for table

--- scoring/src/fll/scheduler/SubjectiveStationListEditor.java
+++ scoring/src/fll/scheduler/SubjectiveStationListEditor.java
@@ -11,16 +11,24 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JTable;
+
+import org.apache.log4j.Logger;
+
+import fll.util.LogUtils;
 
 /**
  * Edit a list of {@link SubjectiveStation} objects.
  */
 /* package */ class SubjectiveStationListEditor extends JComponent {
 
+  private static final Logger LOGGER = LogUtils.getLogger();
+  
   private final SubjectiveStationModel tableModel;
 
   private final JTable table;
@@ -36,6 +44,8 @@ import javax.swing.JTable;
     table.setDefaultEditor(String.class, new NameCellEditor());
     table.setDefaultEditor(Integer.class, new IntegerCellEditor(1, 1000));
 
+    new ButtonColumn(table, deleteAction, 2);
+
     final JButton addButton = new JButton("Add Row");
     addButton.addActionListener(new ActionListener() {
       public void actionPerformed(final ActionEvent ae) {
@@ -46,8 +56,23 @@ import javax.swing.JTable;
     add(addButton, BorderLayout.SOUTH);
     add(table, BorderLayout.CENTER);
     add(table.getTableHeader(), BorderLayout.NORTH);
-
   }
+
+  private final Action deleteAction = new AbstractAction() {
+    @Override
+    public void actionPerformed(final ActionEvent ae) {
+      final String cmd = ae.getActionCommand();
+      try {
+        final int row = Integer.valueOf(cmd);
+        tableModel.deleteRow(row);
+      } catch (final NumberFormatException nfe) {
+        if (LOGGER.isDebugEnabled()) {
+          LOGGER.debug("Can't parse row number from action command: "
+              + cmd, nfe);
+        }
+      }
+    }
+  };
 
   public void setStations(final List<SubjectiveStation> stations) {
     this.tableModel.setData(stations);
