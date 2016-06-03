4499d958049808a50b7a59b0eb9941cd7bc4978d
Jon Schewe <jpschewe@mtu.net>
2016-06-03 12:00:14 -0500

    Fix bug in NameCellEditor that prevented the value from being stored
    The superclasses version of stopCellEditing needs to be called, otherwise nothing happens.

--- scoring/src/fll/scheduler/NameCellEditor.java
+++ scoring/src/fll/scheduler/NameCellEditor.java
@@ -7,24 +7,39 @@
 package fll.scheduler;
 
 import javax.swing.DefaultCellEditor;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 /**
  * Edit strings that cannot be empty.
  */
-/*package*/ final class NameCellEditor extends DefaultCellEditor {
+/* package */ final class NameCellEditor extends DefaultCellEditor {
 
   public NameCellEditor() {
     super(new JTextField());
   }
 
   @Override
+  public JTextField getComponent() {
+    return (JTextField) super.getComponent();
+  }
+
+  @Override
+  public Object getCellEditorValue() {
+    final String str = super.getCellEditorValue().toString();
+    return str;
+  }
+
+  @Override
   public boolean stopCellEditing() {
-    final String value = getCellEditorValue().toString();
-    if (value.isEmpty()) {
+    final String value = getComponent().getText();
+    if (value.isEmpty()
+        || value.contains(",")) {
+      JOptionPane.showMessageDialog(getComponent(), "Names cannot be empty and cannot contain commas", "Error",
+                                    JOptionPane.WARNING_MESSAGE);
       return false;
     } else {
-      return true;
+      return super.stopCellEditing();
     }
   }
 }
