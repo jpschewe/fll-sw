9c46ac6035204404b5904cc0ae0d044022639fc1
Jon Schewe <jpschewe@mtu.net>
2016-06-03 10:12:03 -0500

    Add editor for scheduled breaks

---
+++ scoring/src/fll/scheduler/ScheduleTimeCellEditor.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.scheduler;
+
+import java.awt.Component;
+import java.time.LocalTime;
+
+import javax.swing.AbstractCellEditor;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+
+/**
+ * Table cell editor for {@link LocalTime} schedule objects.
+ */
+/* package */ final class ScheduleTimeCellEditor extends AbstractCellEditor implements TableCellEditor {
+
+  private final ScheduleTimeField delegate;
+  private final ScheduleTimeField.TimeVerifier verifier;
+
+  /**
+   * Create a cell editor for schedule times.
+   */
+  public ScheduleTimeCellEditor() {
+    super();
+    delegate = new ScheduleTimeField();
+    verifier = new ScheduleTimeField.TimeVerifier();
+  }
+
+  @Override
+  public Object getCellEditorValue() {
+    final LocalTime value = delegate.getTime();
+    return value;
+  }
+
+  @Override
+  public boolean stopCellEditing() {
+    boolean stop;
+
+    // attempt to get the value, JPS 6/3/2016 - not sure if this is needed
+    //this.getCellEditorValue();
+    stop = verifier.verify(delegate);
+
+    if (stop) {
+      return super.stopCellEditing();
+    } else {
+      // dialog
+      JOptionPane.showMessageDialog(null, "You must enter a valid time", "Error", JOptionPane.WARNING_MESSAGE);
+      return false;
+    }
+  }
+
+  @Override
+  public Component getTableCellEditorComponent(final JTable table,
+                                               final Object value,
+                                               final boolean isSelected,
+                                               final int row,
+                                               final int column) {
+    final LocalTime time = (LocalTime) value;
+    delegate.setTime(time);
+    return delegate;
+  }
+
+}
