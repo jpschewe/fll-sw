73b09f07336a1070e183d257a1d6738e591fa692
Jon Schewe <jpschewe@mtu.net>
2017-01-02 07:24:43 -0600

    Don't allow '-' in display names
    This should avoid issues with using the display name in code.

--- scoring/src/fll/web/DisplayNames.java
+++ scoring/src/fll/web/DisplayNames.java
@@ -42,7 +42,7 @@ public final class DisplayNames {
     }
   }
 
-  private static final Pattern ILLEGAL_CHAR_PATTERN = Pattern.compile("[^A-Za-z0-9_-]");
+  private static final Pattern ILLEGAL_CHAR_PATTERN = Pattern.compile("[^A-Za-z0-9_]");
 
   /**
    * Add a display name to the list of known displays.
