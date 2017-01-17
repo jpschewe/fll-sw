b2e018d2eba0d665f535157876c3b08a0b4c99af
Jon Schewe <jpschewe@mtu.net>
2017-01-16 19:14:40 -0600

    Support for multiple brackets in the ajax brackets

--- scoring/src/fll/web/ajax/AJAXBracketQueryServlet.java
+++ scoring/src/fll/web/ajax/AJAXBracketQueryServlet.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,9 +18,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
+import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.log4j.Logger;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import fll.util.JsonUtilities;
+import fll.util.JsonUtilities.BracketLeafResultSet;
 import fll.util.LogUtils;
 import fll.web.ApplicationAttributes;
 import fll.web.BaseFLLServlet;
@@ -44,6 +51,8 @@ public class AJAXBracketQueryServlet extends BaseFLLServlet {
                                 final HttpSession session)
       throws IOException, ServletException {
 
+    final ChallengeDescription description = ApplicationAttributes.getChallengeDescription(application);
+
     final PrintWriter writer = response.getWriter();
 
     final DataSource datasource = ApplicationAttributes.getDataSource(application);
@@ -63,28 +72,47 @@ public class AJAXBracketQueryServlet extends BaseFLLServlet {
           LOGGER.debug("Session bracket information is different than application bracket information, forcing refresh");
         }
 
-        response.reset();
-        response.setContentType("application/json");
-        writer.print("{\"refresh\":\"true\"}");
+        writeRefresh(response, writer);
         return;
       }
 
-      final DisplayInfo.H2HBracketDisplay bracketInfo = displayInfo.getBrackets().get(0); // HACK
-                                                                                          // just
-                                                                                          // for
-                                                                                          // now
-
-      if (LOGGER.isTraceEnabled()) {
-        LOGGER.trace("Displaying bracket: "
-            + bracketInfo.getBracket());
-      }
-
       final String multiParam = request.getParameter("multi");
-      final String division = bracketInfo.getBracket();
       if (multiParam != null) {
         // Send off request to helpers
-        handleMultipleQuery(bracketInfo, parseInputToMap(multiParam), division, writer, application, response,
-                            connection);
+        final Map<DisplayInfo.H2HBracketDisplay, Map<Integer, Integer>> inputMap = parseInputToMap(displayInfo.getBrackets(),
+                                                                                                   multiParam);
+
+        final List<BracketLeafResultSet> allLeaves = new LinkedList<>();
+
+        for (final Map.Entry<DisplayInfo.H2HBracketDisplay, Map<Integer, Integer>> mapEntry : inputMap.entrySet()) {
+          final DisplayInfo.H2HBracketDisplay bracketInfo = mapEntry.getKey();
+          final Map<Integer, Integer> pairedMap = mapEntry.getValue();
+
+          if (LOGGER.isTraceEnabled()) {
+            LOGGER.trace(String.format("Handling bracket: %s index: %d map: %s", bracketInfo.getBracket(),
+                                       bracketInfo.getIndex(), pairedMap));
+          }
+
+          final List<BracketLeafResultSet> leaves = handleMultipleQuery(description, bracketInfo, pairedMap,
+                                                                        connection);
+          if (null == leaves) {
+            writeRefresh(response, writer);
+            return;
+          } else {
+            allLeaves.addAll(leaves);
+          }
+
+        }
+
+        response.reset();
+        response.setContentType("application/json");
+        try {
+          final ObjectMapper jsonMapper = new ObjectMapper();
+          writer.write(jsonMapper.writeValueAsString(allLeaves));
+        } catch (final JsonProcessingException e) {
+          throw new RuntimeException(e);
+        }
+
       } else {
         response.reset();
         response.setContentType("application/json");
@@ -95,6 +123,17 @@ public class AJAXBracketQueryServlet extends BaseFLLServlet {
     } finally {
       SQLFunctions.close(connection);
     }
+  }
+
+  /**
+   * Write a refresh response.
+   * You should return from the servlet immediately.
+   */
+  private void writeRefresh(final HttpServletResponse response,
+                            final PrintWriter writer) {
+    response.reset();
+    response.setContentType("application/json");
+    writer.print("{\"refresh\":\"true\"}");
   }
 
   /**
@@ -122,29 +161,52 @@ public class AJAXBracketQueryServlet extends BaseFLLServlet {
     return true;
   }
 
-  private Map<Integer, Integer> parseInputToMap(final String param) {
-    final String[] pairs = param.split("\\|");
-    final Map<Integer, Integer> pairedMap = new HashMap<Integer, Integer>();
-    for (final String pair : pairs) {
-      final String[] pieces = pair.split("\\-");
-      if (pieces.length >= 2) {
-        final String one = pieces[0];
-        final String two = pieces[1];
-        pairedMap.put(Integer.parseInt(one), Integer.parseInt(two));
-      }
+  private Map<DisplayInfo.H2HBracketDisplay, Map<Integer, Integer>> parseInputToMap(final List<DisplayInfo.H2HBracketDisplay> allBrackets,
+                                                                                    final String param) {
+
+    final String[] lids = param.split("\\|");
+    if (LOGGER.isTraceEnabled()) {
+      LOGGER.trace(String.format("parsing input: %s", Arrays.asList(lids)));
     }
-    return pairedMap;
+
+    final Map<DisplayInfo.H2HBracketDisplay, Map<Integer, Integer>> retval = new HashMap<>();
+    for (final String lid : lids) {
+      final ImmutableTriple<Integer, Integer, Integer> parsed = BracketData.parseLeafId(lid);
+      final int bracketIdx = parsed.left;
+      final int row = parsed.middle;
+      final int round = parsed.right;
+
+      if(LOGGER.isTraceEnabled()) {
+        LOGGER.trace(String.format("lid: '%s' bracketIdx: %d row: %d round: %d", bracketIdx, row, round));
+      }
+      if (bracketIdx < 0
+          || bracketIdx >= allBrackets.size()) {
+        LOGGER.error(String.format("Got bracket index out of range %d is outside [%d, %d]", bracketIdx, 0,
+                                   allBrackets.size()));
+        return null;
+      }
+
+      final DisplayInfo.H2HBracketDisplay bracket = allBrackets.get(bracketIdx);
+      if (!retval.containsKey(bracket)) {
+        retval.put(bracket, new HashMap<>());
+      }
+
+      final Map<Integer, Integer> pairedMap = retval.get(bracket);
+      pairedMap.put(row, round);
+    }
+    return retval;
   }
 
-  private void handleMultipleQuery(final H2HBracketDisplay bracketInfo,
-                                   final Map<Integer, Integer> pairedMap,
-                                   final String division,
-                                   final PrintWriter writer,
-                                   final ServletContext application,
-                                   final HttpServletResponse response,
-                                   final Connection connection)
+  /**
+   * @see JsonUtilities#generateJsonBracketInfo(String, Map, Connection,
+   *      fll.xml.PerformanceScoreCategory, BracketData, boolean, boolean)
+   * @throws SQLException
+   */
+  private List<BracketLeafResultSet> handleMultipleQuery(final ChallengeDescription description,
+                                                         final H2HBracketDisplay bracketInfo,
+                                                         final Map<Integer, Integer> pairedMap,
+                                                         final Connection connection)
       throws SQLException {
-    final ChallengeDescription description = ApplicationAttributes.getChallengeDescription(application);
 
     final int playoffRoundNumber = bracketInfo.getFirstRound();
     final int roundsLong = 3;
@@ -152,13 +214,17 @@ public class AJAXBracketQueryServlet extends BaseFLLServlet {
     final boolean showFinalsScores = false;
     final boolean showOnlyVerifiedScores = true;
 
-    final BracketData bd = new BracketData(connection, bracketInfo.getBracket(), playoffRoundNumber, playoffRoundNumber
-        + roundsLong - 1, rowsPerTeam, showFinalsScores, showOnlyVerifiedScores);
+    if (LOGGER.isTraceEnabled()) {
+      LOGGER.trace(String.format("handleMultipleQuery bracket: %s index: %d", bracketInfo.getBracket(),
+                                 bracketInfo.getIndex()));
+    }
 
-    response.reset();
-    response.setContentType("application/json");
-    writer.print(JsonUtilities.generateJsonBracketInfo(division, pairedMap, connection, description.getPerformance(),
-                                                       bd, showOnlyVerifiedScores, showFinalsScores));
+    final BracketData bd = new BracketData(connection, bracketInfo.getBracket(), playoffRoundNumber, playoffRoundNumber
+        + roundsLong - 1, rowsPerTeam, showFinalsScores, showOnlyVerifiedScores, bracketInfo.getIndex());
+
+    return JsonUtilities.generateJsonBracketInfo(bracketInfo.getBracket(), pairedMap, connection,
+                                                 description.getPerformance(), bd, showOnlyVerifiedScores,
+                                                 showFinalsScores);
   }
 
 }
