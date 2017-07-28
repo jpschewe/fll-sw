7c87e201c62b096d56c407782576a7570bf2f515
Jon Schewe <jpschewe@mtu.net>
2017-07-28 13:10:38 -0500

    Start of adding support for sending messages to mhub
    The display system thread is there and will create the correct messages
    (still need to test against mhub). There is an outline of what the mhub
    message handler needs to do, but no real implementation yet.
    
    Issue #570

---
+++ scoring/src/fll/flltools/displaySystem/list/Hide.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2017 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.flltools.displaySystem.list;
+
+/**
+ * A message to tell the list module to hide.
+ */
+public class Hide extends BaseMessage {
+  public Hide() {
+    super("hide");
+  }
+
+  /**
+   * @return null as there is no data for this message
+   */
+  @Override
+  public Object getData() {
+    return null;
+  }
+
+}
---
+++ scoring/src/fll/flltools/displaySystem/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2017 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+/**
+ * Messages and classes for the display system. The documentation for this module can
+ * be found at https://github.com/FirstLegoLeague/displaySystem.
+ */
+package fll.flltools.displaySystem;
