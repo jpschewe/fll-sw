cfbe6fe45fde6059aa7e78323243faf82a81992b
Jon Schewe <jpschewe@mtu.net>
2017-11-19 15:07:58 -0600

    Correct path to ant in tools

--- scoring/ant-win.bat
+++ scoring/ant-win.bat
@@ -1,4 +1,4 @@
 rem this silly expression emulates the unix dirname command
 set BATPATH=%~dp0
 
-%BATPATH%\tools\bin\ant %1 %2 %3 %4 %5 %6 %7 %8 %9
+%BATPATH%\tools\ant\bin\ant %1 %2 %3 %4 %5 %6 %7 %8 %9
