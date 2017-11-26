f944d2d15fab1caf58dd5e06e742c937838340e5
Jon Schewe <jpschewe@mtu.net>
2017-11-26 16:14:25 -0600

    Ensure that enumerated value names are database names

---
+++ scoring/src/fll/util/DatabaseNameCellEditor.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2017 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.util;
+
+import java.util.regex.Pattern;
+
+import javax.swing.DefaultCellEditor;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
+/**
+ * Edit cells in a table enforcing the rules of a database name. Must start with
+ * a character and only allow letters, numbers and underscore after that.
+ */
+public final class DatabaseNameCellEditor extends DefaultCellEditor {
+
+  public static final String DATABASE_NAME_REGEXP = "[a-zA-Z]\\w*";
+
+  private final Pattern pattern = Pattern.compile(DATABASE_NAME_REGEXP);
+
+  /**
+   * Create a cell editor for database name.
+   */
+  public DatabaseNameCellEditor() {
+    super(new JTextField());
+  }
+
+  @Override
+  public JTextField getComponent() {
+    return (JTextField) super.getComponent();
+  }
+
+  @Override
+  public Object getCellEditorValue() {
+    final String str = super.getCellEditorValue().toString();
+    return str;
+  }
+
+  @Override
+  public boolean stopCellEditing() {
+    final String str = getComponent().getText();
+    boolean stop;
+    if (pattern.matcher(str).matches()) {
+      stop = true;
+    } else {
+      stop = false;
+    }
+
+    if (stop) {
+      return super.stopCellEditing();
+    } else {
+      JOptionPane.showMessageDialog(getComponent(),
+                                    "The string must start with a character and then only have letters, numbers and underscores after that",
+                                    "Error", JOptionPane.WARNING_MESSAGE);
+      return false;
+    }
+  }
+
+}
