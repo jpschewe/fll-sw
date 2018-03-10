23a59f257f546988a1f3213c8b93130e4cbcba67
Jon Schewe <jpschewe@mtu.net>
2018-03-10 17:52:01 -0600

    Start of work to add computed goal editor

---
+++ scoring/src/fll/xml/ui/ComputedGoalEditor.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2018 High Tech Kids.  All rights reserved
+ * HighTechKids is on the web at: http://www.hightechkids.org
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+package fll.xml.ui;
+
+import java.awt.GridBagConstraints;
+
+import fll.xml.ComputedGoal;
+
+/**
+ * Editor for {@link ComputedGoal} objects.
+ */
+public class ComputedGoalEditor extends AbstractGoalEditor {
+  /**
+   * @param goal the goal to edit
+   */
+  public ComputedGoalEditor(final ComputedGoal goal) {
+    super(goal);
+
+    GridBagConstraints gbc;
+
+  }
+
+}
