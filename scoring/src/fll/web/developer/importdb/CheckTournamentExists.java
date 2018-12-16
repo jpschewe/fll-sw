5f5dbc0e61ea14818939350874a5f07851aefeee
Jon Schewe <jpschewe@mtu.net>
2018-12-15 18:12:57 -0600

    Allow one to specify what data to import
    Fixes issue #657
    Relates to #670

--- scoring/src/fll/web/developer/importdb/CheckTournamentExists.java
+++ scoring/src/fll/web/developer/importdb/CheckTournamentExists.java
@@ -46,37 +46,54 @@ public class CheckTournamentExists extends BaseFLLServlet {
                                                                                   ImportDBDump.IMPORT_DB_SESSION_KEY,
                                                                                   ImportDbSessionInfo.class);
 
-    try {
-      // support finding the selected tournament in the session as well
-      final String selectedTournamentParam = request.getParameter("tournament");
-      final String selectedTournament;
-      if (null == selectedTournamentParam) {
-        selectedTournament = sessionInfo.getTournamentName();
-      } else {
-        selectedTournament = selectedTournamentParam;
+    // support finding the selected tournament in the session as well
+    final String selectedTournamentParam = request.getParameter("tournament");
+    final String selectedTournament;
+    if (null == selectedTournamentParam) {
+      selectedTournament = sessionInfo.getTournamentName();
+    } else {
+      selectedTournament = selectedTournamentParam;
+    }
+
+    if (null != request.getParameter("submit_tournament")) {
+      // just came from selectTournament.jsp
+      sessionInfo.setImportSubjective(null != request.getParameter("importSubjective"));
+      sessionInfo.setImportPerformance(null != request.getParameter("importPerformance"));
+      sessionInfo.setImportFinalist(null != request.getParameter("importFinalist"));
+
+      if (LOG.isDebugEnabled()) {
+        LOG.debug("subjective: " + request.getParameter("importSubjective"));
+        LOG.debug("performance: " + request.getParameter("importPerformance"));
+        LOG.debug("finalist: " + request.getParameter("importFinalist"));
+        LOG.debug("import subjective: "
+            + sessionInfo.isImportSubjective()
+            + " performance: "
+            + sessionInfo.isImportPerformance()
+            + " finalist: "
+            + sessionInfo.isImportFinalist());
       }
+    }
 
-      if (null != selectedTournament) {
-        sessionInfo.setTournamentName(selectedTournament);
+    if (null != selectedTournament) {
+      sessionInfo.setTournamentName(selectedTournament);
 
-        // Check if the tournament exists
-        final DataSource datasource = ApplicationAttributes.getDataSource(application);
-        try (Connection connection = datasource.getConnection()) {
-          final Tournament tournament = Tournament.findTournamentByName(connection, selectedTournament);
-          if (null == tournament) {
-            session.setAttribute(SessionAttributes.REDIRECT_URL, "promptCreateTournament.jsp");
-          } else {
-            session.setAttribute(SessionAttributes.REDIRECT_URL, "FindMissingTeams");
-          }
+      // Check if the tournament exists
+      final DataSource datasource = ApplicationAttributes.getDataSource(application);
+      try (Connection connection = datasource.getConnection()) {
+        final Tournament tournament = Tournament.findTournamentByName(connection, selectedTournament);
+        if (null == tournament) {
+          session.setAttribute(SessionAttributes.REDIRECT_URL, "promptCreateTournament.jsp");
+        } else {
+          session.setAttribute(SessionAttributes.REDIRECT_URL, "FindMissingTeams");
         }
-
-        session.setAttribute(ImportDBDump.IMPORT_DB_SESSION_KEY, sessionInfo);
-      } else {
-        message.append("<p class='error'>Can't find the 'tournament' parameter</p>");
+      } catch (final SQLException sqle) {
+        LOG.error(sqle, sqle);
+        throw new RuntimeException("Error talking to the database", sqle);
       }
-    } catch (final SQLException sqle) {
-      LOG.error(sqle, sqle);
-      throw new RuntimeException("Error talking to the database", sqle);
+
+      session.setAttribute(ImportDBDump.IMPORT_DB_SESSION_KEY, sessionInfo);
+    } else {
+      message.append("<p class='error'>Can't find the 'tournament' parameter</p>");
     }
 
     session.setAttribute("message", message.toString());
