673116d7dd9d7fe16a952d26235347640334f6df
Jon Schewe <jpschewe@mtu.net>
2018-12-25 11:00:48 -0600

    Refactor common database export code

--- scoring/src/fll/web/admin/DownloadJudgesDatabase.java
+++ scoring/src/fll/web/admin/DownloadJudgesDatabase.java
@@ -9,10 +9,6 @@ package fll.web.admin;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.zip.ZipOutputStream;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -24,9 +20,7 @@ import javax.sql.DataSource;
 
 import org.w3c.dom.Document;
 
-import fll.Tournament;
 import fll.db.DumpDB;
-import fll.db.Queries;
 import fll.web.ApplicationAttributes;
 import fll.web.BaseFLLServlet;
 
@@ -47,24 +41,7 @@ public class DownloadJudgesDatabase extends BaseFLLServlet {
     final DataSource datasource = ApplicationAttributes.getDataSource(application);
     final Document challengeDocument = ApplicationAttributes.getChallengeDocument(application);
     try (Connection connection = datasource.getConnection()) {
-      final int tournamentId = Queries.getCurrentTournament(connection);
-      final Tournament tournament = Tournament.findTournamentByID(connection, tournamentId);
-
-      final DateFormat df = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
-      final String dateStr = df.format(new Date());
-
-      final String filename = String.format("%s_%s_judges-room.flldb", tournament.getName(), dateStr);
-      response.reset();
-      response.setContentType("application/zip");
-      response.setHeader("Content-Disposition", "attachment; filename="
-          + filename);
-
-      final ZipOutputStream zipOut = new ZipOutputStream(response.getOutputStream());
-      try {
-        DumpDB.dumpDatabase(zipOut, connection, challengeDocument, application);
-      } finally {
-        zipOut.close();
-      }
+      DumpDB.exportDatabase(response, application, "_judges-room", challengeDocument, connection);
     } catch (final SQLException sqle) {
       throw new RuntimeException(sqle);
     }
