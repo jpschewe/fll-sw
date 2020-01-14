572f566930504844655eb21d675fea6738a46279
Jon Schewe <jpschewe@mtu.net>
2020-01-14 05:19:54 -0600

    All cache control headers are set in InitFilter

--- src/main/java/fll/web/report/PlayoffReport.java
+++ src/main/java/fll/web/report/PlayoffReport.java
@@ -87,10 +87,6 @@ public class PlayoffReport extends BaseFLLServlet {
 
       document.close();
 
-      // setting some response headers
-      response.setHeader("Expires", "0");
-      response.setHeader("Cache-Control", "must-revalidate, post-check=0, pre-check=0");
-      response.setHeader("Pragma", "public");
       // setting the content type
       response.setContentType("application/pdf");
       response.setHeader("Content-Disposition", "filename=playoffReport.pdf");
