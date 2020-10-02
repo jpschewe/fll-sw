f203c194be0e11cf3d06b0b37f747079e28748c6
Jon Schewe <jpschewe@mtu.net>
2020-10-01 20:23:11 -0500

    Remove unused imports

--- src/test/java/fll/subjective/SubjectiveCompareTest.java
+++ src/test/java/fll/subjective/SubjectiveCompareTest.java
@@ -13,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringWriter;
 import java.util.Collection;
 
 import org.junit.jupiter.api.AfterEach;
