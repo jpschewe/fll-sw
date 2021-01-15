c73ce3bc0c8f781df85cab9007472f5e3ee1e907
Jon Schewe <jpschewe@mtu.net>
2021-01-14 19:26:39 -0600

    Formatting

--- src/main/web/js/fll-objects.js
+++ src/main/web/js/fll-objects.js
@@ -11,10 +11,10 @@
  * to match fll.web.report.finalist.FinalistDBRow.
  */
 function FinalistDBRow(categoryName, hour, minute, teamNumber) {
-  this.categoryName = categoryName;
-  this.hour = hour;
-  this.minute = minute;
-  this.teamNumber = teamNumber;
+    this.categoryName = categoryName;
+    this.hour = hour;
+    this.minute = minute;
+    this.teamNumber = teamNumber;
 }
 
 /**
@@ -22,52 +22,52 @@ function FinalistDBRow(categoryName, hour, minute, teamNumber) {
  * to match fll.web.report.finalist.FinalistCategory.
  */
 function FinalistCategory(categoryName, room) {
-  this.categoryName = categoryName;
-  this.room = room;
+    this.categoryName = categoryName;
+    this.room = room;
 }
 
 /**
  * Needs to match fll.db.NonNumericNominees.
  */
 function NonNumericNominees(categoryName, nominees) {
-  this.categoryName = categoryName;
-  this.nominees = nominees;
+    this.categoryName = categoryName;
+    this.nominees = nominees;
 }
 
 /**
  * Needs to match fll.db.NonNumericNominees.Nominee
  */
 function Nominee(teamNumber, judges) {
-  this.teamNumber = teamNumber;
-  this.judges = judges;
-  if(typeof this.judges == 'undefined') {
-    this.judges = [null];
-}
+    this.teamNumber = teamNumber;
+    this.judges = judges;
+    if (typeof this.judges == 'undefined') {
+        this.judges = [null]
+    }
 }
 
 /**
  * Javascript parallel of fll.db.AwardWinner.
  */
 function AwardWinner(name, awardGroup, teamNumber, description) {
-  this.name = name;
-  this.awardGroup = awardGroup;
-  this.teamNumber = teamNumber;
-  this.description = description;
+    this.name = name;
+    this.awardGroup = awardGroup;
+    this.teamNumber = teamNumber;
+    this.description = description;
 }
 
 /**
  * Javascript parallel of fll.db.OverallAwardWinner.
  */
 function OverallAwardWinner(name, teamNumber, description) {
-  this.name = name;
-  this.teamNumber = teamNumber;
-  this.description = description;
+    this.name = name;
+    this.teamNumber = teamNumber;
+    this.description = description;
 }
 
 /**
  * Javascript parallel of fll.db.AdvancingTeam.
  */
 function AdvancingTeam(teamNumber, group) {
-  this.teamNumber = teamNumber;
-  this.group = group;
+    this.teamNumber = teamNumber;
+    this.group = group;
 }
