55733a8ba04516e0e6561e2fbb4a2ff471493e7e
Jon Schewe <jpschewe@mtu.net>
2021-01-23 14:46:02 -0600

    Remove illegal access warnings for Tomcat
    Add jvm arguments specified by Tomcat for getting around illegal access
    warnings.
    https://github.com/apache/tomcat/blob/7.0.x/bin/catalina.sh#L322

--- src/main/launch-script/fll-sw.sh
+++ src/main/launch-script/fll-sw.sh
@@ -96,6 +96,15 @@ if $cygwin; then
   mypath=`cygpath --windows -a "$mypath"`
 fi
 
+# Add the JAVA 9 specific start-up parameters required by Tomcat
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.lang=ALL-UNNAMED"
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.io=ALL-UNNAMED"
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.util=ALL-UNNAMED"
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.base/java.util.concurrent=ALL-UNNAMED"
+JDK_JAVA_OPTIONS="$JDK_JAVA_OPTIONS --add-opens=java.rmi/sun.rmi.transport=ALL-UNNAMED"
+export JDK_JAVA_OPTIONS
+
+
 cd "${mypath}"
 CLASSPATH='classes/:lib/*'
 export CLASSPATH
