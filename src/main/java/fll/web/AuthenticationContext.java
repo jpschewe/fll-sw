74fd25844df197b26ecef8fd8764665115db3178
Jon Schewe <jpschewe@mtu.net>
2021-02-20 07:46:24 -0600

    Remove unused imports
    Issue #887

--- src/main/java/fll/web/AuthenticationContext.java
+++ src/main/java/fll/web/AuthenticationContext.java
@@ -16,8 +16,6 @@ import java.util.stream.Collectors;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
