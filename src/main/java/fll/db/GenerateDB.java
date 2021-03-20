18f8bcea188d289649f5001f075cc2142c7cf12d
Jon Schewe <jpschewe@mtu.net>
2021-03-20 13:40:09 -0500

    Fix delete of tables
    Need to use cascade to clean up the foreign key constraints.

--- src/main/java/fll/db/GenerateDB.java
+++ src/main/java/fll/db/GenerateDB.java
@@ -408,7 +408,10 @@ public final class GenerateDB {
                                                         final boolean createConstraints)
       throws SQLException {
     try (Statement stmt = connection.createStatement()) {
-      stmt.executeUpdate("DROP TABLE IF EXISTS finalist_categories");
+      // drop tables first
+      stmt.executeUpdate("DROP TABLE IF EXISTS finalist_schedule CASCADE");
+      stmt.executeUpdate("DROP TABLE IF EXISTS finalist_categories CASCADE");      
+      
       final StringBuilder sql = new StringBuilder();
       sql.append("CREATE TABLE finalist_categories (");
       sql.append("  tournament INTEGER NOT NULL");
@@ -422,7 +425,6 @@ public final class GenerateDB {
       sql.append(")");
       stmt.executeUpdate(sql.toString());
 
-      stmt.executeUpdate("DROP TABLE IF EXISTS finalist_schedules");
       final StringBuilder scheduleSql = new StringBuilder();
       scheduleSql.append("CREATE TABLE finalist_schedules (");
       scheduleSql.append("  tournament INTEGER NOT NULL");
@@ -431,11 +433,11 @@ public final class GenerateDB {
       scheduleSql.append(" ,judge_end_time TIME NOT NULL");
       scheduleSql.append(" ,team_number INTEGER NOT NULL");
       scheduleSql.append(" ,division VARCHAR(32) NOT NULL");
-      scheduleSql.append(" ,CONSTRAINT finalist_schedules_pk PRIMARY KEY (tournament, category, division, judge_time)");
+      scheduleSql.append(" ,CONSTRAINT finalist_schedule_pk PRIMARY KEY (tournament, category, division, judge_time)");
       if (createConstraints) {
-        scheduleSql.append(" ,CONSTRAINT finalist_schedules_fk1 FOREIGN KEY(tournament) REFERENCES Tournaments(tournament_id)");
-        scheduleSql.append(" ,CONSTRAINT finalist_schedules_fk2 FOREIGN KEY(team_number) REFERENCES Teams(TeamNumber)");
-        scheduleSql.append(" ,CONSTRAINT finalist_schedules_fk3 FOREIGN KEY(tournament, category, division) REFERENCES finalist_categories(tournament, category, division)");
+        scheduleSql.append(" ,CONSTRAINT finalist_schedule_fk1 FOREIGN KEY(tournament) REFERENCES Tournaments(tournament_id)");
+        scheduleSql.append(" ,CONSTRAINT finalist_schedule_fk2 FOREIGN KEY(team_number) REFERENCES Teams(TeamNumber)");
+        scheduleSql.append(" ,CONSTRAINT finalist_schedule_fk3 FOREIGN KEY(tournament, category, division) REFERENCES finalist_categories(tournament, category, division)");
       }
       scheduleSql.append(")");
       stmt.executeUpdate(scheduleSql.toString());
@@ -1105,6 +1107,8 @@ public final class GenerateDB {
   /* package */ static void createFinalistParameterTables(final Connection connection,
                                                           final boolean createConstraints)
       throws SQLException {
+    LOGGER.trace("Creating finalist parameter tables. Create constraints: {}", createConstraints);
+    
     try (Statement stmt = connection.createStatement()) {
 
       stmt.executeUpdate("DROP TABLE IF EXISTS playoff_schedules CASCADE");
