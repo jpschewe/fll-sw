5c2143809c2b276c9358d490a0e7fc6d907965d4
Jon Schewe <jpschewe@mtu.net>
2021-06-12 17:03:34 -0500

    Remove unused imports

--- src/main/java/fll/util/ScoreUtils.java
+++ src/main/java/fll/util/ScoreUtils.java
@@ -14,10 +14,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.checkerframework.checker.nullness.qual.KeyFor;
-
-import static org.checkerframework.checker.nullness.util.NullnessUtil.castNonNull;
-
 /**
  * Scoring utilities.
  */
