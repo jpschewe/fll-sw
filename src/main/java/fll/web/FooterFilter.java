c4d4534e722ae23e38026356bcc8c8b496f30071
Jon Schewe <jpschewe@mtu.net>
2021-07-24 10:13:01 -0500

    Remove header/footer from score entry page

--- src/main/java/fll/web/FooterFilter.java
+++ src/main/java/fll/web/FooterFilter.java
@@ -153,6 +153,9 @@ public class FooterFilter implements Filter {
       return true;
     } else if (url.indexOf("finalist/load.jsp") != -1) {
       return true;
+    } else if (url.indexOf("scoreEntry/GatherScoreEntryData") != -1
+        || url.indexOf("scoreEntry/scoreEntry.jsp") != -1) {
+      return true;
     } else {
       return false;
     }
