9c1260bf393f5e1f35fc578b4a9876fbbe3d8cb1
Jon Schewe <jpschewe@mtu.net>
2021-12-18 09:06:22 -0600

    Remove unused import

--- src/main/java/fll/web/StoreColumnNames.java
+++ src/main/java/fll/web/StoreColumnNames.java
@@ -12,7 +12,6 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
