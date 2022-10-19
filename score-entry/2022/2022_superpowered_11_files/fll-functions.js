38a7acbfdc6557dc5ff05ef497eb05b54ced8ec1
Jon Schewe <jpschewe@mtu.net>
2022-10-18 20:38:04 -0500

    Add 2022 challenge for teams to test

---
+++ score-entry/2022/2022_superpowered_11_files/fll-functions.js
@@ -0,0 +1,148 @@
+"use strict";
+
+/**
+ * Log the console if it's available.
+ * 
+ * @param str
+ *          the message to log
+ */
+function _log(str) {
+    if (typeof (console) != 'undefined') {
+        console.log(str);
+    }
+}
+
+/**
+ * Remove obj from the array ar. Uses identify equals (===).
+ * 
+ * @param ar
+ *          the array (modified)
+ * @param obj
+ *          the object to remove
+ */
+function removeFromArray(ar, obj) {
+    for (var i = 0; i < ar.length; i++) {
+        if (ar[i] === obj) {
+            ar.splice(i, 1);
+        }
+    }
+}
+
+/**
+ * Determine if using TLS or not. Returns "wss:" or "ws:".
+ */
+function getWebsocketProtocol() {
+    if ("https:" == document.location.protocol) {
+        return "wss:";
+    } else {
+        return "ws:";
+    }
+}
+
+/**
+ * Check if a string is empty, null or undefined.
+ */
+function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+}
+
+/**
+ * Return a promise that on success passes along the JSON from the response and rejects on an invalid HTTP response.
+ */
+function checkJsonResponse(response) {
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+}
+
+/**
+ * Remove all children from an element.
+ */
+function removeChildren(element) {
+    while (element.firstChild) {
+        element.removeChild(element.lastChild);
+    }
+}
+
+
+/**
+ * Insert text at the caret position in a text area.
+ *
+ * @param area the text area element
+ * @param text the text to insert
+ */
+function insertAtCaret(area, text) {
+    var scrollPos = area.scrollTop;
+    var strPos = 0;
+    var br = ((area.selectionStart || area.selectionStart == '0') ? "ff" : (document.selection ? "ie" : false));
+    if (br == "ie") {
+        area.focus();
+        var range = document.selection.createRange();
+        range.moveStart('character', -(area.value.length));
+        strPos = range.text.length;
+    } else if (br == "ff") {
+        strPos = area.selectionStart;
+    }
+
+    var front = (area.value).substring(0, strPos);
+    var back = (area.value).substring(strPos, area.value.length);
+    area.value = front + text + back;
+    strPos = strPos + text.length;
+    if (br == "ie") {
+        area.focus();
+        var range = document.selection.createRange();
+        range.moveStart('character', -(area.value.length));
+        range.moveStart('character', strPos);
+        range.moveEnd('character', 0);
+        range.select();
+    } else if (br == "ff") {
+        area.selectionStart = strPos;
+        area.selectionEnd = strPos;
+        area.focus();
+    }
+    area.scrollTop = scrollPos;
+}
+
+
+/**
+ * @param element check if this element is visble based on scroll
+ * @return true if the element is visible
+ */
+function elementIsVisible(element) {
+    const bounding = element.getBoundingClientRect();
+    return bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight);
+}
+
+/**
+ * @return true if val is a number 
+ */
+function isNumeric(val) {
+    return Number(parseFloat(val)) === val;
+}
+
+/**
+ * Compute the height of an element including the margins.
+ */
+function computeHeight(element) {
+    const styles = window.getComputedStyle(element);
+    const margin = parseFloat(styles['marginTop']) + parseFloat(styles['marginBottom']);
+    const height = element.offsetHeight + margin;
+    return height;
+}
+
+/**
+ * Open the link in a new window without any browser buttons. Will fall back to standard link
+ * opening if the popup is blocked.
+ * 
+ * @param link an anchor DOM object
+ */
+function openMinimalBrowser(link) {
+    const w = window.open(link.href,
+        link.target || "_blank",
+        'menubar=no,toolbar=no,location=no,directories=no,status=no,scrollbars=no,resizable=no,dependent,left=0,top=0');
+    return w ? false : true; // allow the link to work if popup is blocked
+}
---
+++ score-entry/2022/2022_superpowered_11_files/scoreEntry.js
@@ -0,0 +1,118 @@
+/*
+ * This code is released under GPL; see LICENSE.txt for details.
+ */
+
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    init();
+
+    // if set, return true to submit, false to skip submit
+    var yesCallback = null;
+
+    document.getElementById("verification-warning").classList.add("fll-sw-ui-inactive");
+
+    document.getElementById("yesno-dialog_yes").addEventListener("click", function() {
+        document.getElementById("yesno-dialog").classList.add("fll-sw-ui-inactive");
+
+        let dosubmit = true;
+        if (null != yesCallback) {
+            dosubmit = yesCallback();
+        }
+
+        if (dosubmit) {
+            document.getElementById("scoreEntry").submit();
+        }
+    });
+
+    document.getElementById("yesno-dialog_no").addEventListener("click", function() {
+        document.getElementById("yesno-dialog").classList.add("fll-sw-ui-inactive");
+    });
+
+    const submitScoreButton = document.getElementById("submit_score");
+    if (submitScoreButton) {
+        submitScoreButton.addEventListener("click", function() {
+            yesCallback = null;
+
+            var text = "";
+            if (EditFlag) {
+                if (Verified == 1) {
+                    if (savedTotalScore != document.scoreEntry.totalScore.value) {
+                        text = "You are changing and verifying a score -- are you sure?";
+                    } else {
+                        text = "You are verifying a score -- are you sure?";
+                    }
+                } else {
+                    text = "You are submitting a score without verification -- are you sure?";
+                }
+            } else {
+                text = "Submit Data -- Are you sure?";
+            }
+
+            document.getElementById("yesno-dialog_text").innerText = text;
+            document.getElementById("yesno-dialog").classList.remove("fll-sw-ui-inactive");
+        });
+    }
+
+    const deleteButton = document.getElementById("submit_delete");
+    if (deleteButton) {
+        deleteButton.addEventListener("click", function() {
+            yesCallback = function() {
+                document.getElementById("delete").value = "true";
+                return true;
+            };
+
+            document.getElementById("yesno-dialog_text").innerText =
+                "Are you sure you want to delete this score?";
+            document.getElementById("yesno-dialog").classList.remove("fll-sw-ui-inactive");
+        });
+    }
+
+    const noShowButton = document.getElementById("no_show");
+    if (noShowButton) {
+        noShowButton.addEventListener("click", function() {
+            yesCallback = function() {
+                document.getElementById("NoShow").value = "true";
+                Verified = 1;
+                refresh();
+                return true;
+            };
+
+            document.getElementById("yesno-dialog_text").innerText = "Are you sure this is a 'No Show'?";
+            document.getElementById("yesno-dialog").classList.remove("fll-sw-ui-inactive");
+        });
+    }
+
+    const cancelButton = document.getElementById("cancel");
+    if (cancelButton) {
+        cancelButton.addEventListener("click", function() {
+            yesCallback = function() {
+                window.location.assign("select_team.jsp");
+                return false;
+            };
+
+            let text;
+            if (EditFlag) {
+                text = "Cancel and lose changes?";
+            } else {
+                text = "Cancel and lose data?"
+            }
+            document.getElementById("yesno-dialog_text").innerText = text;
+            document.getElementById("yesno-dialog").classList.remove("fll-sw-ui-inactive");
+        });
+    }
+
+
+    document.getElementById("score-entry_toggle-review-mode").addEventListener('click', function() {
+        const glassPane = document.getElementById("review-mode_glasspane");
+        if (glassPane.style.zIndex > 0) {
+            glassPane.style.zIndex = -1;
+            this.classList.remove("fll-sw-button-pressed");
+        } else {
+            glassPane.style.zIndex = 10;
+            this.classList.add("fll-sw-button-pressed");
+        }
+    });
+
+
+}); // end ready function
