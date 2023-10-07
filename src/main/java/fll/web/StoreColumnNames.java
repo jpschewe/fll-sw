f121d5f565099031aca144a3f63616346d94aafa
Jon Schewe <jpschewe@mtu.net>
2023-10-07 08:56:18 -0500

    Remove unused import

--- src/main/java/fll/web/StoreColumnNames.java
+++ src/main/java/fll/web/StoreColumnNames.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
