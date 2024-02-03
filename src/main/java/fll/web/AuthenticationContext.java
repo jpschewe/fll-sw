e3fc3dc0fe2c166bca867b29ee56900098235532
Jon Schewe <jpschewe@mtu.net>
2024-02-03 16:45:13 -0600

    Admin and head judge are implicity report generators

--- src/main/java/fll/web/AuthenticationContext.java
+++ src/main/java/fll/web/AuthenticationContext.java
@@ -108,10 +108,12 @@ public final class AuthenticationContext implements Serializable {
   }
 
   /**
-   * @return is this user a report generator
+   * @return is this user a report generator, includes admin and head judge
    */
   public boolean isReportGenerator() {
-    return roles.contains(UserRole.REPORT_GENERATOR);
+    return roles.contains(UserRole.REPORT_GENERATOR)
+        || roles.contains(UserRole.HEAD_JUDGE)
+        || roles.contains(UserRole.ADMIN);
   }
 
   /**
