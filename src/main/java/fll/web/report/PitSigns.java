5dc255040ff3f2e9f3fafb68f5a7783f2a5efd47
Jon Schewe <jpschewe@mtu.net>
2024-04-09 20:04:32 -0500

    Shrink challenge logo sum
    Issue #1174

--- src/main/java/fll/web/report/PitSigns.java
+++ src/main/java/fll/web/report/PitSigns.java
@@ -173,7 +173,7 @@ public class PitSigns extends BaseFLLServlet {
 
       final Element challengeImageGraphic = FOPUtils.createXslFoElement(document, "external-graphic");
       imageBlock.appendChild(challengeImageGraphic);
-      challengeImageGraphic.setAttribute("content-width", "700px");
+      challengeImageGraphic.setAttribute("content-width", "500px");
       challengeImageGraphic.setAttribute("content-height", "150px");
       challengeImageGraphic.setAttribute("scaling", "uniform");
       challengeImageGraphic.setAttribute("src", String.format("url('data:image/png;base64,%s')", challengeImageBase64));
