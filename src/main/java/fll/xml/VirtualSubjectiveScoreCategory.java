7f0073887fab69329ef266c049a7d872a383fef6
Jon Schewe <jpschewe@mtu.net>
2024-08-03 21:55:18 -0500

    Remove unused import

--- src/main/java/fll/xml/VirtualSubjectiveScoreCategory.java
+++ src/main/java/fll/xml/VirtualSubjectiveScoreCategory.java
@@ -11,7 +11,6 @@ import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.checkerframework.checker.initialization.qual.UnknownInitialization;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
