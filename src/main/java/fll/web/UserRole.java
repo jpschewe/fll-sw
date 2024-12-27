3b47b05e72fb9e11ff68f373e54cfc62d48ca299
Jon Schewe <jpschewe@mtu.net>
2024-12-27 17:18:05 -0600

    Sort user roles roughly from least privilege to most privilege

--- src/main/java/fll/web/UserRole.java
+++ src/main/java/fll/web/UserRole.java
@@ -10,10 +10,10 @@ package fll.web;
  * Roles that a user can be in.
  */
 public enum UserRole {
-  PUBLIC("This role has no special permissions"), REF("This role can enter performance scores"), JUDGE(
-      "This role can enter subjective scores"), ADMIN("This role can do anything"), HEAD_JUDGE(
-          "This role is a judge that can run reports"), REPORT_GENERATOR(
-              "This role can run reports, but not enter scores");
+  PUBLIC("This role has no special permissions"), REPORT_GENERATOR(
+      "This role can run reports, but not enter scores"), REF("This role can enter performance scores"), JUDGE(
+          "This role can enter subjective scores"), HEAD_JUDGE(
+              "This role is a judge that can run reports"), ADMIN("This role can do anything");
 
   UserRole(final String description) {
     this.description = description;
