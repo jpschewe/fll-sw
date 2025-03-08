c3b2d05e61951451ec0268efb9e230754a06a43d
Jon Schewe <jpschewe@mtu.net>
2025-03-08 12:57:34 -0600

    Update name text field when time value is selected
    Issue #1234

--- src/main/web/schedule/chooseScheduleHeaders.js
+++ src/main/web/schedule/chooseScheduleHeaders.js
@@ -1,10 +1,11 @@
 "use strict";
 
-function setTableValue(timeSelect, tableSelect) {
+function setTableValue(timeSelect, tableSelect, nameInput) {
     let found = false;
     for (const option of timeSelect.options) {
         if (option.selected) {
             found = true;
+            nameInput.value = option.value;
         } else if (found) {
             // assume the table is the next value
             setSelectValue(tableSelect, option.value);
@@ -18,12 +19,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const roundNumber = i + 1;
         const timeSelect = document.getElementById(`perf${roundNumber}_time`);
         const tableSelect = document.getElementById(`perf${roundNumber}_table`);
+        const nameInput = document.getElementById(`perf${roundNumber}_name`);
         // sync table with initial value
-        setTableValue(timeSelect, tableSelect);
+        setTableValue(timeSelect, tableSelect, nameInput);
 
         // update table when time is selected
         timeSelect.addEventListener('change', () => {
-            setTableValue(timeSelect, tableSelect);
+            setTableValue(timeSelect, tableSelect, nameInput);
         })
     }
 
